a1ed5e28f53be8981e80513f2eea00b641317e5d
Silvano di Ninno <silvano.dininno@nxp.com>
2018-02-12 17:29:04 +0100

    add tool/script for compilation
    not for upstream
    Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>

---
+++ scripts/imx_build.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+boards_list=(mx6ulevk mx6ul9x9evk mx6ullevk mx6slevk mx6sllevk mx6sxsabreauto \
+	mx6sxsabresd mx6qsabrelite mx6qsabresd mx6qsabreauto mx6qpsabresd mx6qpsabreauto \
+	mx6dlsabresd mx6dlsabreauto mx7dsabresd mx7ulpevk mx8mqevk)
+
+CROSS_COMPILE="${CROSS_COMPILE:-arm-linux-gnueabihf-}"
+CROSS_COMPILE64="${CROSS_COMPILE64:-aarch64-linux-gnu-}"
+MKIMAGE="${MKIMAGE:-mkimage}"
+O="${O:-.}"
+
+mx67build()
+{
+	platform=$1 && \
+	make CROSS_COMPILE=${CROSS_COMPILE} CROSS_COMPILE64=${CROSS_COMPILE64} \
+		PLATFORM=imx PLATFORM_FLAVOR=$platform CFG_TEE_CORE_LOG_LEVEL=1 O=${O}/build.$platform && \
+	${CROSS_COMPILE}objcopy -O binary ${O}/build.$platform/core/tee.elf ${O}/build.$platform/tee.bin && \
+	imx_load_addr=`cat ${O}/build.$platform/core/tee-init_load_addr.txt` && \
+	${MKIMAGE} -A arm -O linux -C none -a $imx_load_addr -e $imx_load_addr \
+			-d ${O}/build.$platform/tee.bin ${O}/build.$platform/uTee.$platform && \
+	return 0
+}
+
+mx8mqevk()
+{
+	make CROSS_COMPILE=${CROSS_COMPILE} CROSS_COMPILE64=${CROSS_COMPILE64} \
+		PLATFORM=imx PLATFORM_FLAVOR=mx8mqevk  CFG_TEE_CORE_LOG_LEVEL=1 O=${O}/build.mx8mqevk && \
+	${CROSS_COMPILE64}objcopy -O binary ${O}/build.mx8mqevk/core/tee.elf ${O}/build.mx8mqevk/tee.bin && \
+	return 0
+}
+
+build()
+{
+	if [ "$1" == "mx8mqevk" ]; then
+		mx8mqevk
+	else
+		mx67build $1
+	fi
+		
+}
+
+list_board()
+{
+
+	i=0
+
+	while ((i<=${#boards_list[@]}))
+	do
+		echo ${boards_list[i]}
+		let ++i
+	done
+}
+
+help()
+{
+	echo "Make sure you have you cross compile toolchain ready"
+	echo "./build.sh help"
+	echo "Build all boards"
+	echo "./build.sh all"
+	echo "Build all boards with only building error output"
+	echo "./build.sh all-silence"
+	echo "Build specfic board"
+	echo "./build.sh 'boardname'"
+	echo "supported boards:"
+	list_board
+}
+
+arg_num=$#
+if [ ${arg_num} -eq 0 ]
+then
+	help
+	exit 0
+fi
+
+if [ "$1" == "help" ]
+then
+	help
+	exit 0
+fi
+
+if [ "$1" == "all" ]
+then
+	i=0
+
+	while ((i<${#boards_list[@]}))
+	do
+		echo "=============Building ${boards_list[i]}================"
+		build ${boards_list[i]}
+		if [ $? -ne 0 ]; then
+			echo "=============Fail building ${boards_list[i]}================"
+			exit 1
+		fi
+		let ++i
+	done
+fi
+
+if [ "$1" == "all-silence" ]
+then
+	i=0
+
+	while ((i<${#boards_list[@]}))
+	do
+		echo "=============Building ${boards_list[i]}================"
+		build ${boards_list[i]} > /dev/null
+		if [ $? -ne 0 ]; then
+			echo "=============Fail building ${boards_list[i]}================"
+			exit 1
+		fi
+		let ++i
+	done
+fi
+
+
+case $1 in
+	mx[6-7]*) mx67build $1 ;;&
+	mx8mqevk) mx8mqevk ;;&
+esac
