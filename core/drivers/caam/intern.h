17fec4d15572a06f354b00bf7ec9be08123ba3db
Cedric Neveux <cedric.neveux@nxp.com>
2018-04-13 17:21:52 +0200

    MLK-17909 RNG Instantation with basic Job Ring
      - Add RNG Handles instantation using basic Job Ring
      - Increment TRNG entropy configuration in case of failure
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
    (cherry picked from commit 1fc8c8532af7333c48b78ad908e3822fac89af7c)

--- core/drivers/caam/intern.h
+++ core/drivers/caam/intern.h
@@ -1,13 +1,13 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright 2017 NXP
+ * Copyright 2017-2018 NXP
  *
  */
 
 #ifndef __INTERN_H__
 #define __INTERN_H__
 
-void kick_trng(vaddr_t ctrl_base, uint32_t ent_delay);
+TEE_Result rng_init(vaddr_t ctrl_base);
 
 #endif /* __INTERN_H__ */
 
