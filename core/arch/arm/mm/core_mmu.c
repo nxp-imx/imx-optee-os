8111354fea1ba26c4c9900c3bee632c629fd8137
Cedric Neveux <cedric.neveux@nxp.com>
2018-07-04 16:57:15 +0200

    TEE-272 Cortex-A9 add PL310 Linux/Optee Mutex
    Check if the cache area to maintain is secure or non-secure.
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

--- core/arch/arm/mm/core_mmu.c
+++ core/arch/arm/mm/core_mmu.c
@@ -14,6 +14,7 @@
 #include <kernel/tee_misc.h>
 #include <kernel/tlb_helpers.h>
 #include <kernel/tz_ssvce_pl310.h>
+#include <mm/core_memprot.h>
 #include <mm/core_mmu.h>
 #include <platform_config.h>
 #include <trace.h>
@@ -99,6 +100,13 @@ TEE_Result cache_op_outer(enum cache_op op, paddr_t pa, size_t len)
 {
 	TEE_Result ret = TEE_SUCCESS;
 	uint32_t exceptions = thread_mask_exceptions(THREAD_EXCP_FOREIGN_INTR);
+	bool sec = tee_pbuf_is_sec(pa, len);
+	vaddr_t pl310_addr = 0;
+
+	if (sec)
+		pl310_addr = pl310_base();
+	else
+		pl310_addr = pl310_nsbase();
 
 	tee_l2cc_mutex_lock();
 	switch (op) {
@@ -107,21 +115,21 @@ TEE_Result cache_op_outer(enum cache_op op, paddr_t pa, size_t len)
 		break;
 	case DCACHE_AREA_INVALIDATE:
 		if (len)
-			arm_cl2_invbypa(pl310_base(), pa, pa + len - 1);
+			arm_cl2_invbypa(pl310_addr, pa, pa + len - 1);
 		break;
 	case DCACHE_CLEAN:
 		arm_cl2_cleanbyway(pl310_base());
 		break;
 	case DCACHE_AREA_CLEAN:
 		if (len)
-			arm_cl2_cleanbypa(pl310_base(), pa, pa + len - 1);
+			arm_cl2_cleanbypa(pl310_addr, pa, pa + len - 1);
 		break;
 	case DCACHE_CLEAN_INV:
 		arm_cl2_cleaninvbyway(pl310_base());
 		break;
 	case DCACHE_AREA_CLEAN_INV:
 		if (len)
-			arm_cl2_cleaninvbypa(pl310_base(), pa, pa + len - 1);
+			arm_cl2_cleaninvbypa(pl310_addr, pa, pa + len - 1);
 		break;
 	default:
 		ret = TEE_ERROR_NOT_IMPLEMENTED;
