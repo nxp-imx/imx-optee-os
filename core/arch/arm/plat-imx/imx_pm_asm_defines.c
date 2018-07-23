de21155bc895cb3ef4cf9901b41cd0b45f2ac0f0
Silvano di Ninno <silvano.dininno@nxp.com>
2018-07-23 14:11:16 +0200

    core: imx: pm: add support for i.mx6 family
    Add suspend/resume and cpu idle functionality for
    i.MX6Q/QP/D/DL/SX/SL/SLL/UL/ULL
    
    Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
    Signed-off-by: Alessandro Di Chiara <alessandro.dichiara@nxp.com>

--- core/arch/arm/plat-imx/imx_pm_asm_defines.c
+++ core/arch/arm/plat-imx/imx_pm_asm_defines.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (c) 2018, Linaro Limited
+ * Copyright 2018 NXP
  */
 
 #include <gen-asm-defines.h>
@@ -80,4 +81,68 @@ DEFINES
 	       offsetof(struct imx7_pm_info, ddrc_phy_num));
 	DEFINE(PM_INFO_MX7_DDRC_PHY_REG_OFF,
 	       offsetof(struct imx7_pm_info, ddrc_phy_val));
+
+	DEFINE(PM_INFO_PBASE_OFF,
+	       offsetof(struct imx6_pm_info, pa_base));
+	DEFINE(PM_INFO_ENTRY_OFF,
+	       offsetof(struct imx6_pm_info, entry));
+	DEFINE(PM_INFO_TEE_RESUME_OFF,
+	       offsetof(struct imx6_pm_info, tee_resume));
+	DEFINE(PM_INFO_DDR_TYPE_OFF,
+	       offsetof(struct imx6_pm_info, ddr_type));
+	DEFINE(PM_INFO_INFO_SIZE_OFF,
+	       offsetof(struct imx6_pm_info, pm_info_size));
+	DEFINE(PM_INFO_MMDC0_P_OFF,
+	       offsetof(struct imx6_pm_info, mmdc0_pa_base));
+	DEFINE(PM_INFO_MMDC0_V_OFF,
+	       offsetof(struct imx6_pm_info, mmdc0_va_base));
+	DEFINE(PM_INFO_MMDC1_P_OFF,
+	       offsetof(struct imx6_pm_info, mmdc1_pa_base));
+	DEFINE(PM_INFO_MMDC1_V_OFF,
+	       offsetof(struct imx6_pm_info, mmdc1_va_base));
+	DEFINE(PM_INFO_SRC_P_OFF,
+	       offsetof(struct imx6_pm_info, src_pa_base));
+	DEFINE(PM_INFO_SRC_V_OFF,
+	       offsetof(struct imx6_pm_info, src_va_base));
+	DEFINE(PM_INFO_IOMUXC_P_OFF,
+	       offsetof(struct imx6_pm_info, iomuxc_pa_base));
+	DEFINE(PM_INFO_IOMUXC_V_OFF,
+	       offsetof(struct imx6_pm_info, iomuxc_va_base));
+	DEFINE(PM_INFO_CCM_P_OFF,
+	       offsetof(struct imx6_pm_info, ccm_pa_base));
+	DEFINE(PM_INFO_CCM_V_OFF,
+	       offsetof(struct imx6_pm_info, ccm_va_base));
+	DEFINE(PM_INFO_GPC_P_OFF,
+	       offsetof(struct imx6_pm_info, gpc_pa_base));
+	DEFINE(PM_INFO_GPC_V_OFF,
+	       offsetof(struct imx6_pm_info, gpc_va_base));
+	DEFINE(PM_INFO_PL310_P_OFF,
+	       offsetof(struct imx6_pm_info, pl310_pa_base));
+	DEFINE(PM_INFO_PL310_V_OFF,
+	       offsetof(struct imx6_pm_info, pl310_va_base));
+	DEFINE(PM_INFO_ANATOP_P_OFF,
+	       offsetof(struct imx6_pm_info, anatop_pa_base));
+	DEFINE(PM_INFO_ANATOP_V_OFF,
+	       offsetof(struct imx6_pm_info, anatop_va_base));
+	DEFINE(PM_INFO_SEMA4_P_OFF,
+	       offsetof(struct imx6_pm_info, sema4_pa_base));
+	DEFINE(PM_INFO_SEMA4_V_OFF,
+	       offsetof(struct imx6_pm_info, sema4_va_base));
+	DEFINE(PM_INFO_TTBR0_OFF,
+	       offsetof(struct imx6_pm_info, ttbr0));
+	DEFINE(PM_INFO_TTBR1_OFF,
+	       offsetof(struct imx6_pm_info, ttbr1));
+	DEFINE(PM_INFO_SAVED_DIAGNOSTIC_OFF,
+	       offsetof(struct imx6_pm_info, diagnostic));
+	DEFINE(PM_INFO_IDLE_STATE,
+	       offsetof(struct imx6_pm_info, idle_state));
+	DEFINE(PM_INFO_MMDC_IO_NUM_OFF,
+	       offsetof(struct imx6_pm_info, mmdc_io_num));
+	DEFINE(PM_INFO_MMDC_IO_VAL_OFF,
+	       offsetof(struct imx6_pm_info, mmdc_io_val));
+/* below offsets depends on MX6_MAX_MMDC_IO_NUM(36) definition */
+	DEFINE(PM_INFO_MMDC_NUM_OFF,
+	       offsetof(struct imx6_pm_info, mmdc_num));
+	DEFINE(PM_INFO_MMDC_VAL_OFF,
+	       offsetof(struct imx6_pm_info, mmdc_val));
 }
