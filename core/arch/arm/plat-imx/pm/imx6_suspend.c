80a99a57de6f0c1f67a0b62b70da84362c014f3c
Clement Faure <clement.faure@nxp.com>
2019-02-01 14:00:29 +0100

    MLK-20701 MLK-20607 Lock PL310 for suspend/resume
    Instead of disabling the PL310 before suspend, it is locked with the
    following procedure:
    	* L2 lock
    	* L2 sync
    	* L2 flush
    	* L2 sync
    	* L1 flush
    	* L1 disable
    	* L1 flush
    
    If an interruption occurs during the suspend, the PL310 is unlocked. At
    wakeup, the the PL310 is re-enalbed with the following procedure:
    	* L2 configuration
    	* L2 invalidate
    	* L2 enabled
    	* L2 invalidate
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>
    (cherry picked from commit 7c98ced1dcb3feba06b41b14fb6b7c2d576b72cb)

--- core/arch/arm/plat-imx/pm/imx6_suspend.c
+++ core/arch/arm/plat-imx/pm/imx6_suspend.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright 2017-2018 NXP
+ * Copyright 2017-2018, 2020 NXP
  *
  */
 #include <arm.h>
@@ -9,6 +9,7 @@
 #include <io.h>
 #include <imx.h>
 #include <imx_pm.h>
+#include <imx_pl310.h>
 #include <kernel/cache_helpers.h>
 #include <kernel/generic_boot.h>
 #include <kernel/misc.h>
@@ -68,11 +69,12 @@ int imx6_cpu_suspend(uint32_t power_state __unused, uintptr_t entry,
 	 * when bootup. Now MMU is up, L1 enabled.
 	 */
 #ifdef CFG_PL310
+	if (pl310_enabled(pl310_base()))
+		return 0;
+
 	arm_cl2_config(pl310_base());
 	arm_cl2_invbyway(pl310_base());
 	arm_cl2_enable(pl310_base());
-	/* Do we need to lock? cpu performance? */
-	/*arm_cl2_lockallways(pl310_base()); */
 	arm_cl2_invbyway(pl310_base());
 #endif
 
