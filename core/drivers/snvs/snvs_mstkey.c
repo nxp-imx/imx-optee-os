nxp-imx/imx-optee-os

commit 58bb15df22bfaa88bac4cc3ccbed5ae29c9c1778

TEE-358 Add Master Key selection in SNVS
       - Add selection of the Master Key to be OMTPK if valid
       - Add Soc function to know if device is closed

 Warning: i.MX7ULP can not access SNVS in the current version

    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/drivers/snvs/snvs_mstkey.c
@@ -1,0 +1,159 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/**
+ * @copyright 2019 NXP
+ *
+ * @file    snvs_mstkey.c
+ *
+ * @brief   Device Master key selection
+ */
+/* Global includes */
+#include <drivers/imx_snvs.h>
+#include <io.h>
+#include <kernel/panic.h>
+#include <mm/core_memprot.h>
+#include <platform_config.h>
+
+/* Platform includes */
+#include <imx.h>
+
+/**
+ * @brief   Checks if the master key is the OTPMK
+ *
+ * @retval true if master key is OTPMK
+ * @retval false otherwise
+ */
+static inline bool is_otpmk_selected(void)
+{
+	vaddr_t snvs_base = core_mmu_get_va(SNVS_BASE, MEM_AREA_IO_SEC);
+	uint32_t hp_mks;
+	uint32_t lp_mks;
+
+	hp_mks = read32(snvs_base + SNVS_HPCOMR);
+	hp_mks &= ~BM_SNVS_HPCOMR_MKS_EN;
+
+	/*
+	 * If Master Key is function of the LP MASTER_KEY_SEL
+	 * field
+	 */
+	if (hp_mks) {
+		lp_mks = read32(snvs_base + SNVS_LPMKCR);
+		lp_mks &= ~BM_SNVS_LP_MKCR_MKS_SEL;
+
+		if (lp_mks != 0)
+			return false;
+	}
+
+	return true;
+}
+
+/**
+ * @brief   Check if master key selection is locked
+ *
+ * @retval true if locked
+ * @retval false otherwise
+ */
+static inline bool is_mks_locked(void)
+{
+	vaddr_t snvs_base = core_mmu_get_va(SNVS_BASE, MEM_AREA_IO_SEC);
+	uint32_t hp_lr;
+	uint32_t lp_lr;
+
+	hp_lr = read32(snvs_base + SNVS_HPLR);
+	lp_lr = read32(snvs_base + SNVS_LPLR);
+
+	if ((hp_lr & BM_SNVS_HPLR_MKS_SL) || (lp_lr & BM_SNVS_LPLR_MKS_HL))
+		return true;
+
+	return false;
+}
+
+/**
+ * @brief   Set the Master key to use OTPMK and lock it. If the
+ *          Master Key is already locked returns.
+ *
+ * @retval 0    success
+ * @retval (-1) Master Key is already locked
+ */
+static inline int set_mks_otpmk(void)
+{
+	vaddr_t snvs_base = core_mmu_get_va(SNVS_BASE, MEM_AREA_IO_SEC);
+
+	if (is_mks_locked())
+		return (-1);
+
+	io_mask32(snvs_base + SNVS_HPCOMR, BM_SNVS_HPCOMR_MKS_EN,
+			BM_SNVS_HPCOMR_MKS_EN);
+	io_mask32(snvs_base + SNVS_LPMKCR, 0, BM_SNVS_LP_MKCR_MKS_SEL);
+
+	io_mask32(snvs_base + SNVS_HPLR, BM_SNVS_HPLR_MKS_SL,
+			BM_SNVS_HPLR_MKS_SL);
+	io_mask32(snvs_base + SNVS_LPLR, BM_SNVS_LPLR_MKS_HL,
+			BM_SNVS_LPLR_MKS_HL);
+
+	return 0;
+}
+
+/**
+ * @brief   Check if OTPMK is valid
+ *
+ * @retval true if valid
+ * @retval false otherwise
+ */
+static inline bool is_otpmk_valid(void)
+{
+	vaddr_t snvs_base = core_mmu_get_va(SNVS_BASE, MEM_AREA_IO_SEC);
+
+	uint32_t hp_status;
+
+	hp_status = read32(snvs_base + SNVS_HPLR);
+
+	hp_status &= (BM_SNVS_HPSTATUS_OTPMK_ZERO |
+			BM_SNVS_HPSTATUS_OTPMK_SYND);
+
+	if (hp_status) {
+		EMSG("OTPK Key is not valid");
+		return false;
+	}
+
+	return true;
+
+}
+
+/**
+ * @brief   Set the OTPMK Key as Master key.
+ *          If device is a closed device and OTMPK can not be set
+ *          system stop in panic.
+ *          If device is NOT a closed device and OTPMK can not be
+ *          set, continue anyway.
+ */
+void snvs_set_master_otpmk(void)
+{
+	int status = (-1);
+
+	if (is_otpmk_valid()) {
+		if (is_otpmk_selected() && is_mks_locked())
+			return;
+
+		status = set_mks_otpmk();
+	}
+
+	if (!imx_is_device_closed() && (status != 0)) {
+		IMSG("*******************************************************");
+		IMSG("* The system not in Trusted State                     *");
+		IMSG("* This mode is only suitable for development purposes *");
+		IMSG("* WARNING: Master Key is cannot be set to OTPMK       *");
+		IMSG("* Continue without error                              *");
+		IMSG("* Please do not go in production with this            *");
+		IMSG("* All Secure storage object created in this state     *");
+		IMSG("* will not be recoverable once this device will be    *");
+		IMSG("* closed                                              *");
+		IMSG("*******************************************************");
+	} else if (imx_is_device_closed() && (status != 0)) {
+		IMSG("*******************************************************");
+		IMSG("* The system in Trusted State                         *");
+		IMSG("* ERROR: Master Key is cannot be set to OTPMK         *");
+		IMSG("* Stop Here                                           *");
+		IMSG("*******************************************************");
+		panic();
+	}
+}

core/arch/arm/plat-imx/conf.mk
@@ -138,6 +138,7 @@ CFG_TZC380 ?= y
 CFG_IMX_UART ?= y
 CFG_IMX_LPUART ?= y
 CFG_IMX_CAAM ?= y
+CFG_IMX_SNVS ?= y
 else
 # arm-v7 platforms Common definition
 ta-targets = ta_arm32
@@ -157,6 +158,7 @@ CFG_IMX_UART ?= y
 CFG_TZC380 ?= y
 CFG_CSU ?= y
 CFG_IMX_CAAM ?= y
+CFG_IMX_SNVS ?= y
 $(call force,CFG_BOOT_SYNC_CPU,n)
 $(call force,CFG_BOOT_SECONDARY_REQUEST,n)
 $(call force,CFG_IMX_LPUART,n)
@@ -177,6 +179,7 @@ CFG_BOOT_SYNC_CPU ?= y
 CFG_BOOT_SECONDARY_REQUEST ?= y
 CFG_ENABLE_SCTLR_RR ?= y
 CFG_IMX_CAAM ?= y
+CFG_IMX_SNVS ?= y
 $(call force,CFG_IMX_LPUART,n)
 endif
 
@@ -189,6 +192,7 @@ CFG_IMX_UART ?= y
 CFG_TZC380 ?= y
 CFG_CSU ?= y
 CFG_IMX_CAAM ?= y
+CFG_IMX_SNVS ?= y
 $(call force,CFG_IMX_LPUART,n)
 endif
 

core/arch/arm/plat-imx/imx-common.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (C) 2016 Freescale Semiconductor, Inc.
- * Copyright 2017-2018 NXP
+ * Copyright 2017-2019 NXP
  *
  * Peng Fan <peng.fan@nxp.com>
  */
@@ -198,3 +198,25 @@ uint16_t soc_revision(void)
 
 	return imx_soc_revision;
 }
+
+#ifdef CFG_IMX_SNVS
+/**
+ * @brief   Returns if the device is closed (full secure) or not
+ *
+ * @retval  true if closed device
+ * @retval  false if not closed device
+ */
+bool imx_is_device_closed(void)
+{
+	uint32_t val;
+	vaddr_t  snvs_base = core_mmu_get_va(SNVS_BASE, MEM_AREA_IO_SEC);
+
+	val = read32(snvs_base + SNVS_HPSTATUS);
+	val &= BM_SNVS_HPSTATUS_SYS_SEC_CFG;
+
+	if ((val & SNVS_HPSTATUS_CLOSED) && !(val & SNVS_HPSTATUS_BAD))
+		return true;
+
+	return false;
+}
+#endif

core/arch/arm/plat-imx/imx.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright 2017-2018 NXP
+ * Copyright 2017-2019 NXP
  *
  * Peng Fan <peng.fan@nxp.com>
  */
@@ -45,6 +45,8 @@ bool soc_is_imx8mq_b1_layer(void);
 uint16_t soc_revision(void);
 void imx_gpcv2_set_core1_pdn_by_software(void);
 void imx_gpcv2_set_core1_pup_by_software(void);
+
+bool imx_is_device_closed(void);
 
 void plat_cpu_wakeup_late(void);
 

core/arch/arm/plat-imx/pm/psci.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
  * Copyright (C) 2016 Freescale Semiconductor, Inc.
- * Copyright 2018 NXP
+ * Copyright 2019 NXP
  *
  * Peng Fan <peng.fan@nxp.com>
  */
@@ -157,9 +157,8 @@ void __noreturn psci_system_off(void)
 #ifndef CFG_MX7ULP
 	vaddr_t snvs_base = core_mmu_get_va(SNVS_BASE, MEM_AREA_IO_SEC);
 
-	write32(SNVS_LPCR_TOP_MASK |
-		SNVS_LPCR_DP_EN_MASK |
-		SNVS_LPCR_SRTC_ENV_MASK, snvs_base + SNVS_LPCR_OFF);
+	write32(BM_SNVS_LPCR_TOP | BM_SNVS_LPCR_DP_EN |	BM_SNVS_LPCR_SRTC_ENV,
+			snvs_base + SNVS_LPCR);
 	dsb();
 #endif
 

core/arch/arm/plat-imx/registers/imx-regs.h
@@ -4,7 +4,7 @@
  * All rights reserved.
  * Copyright (c) 2016, Wind River Systems.
  * All rights reserved.
- * Copyright 2017-2018 NXP
+ * Copyright 2017-2019 NXP
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -49,18 +49,14 @@
 #error "PLATFORM not defined"
 #endif
 
+#include <registers/snvs-regs.h>
+
 /* Register offset used to get the CPU Type and Revision */
 #define HW_ANADIG_DIGPROG		0x260
 #define HW_ANADIG_DIGPROG_IMX6SL	0x280
 #define HW_ANADIG_DIGPROG_IMX7D		0x800
 #define HW_ANADIG_DIGPROG_IMX8MQ	0x06c
 #define HW_ANADIG_DIGPROG_IMX8MM	0x800
-
-#define SNVS_LPCR_OFF			0x38
-#define SNVS_LPCR_TOP_MASK		BIT(6)
-#define SNVS_LPCR_DP_EN_MASK		BIT(5)
-#define SNVS_LPCR_SRTC_ENV_MASK		1
-
 
 #define IOMUXC_GPR4_OFFSET	0x10
 #define IOMUXC_GPR5_OFFSET	0x14

core/arch/arm/plat-imx/registers/imx8m-regs.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright 2017 NXP
+ * Copyright 2017-2019 NXP
  *
  */
 
@@ -16,6 +16,7 @@
 #define TZASC_BASE	0x32F80000
 #define CAAM_BASE	0x30900000
 #define ANATOP_BASE 0x30360000
+#define SNVS_BASE   0x30370000
 
 #define SW_INFO_B1	0x40
 #define SW_B1_MAGIC	0xff0055aa

core/arch/arm/plat-imx/registers/imx8mm-regs.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: BSD-2-Clause */
 /*
- * Copyright 2017 NXP
+ * Copyright 2017-2019 NXP
  *
  */
 
@@ -16,5 +16,6 @@
 #define TZASC_BASE	0x32F80000
 #define CAAM_BASE	0x30900000
 #define ANATOP_BASE 0x30360000
+#define SNVS_BASE   0x30370000
 
 #endif /* __IMX8_REGS_H__ */

commit 093959fc13b643fb24a2e69b50f73680b63233e0

TEE-358 Fix wrong SNVS HP register read in is_otpmk_valid api.
Fixes: 58bb15df22b ("TEE-358 Add Master Key selection in SNVS").

Signed-off-by: Ankit Patel <ankit.patel@nxp.com>

core/drivers/snvs/snvs_mstkey.c
@@ -105,7 +105,7 @@ static inline bool is_otpmk_valid(void)
 
 	uint32_t hp_status;
 
-	hp_status = read32(snvs_base + SNVS_HPLR);
+	hp_status = read32(snvs_base + SNVS_HPSTATUS);
 
 	hp_status &= (BM_SNVS_HPSTATUS_OTPMK_ZERO |
 			BM_SNVS_HPSTATUS_OTPMK_SYND);