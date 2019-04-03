6627ad1b6466a52d34734f1756b7fed8bc09cfba
Sahil Malhotra <sahil.malhotra@nxp.com>
2019-04-03 17:18:08 +0530

    Enable CAAM hash and descriptor construction.
    Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>

--- core/arch/arm/plat-ls/crypto_conf.mk
+++ core/arch/arm/plat-ls/crypto_conf.mk
@@ -34,7 +34,7 @@ else
 $(call force, CFG_WITH_SOFTWARE_PRNG,y)
 endif
 
-$(call force, CFG_CRYPTO_HASH_HW,n)
+$(call force, CFG_CRYPTO_HASH_HW,y)
 $(call force, CFG_CRYPTO_CIPHER_HW,n)
 
 $(call force, CFG_CRYPTO_CCM_HW,n)
--- core/drivers/caam/caam_hash.c
+++ core/drivers/caam/caam_hash.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /**
- * @copyright 2018 NXP
+ * @copyright 2018-2019 NXP
  *
  * @file    caam_hash.c
  *
@@ -169,12 +169,15 @@ static enum CAAM_Status do_reduce_key(const struct hashalg *alg,
 				const struct caambuf *inkey,
 				struct caambuf *outkey)
 {
+#ifdef	CFG_PHYS_64BIT
+#define KEY_REDUCE_DESC_ENTRIES	10
+#else
 #define KEY_REDUCE_DESC_ENTRIES	8
+#endif
 	enum CAAM_Status retstatus = CAAM_FAILURE;
 
 	struct jr_jobctx jobctx  = {0};
 	descPointer_t desc    = NULL;
-	uint8_t       desclen = 1;
 
 	/* Allocate the job descriptor */
 	desc = caam_alloc_desc(KEY_REDUCE_DESC_ENTRIES);
@@ -183,20 +186,18 @@ static enum CAAM_Status do_reduce_key(const struct hashalg *alg,
 		goto exit_reduce;
 	}
 
-	desc[desclen++] = HASH_INITFINAL(alg->type);
+	desc_init(desc);
+	desc_add_word(desc, DESC_HEADER(0));
+	desc_add_word(desc, HASH_INITFINAL(alg->type));
 
 	/* Load the input key */
-	desc[desclen++] = FIFO_LD_EXT(CLASS_2, MSG, LAST_C2);
-	desc[desclen++] = inkey->paddr;
-	desc[desclen++] = inkey->length;
+	desc_add_word(desc, FIFO_LD_EXT(CLASS_2, MSG, LAST_C2));
+	desc_add_ptr(desc, inkey->paddr);
+	desc_add_word(desc, inkey->length);
 
 	/* Store key reduced */
-	desc[desclen++] = ST_NOIMM(CLASS_2, REG_CTX, outkey->length);
-	desc[desclen++] = outkey->paddr;
-
-	/* Set the descriptor Header with length */
-	desc[0] = DESC_HEADER(desclen);
-
+	desc_add_word(desc, ST_NOIMM(CLASS_2, REG_CTX, outkey->length));
+	desc_add_ptr(desc, outkey->paddr);
 	HASH_DUMPDESC(desc);
 
 	cache_operation(TEE_CACHECLEAN, inkey->data, inkey->length);
@@ -231,7 +232,11 @@ exit_reduce:
  */
 static TEE_Result do_split_key(void *ctx, const uint8_t *ikey, size_t ilen)
 {
+#ifdef	CFG_PHYS_64BIT
+#define KEY_COMPUTE_DESC_ENTRIES	10
+#else
 #define KEY_COMPUTE_DESC_ENTRIES	8
+#endif
 	TEE_Result    ret = TEE_ERROR_GENERIC;
 	enum CAAM_Status retstatus;
 
@@ -245,8 +250,6 @@ static TEE_Result do_split_key(void *ctx, const uint8_t *ikey, size_t ilen)
 
 	struct jr_jobctx jobctx  = {0};
 	descPointer_t    desc     = NULL;
-	uint8_t          desclen  = 1;
-
 	HASH_TRACE("split key length %d", ilen);
 
 	inkey.data   = (uint8_t *)ikey;
@@ -301,20 +304,18 @@ static TEE_Result do_split_key(void *ctx, const uint8_t *ikey, size_t ilen)
 		key.length = inkey.length;
 	}
 
+	desc_init(desc);
+	desc_add_word(desc, DESC_HEADER(0));
 	/* Load either input key or the reduced input key into key register */
-	desc[desclen++] = LD_KEY_PLAIN(CLASS_2, REG, key.length);
-	desc[desclen++] = key.paddr;
+	desc_add_word(desc, LD_KEY_PLAIN(CLASS_2, REG, key.length));
+	desc_add_ptr(desc, key.paddr);
 	/* Split the key */
-	desc[desclen++] = HMAC_INIT_DECRYPT(alg->type);
-	desc[desclen++] = FIFO_LD_IMM(CLASS_2, MSG, LAST_C2, 0);
+	desc_add_word(desc, HMAC_INIT_DECRYPT(alg->type));
+	desc_add_word(desc, FIFO_LD_IMM(CLASS_2, MSG, LAST_C2, 0));
 	/* Store the split key */
-	desc[desclen++] = FIFO_ST(C2_MDHA_SPLIT_KEY_AES_ECB_JKEK,
-					hashdata->key.length);
-	desc[desclen++] = hashdata->key.paddr;
-
-	/* Set the descriptor Header with length */
-	desc[0] = DESC_HEADER(desclen);
-
+	desc_add_word(desc, FIFO_ST(C2_MDHA_SPLIT_KEY_AES_ECB_JKEK,
+					hashdata->key.length));
+	desc_add_ptr(desc, hashdata->key.paddr);
 	HASH_DUMPDESC(desc);
 
 	cache_operation(TEE_CACHECLEAN, key.data, key.length);
@@ -505,7 +506,6 @@ static TEE_Result do_update(void *ctx, const uint8_t *data, size_t len)
 
 	struct jr_jobctx jobctx = {0};
 	descPointer_t    desc;
-	uint8_t          desclen = 1;
 
 	size_t fullSize;
 	size_t size_topost;
@@ -546,28 +546,32 @@ static TEE_Result do_update(void *ctx, const uint8_t *data, size_t len)
 
 	if (size_todo) {
 		desc = hashdata->descriptor;
+		desc_init(desc);
+		desc_add_word(desc, DESC_HEADER(0));
 
 		/* There are blocks to hash - Create the Descriptor */
 		if (hashdata->ctx.length) {
 			HASH_TRACE("Update Operation");
 			/* Algo Operation - Update */
-			desc[desclen++] = HASH_UPDATE(alg->type);
+			desc_add_word(desc, HASH_UPDATE(alg->type));
 			/* Running context to restore */
-			desc[desclen++] = LD_NOIMM(CLASS_2, REG_CTX,
-					hashdata->ctx.length);
-			desc[desclen++] = hashdata->ctx.paddr;
+			desc_add_word(desc, LD_NOIMM(CLASS_2, REG_CTX,
+					hashdata->ctx.length));
+			desc_add_ptr(desc, hashdata->ctx.paddr);
 		} else {
 			HASH_TRACE("Init Operation");
 
 			/* Check if there is a key to load it */
 			if (hashdata->key_type == KEY_PRECOMP) {
 				HASH_TRACE("Insert Key");
-				desc[desclen++] = LD_KEY_SPLIT(
-					hashdata->key.length);
-				desc[desclen++] = hashdata->key.paddr;
+				desc_add_word(desc, LD_KEY_SPLIT(
+					hashdata->key.length));
+				desc_add_ptr(desc,
+					hashdata->key.paddr);
 
 				/* Algo Operation - HMAC Init */
-				desc[desclen++] = HMAC_INIT_PRECOMP(alg->type);
+				desc_add_word(desc,
+					HMAC_INIT_PRECOMP(alg->type));
 
 				/* Clean the Split key */
 				cache_operation(TEE_CACHECLEAN,
@@ -576,7 +580,8 @@ static TEE_Result do_update(void *ctx, const uint8_t *data, size_t len)
 
 			} else {
 				/* Algo Operation - Init */
-				desc[desclen++] = HASH_INIT(alg->type);
+				desc_add_word(desc,
+					HASH_INIT(alg->type));
 			}
 
 			hashdata->ctx.length = alg->size_ctx;
@@ -584,9 +589,10 @@ static TEE_Result do_update(void *ctx, const uint8_t *data, size_t len)
 
 		if (hashdata->blockbuf.filled != 0) {
 			/* Add the temporary buffer */
-			desc[desclen++] = FIFO_LD_EXT(CLASS_2, MSG, NOACTION);
-			desc[desclen++] = hashdata->blockbuf.buf.paddr;
-			desc[desclen++] = hashdata->blockbuf.filled;
+			desc_add_word(desc,
+				FIFO_LD_EXT(CLASS_2, MSG, NOACTION));
+			desc_add_ptr(desc, hashdata->blockbuf.buf.paddr);
+			desc_add_word(desc, hashdata->blockbuf.filled);
 
 			/* Clean the circular buffer data to be loaded */
 			cache_operation(TEE_CACHECLEAN,
@@ -596,20 +602,17 @@ static TEE_Result do_update(void *ctx, const uint8_t *data, size_t len)
 		}
 
 		/* Add the input data multiple of blocksize */
-		desc[desclen++] = FIFO_LD_EXT(CLASS_2, MSG, LAST_C2);
-		desc[desclen++] = paddr_data;
-		desc[desclen++] = size_inmade;
+		desc_add_word(desc, FIFO_LD_EXT(CLASS_2, MSG, LAST_C2));
+		desc_add_ptr(desc, paddr_data);
+		desc_add_word(desc, size_inmade);
 
 		/* Clean the input data to be loaded */
 		cache_operation(TEE_CACHECLEAN, (void *)data, size_inmade);
 
 		/* Save the running context */
-		desc[desclen++] = ST_NOIMM(CLASS_2, REG_CTX,
-					hashdata->ctx.length);
-		desc[desclen++] = hashdata->ctx.paddr;
-
-		/* Set the descriptor Header with length */
-		desc[0] = DESC_HEADER(desclen);
+		desc_add_word(desc, ST_NOIMM(CLASS_2, REG_CTX,
+					hashdata->ctx.length));
+		desc_add_ptr(desc, hashdata->ctx.paddr);
 
 		HASH_DUMPDESC(desc);
 
@@ -679,7 +682,6 @@ static TEE_Result do_final(void *ctx, uint8_t *digest, size_t len)
 
 	struct jr_jobctx jobctx = {0};
 	descPointer_t    desc;
-	uint8_t          desclen = 1;
 
 	int realloc = 0;
 	struct caambuf digest_align = {0};
@@ -718,12 +720,17 @@ static TEE_Result do_final(void *ctx, uint8_t *digest, size_t len)
 				alg->type, (uintptr_t)digest_align.data, len);
 
 	desc = hashdata->descriptor;
+	desc_init(desc);
+
+	/* Set the descriptor Header with length */
+	desc_add_word(desc, DESC_HEADER(0));
 
 	/* Check if there is a key to load it */
 	if (hashdata->key_type == KEY_PRECOMP) {
 		HASH_TRACE("Load key");
-		desc[desclen++] = LD_KEY_SPLIT(hashdata->key.length);
-		desc[desclen++] = hashdata->key.paddr;
+
+		desc_add_word(desc, LD_KEY_SPLIT(hashdata->key.length));
+		desc_add_ptr(desc, hashdata->key.paddr);
 
 		/* Clean Split key */
 		cache_operation(TEE_CACHECLEAN, hashdata->key.data,
@@ -733,15 +740,15 @@ static TEE_Result do_final(void *ctx, uint8_t *digest, size_t len)
 	if (hashdata->ctx.length) {
 		HASH_TRACE("Final Operation");
 
-		if (hashdata->key_type == KEY_PRECOMP)
-			desc[desclen++] = HMAC_FINAL_PRECOMP(alg->type);
-		else
-			desc[desclen++] = HASH_FINAL(alg->type);
-
+		if (hashdata->key_type == KEY_PRECOMP) {
+			desc_add_word(desc, HMAC_FINAL_PRECOMP(alg->type));
+		} else {
+			desc_add_word(desc, HASH_FINAL(alg->type));
+		}
 		/* Running context to restore */
-		desc[desclen++] = LD_NOIMM(CLASS_2, REG_CTX,
-					hashdata->ctx.length);
-		desc[desclen++] = hashdata->ctx.paddr;
+		desc_add_word(desc, LD_NOIMM(CLASS_2, REG_CTX,
+					hashdata->ctx.length));
+		desc_add_ptr(desc, hashdata->ctx.paddr);
 
 		cache_operation(TEE_CACHEINVALIDATE, hashdata->ctx.data,
 						hashdata->ctx.length);
@@ -749,27 +756,25 @@ static TEE_Result do_final(void *ctx, uint8_t *digest, size_t len)
 		hashdata->ctx.length = 0;
 	} else {
 		HASH_TRACE("Init/Final Operation");
-		if (hashdata->key_type == KEY_PRECOMP)
-			desc[desclen++] = HMAC_INITFINAL_PRECOMP(alg->type);
-		else
-			desc[desclen++] = HASH_INITFINAL(alg->type);
+		if (hashdata->key_type == KEY_PRECOMP) {
+			desc_add_word(desc, HMAC_INITFINAL_PRECOMP(alg->type));
+		} else {
+			desc_add_word(desc, HASH_INITFINAL(alg->type));
+		}
 	}
 
 	HASH_DUMPBUF("Temporary Block", hashdata->blockbuf.buf.data,
 					hashdata->blockbuf.filled);
-	desc[desclen++] = FIFO_LD_EXT(CLASS_2, MSG, LAST_C2);
-	desc[desclen++] = hashdata->blockbuf.buf.paddr;
-	desc[desclen++] = hashdata->blockbuf.filled;
+	desc_add_word(desc, FIFO_LD_EXT(CLASS_2, MSG, LAST_C2));
+	desc_add_ptr(desc, hashdata->blockbuf.buf.paddr);
+	desc_add_word(desc, hashdata->blockbuf.filled);
 	cache_operation(TEE_CACHECLEAN, hashdata->blockbuf.buf.data,
 					hashdata->blockbuf.filled);
 	hashdata->blockbuf.filled = 0;
 
 	/* Save the final digest */
-	desc[desclen++] = ST_NOIMM(CLASS_2, REG_CTX, alg->size_digest);
-	desc[desclen++] = digest_align.paddr;
-
-	/* Set the descriptor Header with length */
-	desc[0] = DESC_HEADER(desclen);
+	desc_add_word(desc, ST_NOIMM(CLASS_2, REG_CTX, alg->size_digest));
+	desc_add_ptr(desc, digest_align.paddr);
 
 	HASH_DUMPDESC(desc);
 
