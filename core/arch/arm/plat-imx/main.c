nxp-imx/imx-optee-os

commit dda69050abc968bee6dc2e56825f98d6a2f648b8

TEE-517 core: imx: Add dynamic shared memory configuration
Add dynamic share memory configuration. Enable it for
i.MX 8mscale family except for i.MX 8MP
Enable DEK Blob support, and put a warning if dynamic share
memory is not enabled, as it depends on this feature.

Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>

core/arch/arm/plat-imx/main.c
@@ -124,6 +124,13 @@ register_phys_mem_pgdir(MEM_AREA_IO_SEC,
 			CORE_MMU_PGDIR_SIZE);
 #endif
 
+#ifdef DRAM0_NSEC_SIZE
+register_dynamic_shm(DRAM0_NSEC_BASE, DRAM0_NSEC_SIZE);
+#endif
+#ifdef DRAM1_NSEC_SIZE
+register_dynamic_shm(DRAM1_NSEC_BASE, DRAM1_NSEC_SIZE);
+#endif
+
 const struct thread_handlers *generic_boot_get_handlers(void)
 {
 	return &handlers;

core/arch/arm/plat-imx/platform_config.h
@@ -66,4 +66,21 @@
 #include <config/imx6sll.h>
 #endif
 
+/*
+ * Calculate Non Secure memory region, after Secure memory carved out.
+ * Assumption is memory for TEE is 32M
+ * Those defines are used to register dynamic shared memory
+ * Currently enable only mscale family except 8MP.
+ */
+#if defined(CFG_MX8MQ) || defined(CFG_MX8MM) || defined(CFG_MX8MN)
+#define DRAM0_NSEC_BASE CFG_DRAM_BASE
+#define DRAM0_NSEC_SIZE (CFG_TZDRAM_START - CFG_DRAM_BASE)
+#define DRAM1_NSEC_BASE (CFG_TZDRAM_START + 0x2000000)
+#define DRAM1_NSEC_SIZE (CFG_DDR_SIZE - DRAM1_NSEC_BASE)
+#endif
+
+#if defined(CFG_IMX_DEK_HAB) && !defined(DRAM0_NSEC_SIZE)
+#warning HAB DEK Blob is enabled but it requires dynamic shared memory \
+and this feature is not enabled !!
+#endif
 #endif /*PLATFORM_CONFIG_H*/

core/arch/arm/plat-imx/conf.mk
@@ -469,6 +469,10 @@ CFG_CRYPTO_DRIVER ?= y
 # DRV_DBG_TRACE BIT32(0) // Driver trace
 # DRV_DBG_BUF   BIT32(1) // Driver dump Buffer
 CFG_CRYPTO_DRIVER_DEBUG ?= 0
+# Enable DEK Blob feature by default on i.MX 8mscale family.
+ifneq (,$(filter y, $(CFG_MX8MQ) $(CFG_MX8MM) $(CFG_MX8MN)))
+CFG_IMX_DEK_HAB ?=y
+endif
 else
 $(call force,CFG_CRYPTO_DRIVER,n)
 $(call force,CFG_WITH_SOFTWARE_PRNG,y)

commit 25fee73c10f0cf077b6707256ccc1950c1120472

TEE-525 core: imx: fix i.MX 8mq boot failure
Fix regression introduced by th edynamic shared memory addition.

Fixes: dda69050abc9 ("TEE-517 core: imx: Add dynamic shared memory configuration")
Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>

core/arch/arm/plat-imx/main.c
@@ -126,9 +126,9 @@ register_phys_mem_pgdir(MEM_AREA_IO_SEC,
 
 #ifdef DRAM0_NSEC_SIZE
 register_dynamic_shm(DRAM0_NSEC_BASE, DRAM0_NSEC_SIZE);
-#endif
-#ifdef DRAM1_NSEC_SIZE
+#if DRAM1_NSEC_SIZE
 register_dynamic_shm(DRAM1_NSEC_BASE, DRAM1_NSEC_SIZE);
+#endif
 #endif
 
 const struct thread_handlers *generic_boot_get_handlers(void)

core/arch/arm/plat-imx/platform_config.h
@@ -76,7 +76,7 @@
 #define DRAM0_NSEC_BASE CFG_DRAM_BASE
 #define DRAM0_NSEC_SIZE (CFG_TZDRAM_START - CFG_DRAM_BASE)
 #define DRAM1_NSEC_BASE (CFG_TZDRAM_START + 0x2000000)
-#define DRAM1_NSEC_SIZE (CFG_DDR_SIZE - DRAM1_NSEC_BASE)
+#define DRAM1_NSEC_SIZE ((CFG_DRAM_BASE - DRAM1_NSEC_BASE) + CFG_DDR_SIZE)
 #endif
 
 #if defined(CFG_IMX_DEK_HAB) && !defined(DRAM0_NSEC_SIZE)