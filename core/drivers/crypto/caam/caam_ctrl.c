nxp-imx/imx-optee-os

commit 1bbc5e2da03d008260d310f3cb0877aaa204122d

drivers: implement HUK CAAM driver
   Add the CAAM driver (part of CAAM Blob) to support the
   generation of a Hardware Unique Key based on the device
   efuse.

   This is a preliminary implementation to enable the HUK generation

   Missing in this patch is the i.MX SNVS functions:
    - Check if the device is closed or not
    - Selection of the OTPMK key used to generate the HUK
   Those functions will be implemented in a separated PR
   in which the SNVS driver will be updated and functional

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/drivers/crypto/caam/caam_ctrl.c
@@ -9,16 +9,17 @@
 #include <caam_hal_clk.h>
 #include <caam_hal_ctrl.h>
 #include <caam_hash.h>
+#include <caam_huk.h>
 #include <caam_jr.h>
 #include <caam_pwr.h>
 #include <caam_rng.h>
-#include <caam_utils_mem.h>
+#include <crypto/crypto.h>
 #include <initcall.h>
 #include <kernel/panic.h>
 #include <tee_api_types.h>
 
 /* Crypto driver initialization */
-static TEE_Result crypto_driver_init(void)
+TEE_Result crypto_driver_init(void)
 {
 	TEE_Result retresult = TEE_ERROR_GENERIC;
 	enum caam_status retstatus = CAAM_FAILURE;
@@ -45,6 +46,13 @@ static TEE_Result crypto_driver_init(void)
 
 	/* Initialize the RNG Module */
 	retstatus = caam_rng_init(jrcfg.base);
+	if (retstatus != CAAM_NO_ERROR) {
+		retresult = TEE_ERROR_GENERIC;
+		goto exit_init;
+	}
+
+	/* Initialize the HUK Module */
+	retstatus = caam_huk_init(jrcfg.base);
 	if (retstatus != CAAM_NO_ERROR) {
 		retresult = TEE_ERROR_GENERIC;
 		goto exit_init;
@@ -78,8 +86,6 @@ exit_init:
 
 	return retresult;
 }
-
-driver_init(crypto_driver_init);
 
 /* Crypto driver late initialization to complete on-going CAAM operations */
 static TEE_Result init_caam_late(void)

core/arch/arm/plat-imx/crypto_conf.mk
@@ -5,18 +5,21 @@
 #
 # CAAM Debug Trace
 #
-# DBG_TRACE_HAL  BIT32(0)  // HAL trace
-# DBG_TRACE_CTRL BIT32(1)  // Controller trace
-# DBG_TRACE_MEM  BIT32(2)  // Memory utility trace
-# DBG_TRACE_SGT  BIT32(3)  // Scatter Gather trace
-# DBG_TRACE_PWR  BIT32(4)  // Power trace
-# DBG_TRACE_JR   BIT32(5)  // Job Ring trace
-# DBG_DESC_JR    BIT32(6)  // Job Ring dump descriptor
-# DBG_TRACE_RNG  BIT32(7)  // RNG trace
-# DBG_DESC_RNG   BIT32(8)  // RNG dump descriptor
-# DBG_TRACE_HASH BIT32(9)  // Hash trace
-# DBG_DESC_HASH  BIT32(10) // Hash dump descriptor
-# DBG_BUF_HASH   BIT32(11) // Hash dump Buffer
+# DBG_TRACE_HAL    BIT32(0)  // HAL trace
+# DBG_TRACE_CTRL   BIT32(1)  // Controller trace
+# DBG_TRACE_MEM    BIT32(2)  // Memory utility trace
+# DBG_TRACE_SGT    BIT32(3)  // Scatter Gather trace
+# DBG_TRACE_PWR    BIT32(4)  // Power trace
+# DBG_TRACE_JR     BIT32(5)  // Job Ring trace
+# DBG_DESC_JR      BIT32(6)  // Job Ring dump descriptor
+# DBG_TRACE_RNG    BIT32(7)  // RNG trace
+# DBG_DESC_RNG     BIT32(8)  // RNG dump descriptor
+# DBG_TRACE_HASH   BIT32(9)  // Hash trace
+# DBG_DESC_HASH    BIT32(10) // Hash dump descriptor
+# DBG_BUF_HASH     BIT32(11) // Hash dump Buffer
+# DBG_TRACE_BLOB   BIT32(12) // BLOB trace
+# DBG_DESC_BLOB    BIT32(13) // BLOB dump descriptor
+# DBG_BUF_BLOB     BIT32(14) // BLOB dump Buffer
 CFG_CAAM_DBG ?= 0x2
 
 #
@@ -28,6 +31,11 @@ $(call force, CFG_JR_BLOCK_SIZE,0x1000)
 
 $(call force, CFG_JR_INDEX,0)  # Default JR index used
 $(call force, CFG_JR_INT,137)  # Default JR IT Number (105 + 32) = 137
+
+#
+# Enable HUK CAAM Generation
+#
+CFG_NXP_CAAM_HUK_DRV ?= y
 
 #
 # Configuration of the Crypto Driver

core/drivers/crypto/caam/blob/caam_huk.c
@@ -1,0 +1,189 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2019 NXP
+ *
+ * Brief   CAAM Generation of a Hardware Unique Key.
+ *         Use the CAAM Blob Verify Master Key operation
+ */
+#include <caam_common.h>
+#include <caam_huk.h>
+#include <caam_jr.h>
+#include <caam_utils_mem.h>
+#include <caam_utils_status.h>
+#include <kernel/tee_common_otp.h>
+#include <mm/core_memprot.h>
+#include <tee/cache.h>
+#include <utee_defines.h>
+#include <string.h>
+
+/* Blob Key Modifier size in bytes */
+#define BLOB_KEY_MODIFIER_SIZE 16
+
+/* Blob Key (BKEK) size in bytes */
+#define BLOB_BKEK_SIZE 32
+
+/*
+ * HUK State
+ */
+enum huk_state {
+	CAAM_HUK_EMPTY,     /* Initialized but not generated */
+	CAAM_HUK_GENERATED, /* Generated */
+	CAAM_HUK_ERROR      /* Error can not be generated */
+};
+
+/*
+ * HUK module private data
+ */
+struct huk_privdata {
+	enum huk_state state;  /* HUK state flag */
+	struct caambuf huk;    /* HUK buffer */
+};
+
+static struct huk_privdata *huk_privdata;
+
+/*
+ * Verify Master Key (derives a BKEK from the secret master key).
+ * This BKEK is not the same used during normal blob encapsulation.
+ *
+ * @outkey  [out] Output key generated
+ */
+static TEE_Result caam_master_key_verif(struct caambuf *outkey)
+{
+	TEE_Result ret = TEE_ERROR_GENERIC;
+	enum caam_status retstatus = CAAM_FAILURE;
+	paddr_t paddr_keymod = 0;
+	uint8_t keymod_buf[BLOB_KEY_MODIFIER_SIZE] = {};
+	struct caam_jobctx jobctx = {};
+	uint32_t *desc = NULL;
+
+#ifdef CFG_CAAM_64BIT
+#define BLOB_MASTER_KEY_VERIF 9
+#else
+#define BLOB_MASTER_KEY_VERIF 7
+#endif
+	/* Check if parameters are correct */
+	if (!outkey)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	if (!outkey->data)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	/* Get the physical address of the key modifier */
+	paddr_keymod = virt_to_phys(keymod_buf);
+	if (!paddr_keymod)
+		return TEE_ERROR_GENERIC;
+
+	/* Allocate the job descriptor */
+	desc = caam_calloc_desc(BLOB_MASTER_KEY_VERIF);
+	if (!desc) {
+		ret = TEE_ERROR_OUT_OF_MEMORY;
+		goto exit_masterkey;
+	}
+
+	/*
+	 * Create the Master Key Verification descriptor
+	 */
+	caam_desc_init(desc);
+	caam_desc_add_word(desc, DESC_HEADER(0));
+
+	/* Load the key modifier */
+	caam_desc_add_word(desc,
+			   LD_NOIMM(CLASS_2, REG_KEY, BLOB_KEY_MODIFIER_SIZE));
+	caam_desc_add_ptr(desc, paddr_keymod);
+
+	/* Output key storage */
+	caam_desc_add_word(desc, SEQ_OUT_PTR(BLOB_BKEK_SIZE));
+	caam_desc_add_ptr(desc, outkey->paddr);
+
+	/* Blob Master key verification operation */
+	caam_desc_add_word(desc, BLOB_MSTR_KEY);
+
+	BLOB_DUMPDESC(desc);
+
+	cache_operation(TEE_CACHECLEAN, keymod_buf, BLOB_KEY_MODIFIER_SIZE);
+	cache_operation(TEE_CACHEFLUSH, outkey->data, outkey->length);
+
+	jobctx.desc = desc;
+	retstatus = caam_jr_enqueue(&jobctx, NULL);
+
+	if (retstatus == CAAM_NO_ERROR) {
+		cache_operation(TEE_CACHEINVALIDATE, outkey->data,
+				outkey->length);
+		BLOB_DUMPBUF("Master Key", outkey->data, outkey->length);
+
+		ret = TEE_SUCCESS;
+	} else {
+		BLOB_TRACE("CAAM Status 0x%08" PRIx32, jobctx.status);
+		ret = job_status_to_tee_result(jobctx.status);
+	}
+
+exit_masterkey:
+	caam_free_desc(&desc);
+
+	return ret;
+}
+
+/*
+ * Return a HW unique key value.
+ * On i.MX device, return a derivation of the Master Key
+ * by calling the CAAM Blob master key verification
+ * operation using a key modifier corresponding of the
+ * first 16 bytes of the Die ID
+ *
+ * @hwhuk  [out] HW Unique key
+ */
+TEE_Result tee_otp_get_hw_unique_key(struct tee_hw_unique_key *hwhuk)
+{
+	TEE_Result ret = TEE_ERROR_GENERIC;
+
+	/* Initialize the HUK value */
+	memset(hwhuk->data, 0, sizeof(hwhuk->data));
+
+	if (!huk_privdata)
+		return ret;
+
+	if (huk_privdata->state == CAAM_HUK_EMPTY) {
+		ret = caam_master_key_verif(&huk_privdata->huk);
+		if (ret == TEE_SUCCESS)
+			huk_privdata->state = CAAM_HUK_GENERATED;
+	}
+
+	if (huk_privdata->state == CAAM_HUK_GENERATED) {
+		memcpy(hwhuk->data, huk_privdata->huk.data,
+		       MIN(sizeof(hwhuk->data), huk_privdata->huk.length));
+	}
+
+	/*
+	 * TODO Add check if device type
+	 * If there is an error during the Master key derivation,
+	 * let the device booting with a 0's key
+	 */
+	if (ret != TEE_SUCCESS)
+		ret = TEE_SUCCESS;
+
+	return ret;
+}
+
+/*
+ * Initialize the HUK module
+ *
+ * @ctrl_addr   Controller base address
+ */
+enum caam_status caam_huk_init(vaddr_t ctrl_addr __unused)
+{
+	enum caam_status ret = CAAM_FAILURE;
+
+	/* Allocate the Private data */
+	huk_privdata = caam_calloc(sizeof(struct huk_privdata));
+	if (!huk_privdata)
+		return CAAM_OUT_MEMORY;
+
+	/* Allocate the HUK buffer */
+	ret = caam_calloc_align_buf(&huk_privdata->huk, BLOB_BKEK_SIZE);
+	if (ret == CAAM_NO_ERROR)
+		huk_privdata->state = CAAM_HUK_EMPTY;
+	else
+		huk_privdata->state = CAAM_HUK_ERROR;
+
+	return ret;
+}

core/drivers/crypto/caam/blob/sub.mk
@@ -1,0 +1,3 @@
+incdirs-y += ../include
+
+srcs-$(CFG_NXP_CAAM_HUK_DRV) += caam_huk.c

core/drivers/crypto/caam/include/caam_huk.h
@@ -1,0 +1,27 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2019 NXP
+ *
+ * Brief   CAAM HUK manager header
+ */
+#ifndef __CAAM_HUK_H__
+#define __CAAM_HUK_H__
+
+#include <caam_common.h>
+#include <tee_api_types.h>
+
+#ifdef CFG_NXP_CAAM_HUK_DRV
+/*
+ * Initialize the HUK module
+ *
+ * @ctrl_addr   Controller base address
+ */
+enum caam_status caam_huk_init(vaddr_t ctrl_addr);
+#else
+static inline enum caam_status caam_huk_init(vaddr_t ctrl_addr __unused)
+{
+	return CAAM_NO_ERROR;
+}
+#endif /* CFG_NXP_CAAM_HUK_DRV */
+
+#endif /* __CAAM_HUK_H__ */

core/drivers/crypto/caam/include/caam_trace.h
@@ -17,18 +17,21 @@
  * The CFG_CAAM_DBG is a bit mask 32 bits value defined
  * as followed:
  */
-#define DBG_TRACE_HAL    BIT32(0)  /* HAL trace */
-#define DBG_TRACE_CTRL   BIT32(1)  /* Controller trace */
-#define DBG_TRACE_MEM    BIT32(2)  /* Memory utility trace */
-#define DBG_TRACE_SGT    BIT32(3)  /* Scatter Gather trace */
-#define DBG_TRACE_PWR    BIT32(4)  /* Power trace */
-#define DBG_TRACE_JR     BIT32(5)  /* Job Ring trace */
-#define DBG_DESC_JR      BIT32(6)  /* Job Ring dump descriptor */
-#define DBG_TRACE_RNG    BIT32(7)  /* RNG trace */
-#define DBG_DESC_RNG     BIT32(8)  /* RNG dump descriptor */
-#define DBG_TRACE_HASH   BIT32(9)  /* Hash trace */
-#define DBG_DESC_HASH    BIT32(10) /* Hash dump descriptor */
-#define DBG_BUF_HASH     BIT32(11) /* Hash dump Buffer */
+#define DBG_TRACE_HAL     BIT32(0)  /* HAL trace */
+#define DBG_TRACE_CTRL    BIT32(1)  /* Controller trace */
+#define DBG_TRACE_MEM     BIT32(2)  /* Memory utility trace */
+#define DBG_TRACE_SGT     BIT32(3)  /* Scatter Gather trace */
+#define DBG_TRACE_PWR     BIT32(4)  /* Power trace */
+#define DBG_TRACE_JR      BIT32(5)  /* Job Ring trace */
+#define DBG_DESC_JR       BIT32(6)  /* Job Ring dump descriptor */
+#define DBG_TRACE_RNG     BIT32(7)  /* RNG trace */
+#define DBG_DESC_RNG      BIT32(8)  /* RNG dump descriptor */
+#define DBG_TRACE_HASH    BIT32(9)  /* Hash trace */
+#define DBG_DESC_HASH     BIT32(10) /* Hash dump descriptor */
+#define DBG_BUF_HASH      BIT32(11) /* Hash dump Buffer */
+#define DBG_TRACE_BLOB    BIT32(12) /* BLOB trace */
+#define DBG_DESC_BLOB     BIT32(13) /* BLOB dump descriptor */
+#define DBG_BUF_BLOB      BIT32(14) /* BLOB dump Buffer */
 
 /* HAL */
 #if (CFG_CAAM_DBG & DBG_TRACE_HAL)
@@ -120,6 +123,29 @@
 #define HASH_TRACE(...)
 #define HASH_DUMPDESC(desc)
 #define HASH_DUMPBUF(...)
+#endif
+
+/* BLOB */
+#if (CFG_CAAM_DBG & DBG_TRACE_BLOB)
+#define BLOB_TRACE DRV_TRACE
+#if (CFG_CAAM_DBG & DBG_DESC_BLOB)
+#define BLOB_DUMPDESC(desc)                                                    \
+	do {                                                                   \
+		BLOB_TRACE("BLOB Descriptor");                                 \
+		DRV_DUMPDESC(desc);                                            \
+	} while (0)
+#else
+#define BLOB_DUMPDESC(desc)
+#endif
+#if (CFG_CAAM_DBG & DBG_BUF_BLOB)
+#define BLOB_DUMPBUF DRV_DUMPBUF
+#else
+#define BLOB_DUMPBUF(...)
+#endif
+#else
+#define BLOB_TRACE(...)
+#define BLOB_DUMPDESC(desc)
+#define BLOB_DUMPBUF(...)
 #endif
 
 #if (TRACE_LEVEL >= TRACE_DEBUG)

core/drivers/crypto/caam/sub.mk
@@ -2,6 +2,7 @@ incdirs-y += include
 
 subdirs-y += hal
 subdirs-y += utils
+subdirs-y += blob
 
 srcs-y += caam_pwr.c
 srcs-y += caam_ctrl.c

commit c966487861a3e74306d6612aa851d36536838268

TEE-543 drivers: caam: move NS job ring assignment later in the boot
When optee-os is compiled with CFG_CRYPTO_DRIVER=n, all the job rings
are assign to NS world. However optee does need a Job ring to instanciate
HUK.
This commit moves job ring assignements later in the boot process.

Fixes: 1bbc5e2da03d ("drivers: implement HUK CAAM driver")
Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>

core/drivers/crypto/caam/caam_ctrl.c
@@ -22,12 +22,13 @@
 #include <kernel/panic.h>
 #include <tee_api_types.h>
 
+static struct caam_jrcfg jrcfg;
+
 /* Crypto driver initialization */
 TEE_Result crypto_driver_init(void)
 {
 	TEE_Result retresult = TEE_ERROR_GENERIC;
 	enum caam_status retstatus = CAAM_FAILURE;
-	struct caam_jrcfg jrcfg = {};
 
 	/* Enable the CAAM Clock */
 	caam_hal_clk_enable(true);
@@ -142,15 +143,6 @@ TEE_Result crypto_driver_init(void)
 	/* Everything is OK, register the Power Management handler */
 	caam_pwr_init();
 
-	/*
-	 * Configure Job Rings to NS World
-	 * If the Driver Crypto is not used CFG_NXP_CAAM_RUNTIME_JR is not
-	 * enable, hence relax the JR used for the CAAM configuration to
-	 * the Non-Secure
-	 */
-	if (jrcfg.base)
-		caam_hal_cfg_setup_nsjobring(&jrcfg);
-
 	retresult = TEE_SUCCESS;
 exit_init:
 	if (retresult != TEE_SUCCESS) {
@@ -172,6 +164,15 @@ static TEE_Result init_caam_late(void)
 		EMSG("CAAM initialization failed");
 		panic();
 	}
+
+	/*
+	 * Configure Job Rings to NS World
+	 * If the Driver Crypto is not used CFG_NXP_CAAM_RUNTIME_JR is not
+	 * enable, hence relax the JR used for the CAAM configuration to
+	 * the Non-Secure
+	 */
+	if (jrcfg.base)
+		caam_hal_cfg_setup_nsjobring(&jrcfg);
 
 	return TEE_SUCCESS;
 }