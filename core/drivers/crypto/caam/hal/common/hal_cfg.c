nxp-imx/imx-optee-os

commit 65d11b9078bed0d1fe2908b473a55e038db2280c

TEE-511 drivers: caam: Fix suspend/resume
Add missing save/restore of the Secure JR configuration (all devices).
On i.MX6Q/D fix the job ring resume to reconfigure the Software JR queues
and re-instantiate the RNG if needed.

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/drivers/crypto/caam/hal/common/hal_cfg.c
@@ -91,9 +91,13 @@ void caam_hal_cfg_setup_nsjobring(struct caam_jrcfg *jrcfg)
 		/*
 		 * When the Cryptographic driver is enabled, keep the
 		 * Secure Job Ring don't release it.
+		 * But save the configuration to restore it when
+		 * device reset after suspend.
 		 */
-		if (jr_offset == jrcfg->offset)
+		if (jr_offset == jrcfg->offset) {
+			caam_hal_jr_prepare_backup(jrcfg->base, jr_offset);
 			continue;
+		}
 #endif
 		status = caam_hal_jr_setowner(jrcfg->base, jr_offset,
 					      JROWN_ARM_NS);

core/drivers/crypto/caam/caam_jr.c
@@ -629,9 +629,6 @@ enum caam_status caam_jr_flush(void)
 void caam_jr_resume(uint32_t pm_hint)
 {
 	if (pm_hint == PM_HINT_CONTEXT_STATE) {
-#if !(defined(CFG_MX6DL) || defined(CFG_MX6D) || defined(CFG_MX6Q) ||          \
-	defined(CFG_MX6QP))
-
 #ifndef CFG_NXP_CAAM_RUNTIME_JR
 		/*
 		 * In case the CAAM is not used the JR used to
@@ -661,7 +658,6 @@ void caam_jr_resume(uint32_t pm_hint)
 		caam_hal_jr_setowner(jr_privdata->ctrladdr,
 				     jr_privdata->jroffset, JROWN_ARM_NS);
 #endif /* CFG_NXP_CAAM_RUNTIME_JR */
-#endif /* !(CFG_MX6DL || CFG_MX6D || CFG_MX6Q || CFG_MX6QP) */
 	} else {
 		caam_hal_jr_resume(jr_privdata->baseaddr);
 	}

commit e59ada84caababcd024545a44022f608d78a03b7

MLK-23726 drivers: caam: Fix CAAM resume function for i.MX 6SX
i.MX6SX low power mode removes CAAM power. The order in which the
registers are restored is important for JR configuration.
The MID registers need to be configured first in order for the other
ones to be configured correctly. This commit makes a call to the backup
function to save the MID settings for the JR used by OP-TEE first.

Fixes: 65d11b9078be ("TEE-511 drivers: caam: Fix suspend/resume")
Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>

core/drivers/crypto/caam/hal/common/hal_cfg.c
@@ -72,7 +72,9 @@ enum caam_status caam_hal_cfg_get_conf(struct caam_jrcfg *jrcfg)
 	jrcfg->nb_jobs = NB_JOBS_QUEUE;
 
 	retstatus = CAAM_NO_ERROR;
-
+#ifdef CFG_NXP_CAAM_RUNTIME_JR
+	caam_hal_jr_prepare_backup(jrcfg->base, jrcfg->offset);
+#endif
 exit_get_conf:
 	HAL_TRACE("HAL CFG Get CAAM config ret (0x%x)\n", retstatus);
 	return retstatus;
@@ -91,13 +93,9 @@ void caam_hal_cfg_common_setup_nsjobring(struct caam_jrcfg *jrcfg)
 		/*
 		 * When the Cryptographic driver is enabled, keep the
 		 * Secure Job Ring don't release it.
-		 * But save the configuration to restore it when
-		 * device reset after suspend.
 		 */
-		if (jr_offset == jrcfg->offset) {
-			caam_hal_jr_prepare_backup(jrcfg->base, jr_offset);
+		if (jr_offset == jrcfg->offset)
 			continue;
-		}
 #endif
 		status = caam_hal_jr_setowner(jrcfg->base, jr_offset,
 					      JROWN_ARM_NS);