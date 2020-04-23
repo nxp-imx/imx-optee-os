nxp-imx/imx-optee-os

commit 96364b5f91886f130d397646af6798405c96caf8

TEE-510 drivers: caam: add HAL for imx8qm and imx8qxp
Add CAAM HAL for i.MX8QM and i.MX8QXP socs.
SCU drivers is used for some init functions.

Signed-off-by: Remi Koman <remi.koman@nxp.com>

core/arch/arm/plat-imx/conf.mk
@@ -184,7 +184,6 @@ $(call force,CFG_IMX_SNVS,n)
 CFG_IMX_LPUART ?= y
 CFG_DRAM_BASE ?= 0x80000000
 CFG_TEE_CORE_NB_CORE ?= 6
-$(call force,CFG_NXP_CAAM,n)
 $(call force,CFG_TZC380,n)
 $(call force,CFG_IMX_SC,y)
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx8qx-flavorlist)))
@@ -194,7 +193,6 @@ $(call force,CFG_IMX_SNVS,n)
 CFG_IMX_LPUART ?= y
 CFG_DRAM_BASE ?= 0x80000000
 CFG_TEE_CORE_NB_CORE ?= 4
-$(call force,CFG_NXP_CAAM,n)
 $(call force,CFG_TZC380,n)
 $(call force,CFG_IMX_SC,y)
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx8dxl-flavorlist)))
@@ -473,11 +471,6 @@ CFG_MMAP_REGIONS ?= 24
 # Almost all platforms include CAAM HW Modules, except the
 # ones forced to be disabled
 CFG_NXP_CAAM ?= y
-
-# Disable CAAM driver for MX8Q
-ifneq (,$(filter y, $(CFG_MX8QM) $(CFG_MX8QX)))
-CFG_NXP_CAAM = n
-endif
 
 ifeq ($(CFG_NXP_CAAM),y)
 # If NXP CAAM driver is enable for MX8Q, SC driver init is done by CAAM driver

core/arch/arm/plat-imx/crypto_conf.mk
@@ -28,30 +28,39 @@ CFG_DBG_CAAM_TRACE ?= 0x2
 CFG_DBG_CAAM_DESC ?= 0x0
 CFG_DBG_CAAM_BUF ?= 0x0
 
-#
+ifeq ($(filter y, $(CFG_MX8QM) $(CFG_MX8QX)),y)
 # Due to the CAAM DMA behaviour on iMX8QM & iMX8QX, 4 bytes need to be add to
 # the buffer size when aligned memory allocation is done
-#
-ifeq ($(filter y, $(CFG_MX8QM) $(CFG_MX8QX)),y)
 $(call force, CFG_CAAM_SIZE_ALIGN,4)
-else
+#
+# CAAM Job Ring configuration
+#  - Normal boot settings
+#  - HAB support boot settings
+#
+$(call force, CFG_JR_BLOCK_SIZE,0x10000)
+$(call force,CFG_JR_INDEX,2)  # Job Ring 2
+$(call force,CFG_JR_INT,485)  # CAAM_INT2 = 485
+else ifneq (,$(filter y, $(CFG_MX8MM) $(CFG_MX8MN) $(CFG_MX8MP) $(CFG_MX8MQ)))
 $(call force, CFG_CAAM_SIZE_ALIGN,1)
-endif
-
 #
 # CAAM Job Ring configuration
 #  - Normal boot settings
 #  - HAB support boot settings
 #
 $(call force, CFG_JR_BLOCK_SIZE,0x1000)
-
-ifneq (,$(filter y, $(CFG_MX8MM) $(CFG_MX8MN) $(CFG_MX8MP) $(CFG_MX8MQ)))
 # On i.MX8 mscale devices OP-TEE runs before u-boot.
 # HAB can still be reuse in u-boot to authenticate linux
 # Use another Job ring other than the one used by HAB.
 $(call force, CFG_JR_INDEX,2)  # Default JR index used
 $(call force, CFG_JR_INT,146)  # Default JR IT Number (114 + 32) = 146
 else
+$(call force, CFG_CAAM_SIZE_ALIGN,1)
+#
+# CAAM Job Ring configuration
+#  - Normal boot settings
+#  - HAB support boot settings
+#
+$(call force, CFG_JR_BLOCK_SIZE,0x1000)
 $(call force, CFG_JR_INDEX,0)  # Default JR index used
 $(call force, CFG_JR_INT,137)  # Default JR IT Number (105 + 32) = 137
 endif
@@ -104,10 +113,12 @@ ifneq ($(filter y, $(CFG_MX6QP) $(CFG_MX6Q) $(CFG_MX6D) $(CFG_MX6DL) \
 	$(CFG_MX6S) $(CFG_MX6SX) $(CFG_MX7ULP)), y)
 $(eval $(call cryphw-enable-drv-hw, ECC))
 $(eval $(call cryphw-enable-drv-hw, RSA))
-$(eval $(call cryphw-enable-drv-hw, MP))
 $(eval $(call cryphw-enable-drv-hw, DH))
 $(eval $(call cryphw-enable-drv-hw, DSA))
+ifneq ($(filter y, $(CFG_MX8QM) $(CFG_MX8QX)), y)
+$(eval $(call cryphw-enable-drv-hw, MP))
 CFG_PTA_MP ?= y
+endif
 endif
 
 $(call force, CFG_NXP_CAAM_ACIPHER_DRV, $(call cryphw-one-enabled, \

core/arch/arm/plat-imx/registers/imx8q.h
@@ -12,7 +12,8 @@
 #define UART1_BASE	   0x5a070000
 #define UART2_BASE	   0x5a080000
 #define UART3_BASE	   0x5a090000
-#define CAAM_BASE	   0x30900000
 #define SC_IPC_BASE_SECURE 0x5d1b0000
+#define CAAM_BASE	   0x31400000
+#define SECMEM_BASE	   0x31800000
 
 #endif /* __IMX8Q_H__ */

core/drivers/crypto/caam/acipher/caam_dh.c
@@ -514,16 +514,12 @@ static struct drvcrypt_dh driver_dh = {
 	.shared_secret = do_shared_secret,
 };
 
-/*
- * Initialize the DH module
- *
- * @ctrl_addr   Controller base address
- */
-enum caam_status caam_dh_init(vaddr_t ctrl_addr)
+enum caam_status caam_dh_init(struct caam_jrcfg *caam_jrcfg)
 {
 	enum caam_status retstatus = CAAM_FAILURE;
+	vaddr_t jr_base = caam_jrcfg->base + caam_jrcfg->offset;
 
-	if (caam_hal_ctrl_pknum(ctrl_addr))
+	if (caam_hal_ctrl_pknum(jr_base))
 		if (drvcrypt_register_dh(&driver_dh) == TEE_SUCCESS)
 			retstatus = CAAM_NO_ERROR;
 

core/drivers/crypto/caam/acipher/caam_ecc.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright 2018-2019 NXP
+ * Copyright 2018-2020 NXP
  *
  * Implementation of ECC functions
  */
@@ -878,11 +878,12 @@ static struct drvcrypt_ecc driver_ecc = {
 	.shared_secret = &do_shared_secret,
 };
 
-enum caam_status caam_ecc_init(vaddr_t ctrl_addr)
+enum caam_status caam_ecc_init(struct caam_jrcfg *caam_jrcfg)
 {
 	enum caam_status retstatus = CAAM_FAILURE;
+	vaddr_t jr_base = caam_jrcfg->base + caam_jrcfg->offset;
 
-	if (caam_hal_ctrl_pknum(ctrl_addr))
+	if (caam_hal_ctrl_pknum(jr_base))
 		if (drvcrypt_register_ecc(&driver_ecc) == TEE_SUCCESS)
 			retstatus = CAAM_NO_ERROR;
 

core/drivers/crypto/caam/acipher/caam_math.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright 2018-2019 NXP
+ * Copyright 2018-2020 NXP
  *
  * Brief   CAAM Mathematical Operation manager.
  *         Implementation of Mathematical operation using CAAM's MATH function
@@ -205,11 +205,12 @@ static struct drvcrypt_math driver_math = {
 	.xor_mod_n = &do_xor_mod_n,
 };
 
-enum caam_status caam_math_init(vaddr_t ctrl_addr __unused)
+enum caam_status caam_math_init(struct caam_jrcfg *caam_jrcfg)
 {
 	enum caam_status retstatus = CAAM_FAILURE;
+	vaddr_t jr_base = caam_jrcfg->base + caam_jrcfg->offset;
 
-	if (caam_hal_ctrl_pknum(ctrl_addr))
+	if (caam_hal_ctrl_pknum(jr_base))
 		if (!drvcrypt_register_math(&driver_math))
 			retstatus = CAAM_NO_ERROR;
 

core/drivers/crypto/caam/acipher/caam_rsa.c
@@ -1623,12 +1623,13 @@ static struct drvcrypt_rsa driver_rsa = {
 	.ssa_verify = NULL,
 };
 
-enum caam_status caam_rsa_init(vaddr_t ctrl_addr)
+enum caam_status caam_rsa_init(struct caam_jrcfg *caam_jrcfg)
 {
 	enum caam_status retstatus = CAAM_FAILURE;
+	vaddr_t jr_base = caam_jrcfg->base + caam_jrcfg->offset;
 
-	if (caam_hal_ctrl_pknum(ctrl_addr)) {
-		caam_era = caam_hal_ctrl_era(ctrl_addr);
+	if (caam_hal_ctrl_pknum(jr_base)) {
+		caam_era = caam_hal_ctrl_era(jr_base);
 		RSA_TRACE("CAAM Era %d", caam_era);
 
 		if (!drvcrypt_register_rsa(&driver_rsa))

core/drivers/crypto/caam/caam_ctrl.c
@@ -77,14 +77,14 @@ TEE_Result crypto_driver_init(void)
 	}
 
 	/* Initialize the Hash Module */
-	retstatus = caam_hash_init(jrcfg.base);
+	retstatus = caam_hash_init(&jrcfg);
 	if (retstatus != CAAM_NO_ERROR) {
 		retresult = TEE_ERROR_GENERIC;
 		goto exit_init;
 	}
 
 	/* Initialize the Hmac Module */
-	retstatus = caam_hmac_init(jrcfg.base);
+	retstatus = caam_hmac_init(&jrcfg);
 	if (retstatus != CAAM_NO_ERROR) {
 		retresult = TEE_ERROR_GENERIC;
 		goto exit_init;
@@ -105,28 +105,28 @@ TEE_Result crypto_driver_init(void)
 	}
 
 	/* Initialize the MATH Module */
-	retstatus = caam_math_init(jrcfg.base);
+	retstatus = caam_math_init(&jrcfg);
 	if (retstatus != CAAM_NO_ERROR) {
 		retresult = TEE_ERROR_GENERIC;
 		goto exit_init;
 	}
 
 	/* Initialize the ECC Module */
-	retstatus = caam_ecc_init(jrcfg.base);
+	retstatus = caam_ecc_init(&jrcfg);
 	if (retstatus != CAAM_NO_ERROR) {
 		retresult = TEE_ERROR_GENERIC;
 		goto exit_init;
 	}
 
 	/* Initialize the RSA Module */
-	retstatus = caam_rsa_init(jrcfg.base);
+	retstatus = caam_rsa_init(&jrcfg);
 	if (retstatus != CAAM_NO_ERROR) {
 		retresult = TEE_ERROR_GENERIC;
 		goto exit_init;
 	}
 
 	/* Initialize the DH Module */
-	retstatus = caam_dh_init(jrcfg.base);
+	retstatus = caam_dh_init(&jrcfg);
 	if (retstatus != CAAM_NO_ERROR) {
 		retresult = TEE_ERROR_GENERIC;
 		goto exit_init;

commit 581b239987d1d72f2ce8686c8064a352049e0a92

TEE-547 drivers: caam: Use last JR for i.MX 8QM and 8QXP
Use last available CAAM Job Ring for i.MX 8 with a SECO
subsystem.

Fixes: 96364b5f9188 ("TEE-510 drivers: caam: add HAL for imx8qm and imx8qxp")
Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>

core/arch/arm/plat-imx/conf.mk
@@ -479,6 +479,10 @@ ifeq ($(CFG_NXP_CAAM),y)
 # If NXP CAAM driver is enable for MX8Q, SC driver init is done by CAAM driver
 ifeq ($(filter y, $(CFG_MX8QM) $(CFG_MX8QX)), y)
 $(call force,CFG_IMX_SC_EXTERN_INIT,y)
+# force CFG_CRYPTO_DRIVER
+# it is not possible with the current implementation
+# to have the CFG_CRYPTO_DRIVER set to n
+$(call force,CFG_CRYPTO_DRIVER,y)
 endif
 
 # As NXP CAAM Driver is enabled, disable the small local CAAM driver

core/arch/arm/plat-imx/crypto_conf.mk
@@ -44,8 +44,8 @@ $(call force, CFG_CAAM_SIZE_ALIGN,4)
 #  - HAB support boot settings
 #
 $(call force, CFG_JR_BLOCK_SIZE,0x10000)
-$(call force,CFG_JR_INDEX,2)  # Job Ring 2
-$(call force,CFG_JR_INT,485)  # CAAM_INT2 = 485
+$(call force,CFG_JR_INDEX,3)  # Job Ring 3
+$(call force,CFG_JR_INT,486)  # CAAM_INT3 = 486
 else ifneq (,$(filter y, $(CFG_MX8MM) $(CFG_MX8MN) $(CFG_MX8MP) $(CFG_MX8MQ)))
 $(call force, CFG_CAAM_SIZE_ALIGN,1)
 #