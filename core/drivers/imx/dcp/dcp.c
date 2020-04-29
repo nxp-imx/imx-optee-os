fa28e265dc8a092c9c03d4f4b74af1347a9baf78
Clement Faure <clement.faure@nxp.com>
2020-04-29 10:47:27 +0200

    TEE-589 drivers: dcp: add CFG_IMX_DCP_NSEC flag
    The OPTEE driver was disabling non secure access to DCP.
    
    Consequently, the Linux Kernel DCP driver was disable if the OPTEE
    driver was running.
    
    The DCP access from the non-secure world must be restored for the
    following reasons:
     * Linux crypto tests must test the Linux DCP driver.
     * RNG tests check if the DCP clock is enabled. If the Linux kernel DCP
     driver is disabled, the clock is probed as disabled.
    
    Please be aware that flag CFG_IMX_DCP_NSEC is UNSAFE! Since DCP used
    for HUK generation is non accessible from non-secure world.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>

--- core/drivers/imx/dcp/dcp.c
+++ core/drivers/imx/dcp/dcp.c
@@ -688,10 +688,12 @@ static TEE_Result dcp_pbase(paddr_t *base)
 	if (_fdt_get_status(fdt, node) == DT_STATUS_DISABLED)
 		return TEE_ERROR_ITEM_NOT_FOUND;
 
-	/* Force secure-status = "okay" and status="disabled" */
-	if (dt_enable_secure_status(fdt, node)) {
-		EMSG("Not able to set DCP Control DTB entry secure");
-		return TEE_ERROR_NOT_SUPPORTED;
+	if (!IS_ENABLED(CFG_IMX_DCP_NSEC)) {
+		/* Force secure-status = "okay" and status="disabled" */
+		if (dt_enable_secure_status(fdt, node)) {
+			EMSG("Not able to set DCP Control DTB entry secure");
+			return TEE_ERROR_NOT_SUPPORTED;
+		}
 	}
 
 	*base = _fdt_reg_base_address(fdt, node);
