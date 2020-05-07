OP-TEE/optee_os

commit 4dac83288b60ece849f809d8035d2acc297d58b2

core: imx: add imx6sll evk board support
Add i.MX6SLL EVK board support.
Add mx6sllevk platform flavor.

Signed-off-by: Bai Ping <ping.bai@nxp.com>
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/arch/arm/plat-imx/imx-common.c
@@ -48,6 +48,11 @@ bool soc_is_imx6sl(void)
 	return imx_soc_type() == SOC_MX6SL;
 }
 
+bool soc_is_imx6sll(void)
+{
+	return imx_soc_type() == SOC_MX6SLL;
+}
+
 bool soc_is_imx6sx(void)
 {
 	return imx_soc_type() == SOC_MX6SX;

.shippable.yml
@@ -63,6 +63,7 @@ build:
     - _make PLATFORM=imx-mx6ul9x9evk
     - _make PLATFORM=imx-mx6ullevk
     - _make PLATFORM=imx-mx6slevk
+    - _make PLATFORM=imx-mx6sllevk
     - _make PLATFORM=imx-mx6sxsabreauto
     - _make PLATFORM=imx-mx6sxsabresd
     - _make PLATFORM=imx-mx6solosabresd

core/arch/arm/plat-imx/conf.mk
@@ -22,6 +22,9 @@ mx6qp-flavorlist = \
 mx6sl-flavorlist = \
 	mx6slevk
 
+mx6sll-flavorlist = \
+	mx6sllevk
+
 mx6sx-flavorlist = \
 	mx6sxsabreauto \
 	mx6sxsabresd \
@@ -89,6 +92,11 @@ $(call force,CFG_TEE_CORE_NB_CORE,1)
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx6sl-flavorlist)))
 $(call force,CFG_MX6,y)
 $(call force,CFG_MX6SL,y)
+$(call force,CFG_TEE_CORE_NB_CORE,1)
+$(call force,CFG_IMX_CAAM,n)
+else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx6sll-flavorlist)))
+$(call force,CFG_MX6,y)
+$(call force,CFG_MX6SLL,y)
 $(call force,CFG_TEE_CORE_NB_CORE,1)
 $(call force,CFG_IMX_CAAM,n)
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx6sx-flavorlist)))
@@ -187,6 +195,11 @@ CFG_NS_ENTRY_ADDR ?= 0x80800000
 CFG_DDR_SIZE ?= 0x40000000
 endif
 
+ifneq (,$(filter $(PLATFORM_FLAVOR),mx6sllevk))
+CFG_NS_ENTRY_ADDR ?= 0x80800000
+CFG_DDR_SIZE ?= 0x80000000
+endif
+
 ifneq (,$(filter $(PLATFORM_FLAVOR),mx6sxsabreauto))
 CFG_DDR_SIZE ?= 0x80000000
 CFG_NS_ENTRY_ADDR ?= 0x80800000
@@ -230,7 +243,7 @@ endif
 
 # i.MX6 Solo/SL/SoloX/DualLite/Dual/Quad specific config
 ifeq ($(filter y, $(CFG_MX6QP) $(CFG_MX6Q) $(CFG_MX6D) $(CFG_MX6DL) $(CFG_MX6S) \
-      $(CFG_MX6SL) $(CFG_MX6SX)), y)
+	$(CFG_MX6SL) $(CFG_MX6SLL) $(CFG_MX6SX)), y)
 include core/arch/arm/cpu/cortex-a9.mk
 
 $(call force,CFG_PL310,y)
@@ -244,7 +257,8 @@ ifeq ($(filter y, $(CFG_MX6QP) $(CFG_MX6Q) $(CFG_MX6D) $(CFG_MX6DL) $(CFG_MX6S))
 CFG_DRAM_BASE ?= 0x10000000
 endif
 
-ifneq (,$(filter y, $(CFG_MX6UL) $(CFG_MX6ULL) $(CFG_MX6SL) $(CFG_MX6SX)))
+ifneq (,$(filter y, $(CFG_MX6UL) $(CFG_MX6ULL) $(CFG_MX6SL) $(CFG_MX6SLL) \
+	$(CFG_MX6SX)))
 CFG_DRAM_BASE ?= 0x80000000
 endif
 

core/arch/arm/plat-imx/config/imx6sll.h
@@ -1,0 +1,91 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2017-2019 NXP
+ */
+
+#ifndef _CONFIG_IMX6SLL_H
+#define _CONFIG_IMX6SLL_H
+
+/*
+ * PL310 TAG RAM Control Register
+ *
+ * bit[10:8]:1 - 2 cycle of write accesses latency
+ * bit[6:4]:3 - 4 cycle of read accesses latency
+ * bit[2:0]:2 - 3 cycle of setup latency
+ */
+#ifndef PL310_TAG_RAM_CTRL_INIT
+#define PL310_TAG_RAM_CTRL_INIT		0x00000132
+#endif
+
+/*
+ * PL310 DATA RAM Control Register
+ *
+ * bit[10:8]:1 - 2 cycle of write accesses latency
+ * bit[6:4]:3 - 4 cycle of read accesses latency
+ * bit[2:0]:2 - 3 cycle of setup latency
+ */
+#ifndef PL310_DATA_RAM_CTRL_INIT
+#define PL310_DATA_RAM_CTRL_INIT	0x00000132
+#endif
+
+/*
+ * PL310 Auxiliary Control Register
+ *
+ * Early BRESP enabled (bit30=1)
+ * I/Dcache prefetch enabled (bit29:28=2b11)
+ * NS can access interrupts (bit27=1)
+ * NS can lockown cache lines (bit26=1)
+ * Pseudo-random replacement policy (bit25=1)
+ * Force write allocated (default) (bit24:23=00)
+ * Shared attribute internally ignored (bit22=1, bit13=0)
+ * Parity disabled (bit21=0)
+ * Event monitor disabled (bit20=0)
+ * 16kb way size (bit19:17=3b001)
+ * 16-way associativity (bit16=1)
+ * Store buffer device limitation enabled (bit11=0)
+ * Cacheable accesses have high prio (bit10=0)
+ * Full Line Zero (FLZ) enabled (bit0=1)
+ */
+#ifndef PL310_AUX_CTRL_INIT
+#define PL310_AUX_CTRL_INIT		0x7E430001
+#endif
+
+/*
+ * PL310 Prefetch Control Register
+ *
+ * Double linefill disabled (bit30=0)
+ * I/D prefetch enabled (bit29:28=2b11)
+ * Prefetch drop enabled (bit24=1)
+ * Incr double linefill disable (bit23=0)
+ * Prefetch offset = 7 (bit4:0)
+ */
+#define PL310_PREFETCH_CTRL_INIT	0x31000007
+
+/*
+ * PL310 Power Register
+ *
+ * Dynamic clock gating enabled
+ * Standby mode enabled
+ */
+#define PL310_POWER_CTRL_INIT		0x00000003
+
+/*
+ * SCU Invalidate Register
+ *
+ * Invalidate all registers
+ */
+#define	SCU_INV_CTRL_INIT		0xFFFFFFFF
+
+/*
+ * SCU Access Register
+ * - both secure CPU access SCU
+ */
+#define SCU_SAC_CTRL_INIT		0x0000000F
+
+/*
+ * SCU NonSecure Access Register
+ * - both nonsec cpu access SCU, private and global timer
+ */
+#define SCU_NSAC_CTRL_INIT		0x00000FFF
+
+#endif

core/arch/arm/plat-imx/drivers/imx_csu.c
@@ -66,7 +66,7 @@ static TEE_Result csu_init(void)
 		csu_setting = csu_setting_imx6sx;
 	else if (soc_is_imx6ul() || soc_is_imx6ull())
 		csu_setting = csu_setting_imx6ul;
-	else if (soc_is_imx6sl())
+	else if (soc_is_imx6sll() || soc_is_imx6sl())
 		csu_setting = csu_setting_imx6sl;
 	else if (soc_is_imx6())
 		csu_setting = csu_setting_imx6;

core/arch/arm/plat-imx/imx.h
@@ -25,6 +25,7 @@ void imx_set_src_gpr(int cpu, uint32_t val);
 bool soc_is_imx6(void);
 bool soc_is_imx6sx(void);
 bool soc_is_imx6sl(void);
+bool soc_is_imx6sll(void);
 bool soc_is_imx6ul(void);
 bool soc_is_imx6ull(void);
 bool soc_is_imx6sdl(void);

core/arch/arm/plat-imx/platform_config.h
@@ -60,6 +60,9 @@
 /* For i.MX 6SL */
 #elif defined(CFG_MX6SL)
 #include <config/imx6sl.h>
+/* For i.MX 6SLL */
+#elif defined(CFG_MX6SLL)
+#include <config/imx6sll.h>
 #endif
 
 #endif /*PLATFORM_CONFIG_H*/

core/arch/arm/plat-imx/sub.mk
@@ -10,7 +10,7 @@ asm-defines-y += imx_pm_asm_defines.c
 endif
 
 ifneq (,$(filter y, $(CFG_MX6Q) $(CFG_MX6QP) $(CFG_MX6D) $(CFG_MX6DL) \
-	$(CFG_MX6S) $(CFG_MX6SL) $(CFG_MX6SX)))
+	$(CFG_MX6S) $(CFG_MX6SL) $(CFG_MX6SLL) $(CFG_MX6SX)))
 srcs-y += a9_plat_init.S
 srcs-$(CFG_SM_PLATFORM_HANDLER) += sm_platform_handler.c
 endif

nxp-imx/imx-optee-os

commit 59c0eba66ad90e06b4914cf4081ee9d1be784397

TEE-557 core: imx: correct i.MX6 soc version handling
soc_is_imx6() function is not handling all the i.MX6

Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
Fixes: 4dac83288b60 ("core: imx: add imx6sll evk board support")

core/arch/arm/plat-imx/imx-common.c
@@ -120,11 +120,12 @@ bool soc_is_imx6dqp(void)
 
 bool soc_is_imx6(void)
 {
-	return ((imx_soc_type() == SOC_MX6SX) ||
-			(imx_soc_type() == SOC_MX6UL) ||
-			(imx_soc_type() == SOC_MX6ULL) ||
-			(imx_soc_type() == SOC_MX6DL) ||
-			(imx_soc_type() == SOC_MX6Q));
+	return ((imx_soc_type() == SOC_MX6SLL) ||
+		(imx_soc_type() == SOC_MX6SL) ||
+		(imx_soc_type() == SOC_MX6SX) ||
+		(imx_soc_type() == SOC_MX6UL) ||
+		(imx_soc_type() == SOC_MX6ULL) ||
+		(imx_soc_type() == SOC_MX6DL) || (imx_soc_type() == SOC_MX6Q));
 }
 
 bool soc_is_imx7ds(void)