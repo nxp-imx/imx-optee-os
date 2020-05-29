nxp-imx/imx-optee-os

commit 4fcee7333abdf24b11e979ac7e927422a21bf2b9

TEE-517 core: imx: Fix DEK Blob support for i.MX 8mscale family
DEK Blob Feature is broken with the upgrade of optee 3.2.0 to
3.7.0.

Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>

core/arch/arm/plat-imx/pta/pta_dek_blob.c
@@ -33,7 +33,14 @@ static TEE_Result generate_dek_blob_pta(uint32_t param_types,
 						   TEE_PARAM_TYPE_VALUE_INPUT,
 						   TEE_PARAM_TYPE_VALUE_INPUT);
 
-	if (param_types != exp_param_types)
+	/* Keep backward compatibility with old API. */
+	uint32_t old_exp_param_types =
+		TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT,
+				TEE_PARAM_TYPE_MEMREF_OUTPUT,
+				TEE_PARAM_TYPE_NONE, TEE_PARAM_TYPE_NONE);
+
+	if (param_types != exp_param_types &&
+	    param_types != old_exp_param_types)
 		return TEE_ERROR_BAD_PARAMETERS;
 
 	/* Verify the input payload length that must be 128/192/256 bits */
@@ -58,9 +65,16 @@ static TEE_Result generate_dek_blob_pta(uint32_t param_types,
 	dek_blob.payload.length = params[0].memref.size;
 	dek_blob.blob.data = params[1].memref.buffer;
 	dek_blob.blob.length = params[1].memref.size;
-	sm_page.page = params[2].value.a;
-	sm_page.nb_pages = params[2].value.b;
-	sm_page.partition = params[3].value.a;
+	if (param_types == exp_param_types) {
+		sm_page.page = params[2].value.a;
+		sm_page.nb_pages = params[2].value.b;
+		sm_page.partition = params[3].value.a;
+	} else {
+		/* Default behavior is to use Page 2 from Partition 1 */
+		sm_page.page = 2;
+		sm_page.nb_pages = 1;
+		sm_page.partition = 1;
+	}
 
 	memset((void *)dek_blob.blob.data, 0x0, dek_blob.blob.length);
 
@@ -125,22 +139,6 @@ static TEE_Result dek_blob_free_partition(uint32_t param_types,
 }
 
 /*
- * Pseudo TA open session. Checks if TA is initiator.
- */
-static TEE_Result
-pta_dek_open_session(uint32_t param_types __unused,
-		     TEE_Param pParams[TEE_NUM_PARAMS] __unused,
-		     void **sess_ctx __unused)
-{
-	struct tee_ta_session *s = tee_ta_get_calling_session();
-
-	if (!s)
-		return TEE_ERROR_ACCESS_DENIED;
-
-	return TEE_SUCCESS;
-}
-
-/*
  * Called when a pseudo TA is invoked.
  *
  * sess_ctx       Session Identifier
@@ -164,5 +162,4 @@ static TEE_Result invokeCommandEntryPoint(void *sess_ctx __unused,
 
 pseudo_ta_register(.uuid = PTA_DEK_BLOB_UUID, .name = PTA_NAME,
 		   .flags = PTA_DEFAULT_FLAGS,
-		   .open_session_entry_point = pta_dek_open_session,
 		   .invoke_command_entry_point = invokeCommandEntryPoint);

commit 7a49776de59265500f10a247125429fde1555ac1

TEE-529 core: imx: Release SM partition after DEK blob creation
The Blob SM driver expects the caller to free the Secure Memory
partition after DEK Blob creation.
U-boot does not do it, because it was not required to do so
in the imx_3.2.y implementation. To keep backward compatibility
with older optee-os releases do the free in the pta.

Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
Fixes: 4fcee7333abd ("TEE-517 core: imx: Fix DEK Blob support for i.MX 8mscale family")
(cherry picked from commit a7698797984a2a0c3f627caa4ced982619c07dc7)

core/arch/arm/plat-imx/pta/pta_dek_blob.c
@@ -27,7 +27,7 @@ static TEE_Result generate_dek_blob_pta(uint32_t param_types,
 	struct hab_dek_blob_header *hdr = NULL;
 	struct crypto_blob dek_blob = {};
 	struct crypto_sm_page sm_page = {};
-
+	bool backward_compatibility = false;
 	uint32_t exp_param_types = TEE_PARAM_TYPES(TEE_PARAM_TYPE_MEMREF_INPUT,
 						   TEE_PARAM_TYPE_MEMREF_OUTPUT,
 						   TEE_PARAM_TYPE_VALUE_INPUT,
@@ -74,6 +74,8 @@ static TEE_Result generate_dek_blob_pta(uint32_t param_types,
 		sm_page.page = 2;
 		sm_page.nb_pages = 1;
 		sm_page.partition = 1;
+		/* keep compatibility with imx_3.2.y based optee-os */
+		backward_compatibility = true;
 	}
 
 	memset((void *)dek_blob.blob.data, 0x0, dek_blob.blob.length);
@@ -95,7 +97,7 @@ static TEE_Result generate_dek_blob_pta(uint32_t param_types,
 	res = caam_blob_sm_encapsulate(&dek_blob, &sm_page);
 	if (res != TEE_SUCCESS) {
 		EMSG("DEK Blob Encapsulation 0x%08" PRIx32, res);
-		return res;
+		goto out;
 	}
 
 	/* Build HAB DEK blob header */
@@ -115,8 +117,12 @@ static TEE_Result generate_dek_blob_pta(uint32_t param_types,
 	params[1].memref.size = hdr->len_lsb;
 	dhex_dump(NULL, 0, TRACE_DEBUG, (void *)params[1].memref.buffer,
 		  params[1].memref.size);
+	res = TEE_SUCCESS;
+out:
+	if (backward_compatibility)
+		res = caam_sm_free_partition(sm_page.partition);
 
-	return TEE_SUCCESS;
+	return res;
 }
 
 /*