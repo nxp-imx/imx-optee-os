OP-TEE/optee_os

commit 405a5072d8bd25c18fd151d97e452d6863786f03

libutils: add simplified fputc(), fputs(), fwrite(), write()
Preparing for C++ support in TAs.

Adds a few <stdio.h> functions to libutils:

 fputc()  fputs()  fwrite()  write()

The proposed implementations are limited in the sense that they only
accept writing to stdout or stderr. The output goes directly to the
secure console and no difference is made between stdout and stderr.

Signed-off-by: Jerome Forissier <jerome@forissier.org>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>
Reviewed-by: Jens Wiklander <jens.wiklander@linaro.org>

lib/libutils/isoc/fputs.c
@@ -1,0 +1,17 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2020, Huawei Technologies Co., Ltd
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <trace.h>
+
+int fputs(const char *s, FILE *stream)
+{
+	if (stream != stdout && stream != stderr)
+		abort();
+
+	trace_ext_puts(s);
+	return 0;
+}

lib/libutils/isoc/fp.c
@@ -1,0 +1,16 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2020, Huawei Technologies Co., Ltd
+ */
+
+#include <stdio.h>
+
+struct _FILE {
+	char dummy;
+};
+
+static struct _FILE _fake_stdout;
+static struct _FILE _fake_stderr;
+
+FILE *stdout = &_fake_stdout;
+FILE *stderr = &_fake_stderr;

lib/libutils/isoc/fputc.c
@@ -1,0 +1,15 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2020, Huawei Technologies Co., Ltd
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int fputc(int c, FILE *stream)
+{
+	if (stream != stdout && stream != stderr)
+		abort();
+
+	return putchar(c);
+}

lib/libutils/isoc/fwrite.c
@@ -1,0 +1,22 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2020, Huawei Technologies Co., Ltd
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
+	int fd = 0;
+
+	if (stream == stdout)
+		fd = 1;
+	else if (stream == stderr)
+		fd = 2;
+	else
+		abort();
+
+	return write(fd, ptr, size * nmemb);
+}

lib/libutils/isoc/include/stdio.h
@@ -24,4 +24,20 @@ int vsnprintf (char *str, size_t size, const char *fmt, va_list ap)
 int puts(const char *str);
 int putchar(int c);
 
+#ifndef __KERNEL__
+
+extern FILE *stdout;
+extern FILE *stderr;
+
+/*
+ * The functions below send their output synchronously to the secure console.
+ * They treat stdout and stderr the same, and will abort if stream is not one or
+ * the other.
+ */
+
+int fputc(int c, FILE *stream);
+int fputs(const char *s, FILE *stream);
+size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream);
+#endif
+
 #endif /*STDIO_H*/

lib/libutils/isoc/include/unistd.h
@@ -11,4 +11,7 @@
 #define __ssize_t_defined
 typedef intptr_t ssize_t;
 
+/* @fd must be 1 or 2. Writes to the secure console. */
+ssize_t write(int fd, const void *buf, size_t count);
+
 #endif

lib/libutils/isoc/sub.mk
@@ -28,5 +28,13 @@ srcs-y += isprint.c
 srcs-y += ispunct.c
 srcs-y += toupper.c
 
+ifneq (,$(filter ta_%,$(sm)))
+srcs-y += fp.c
+srcs-y += fputc.c
+srcs-y += fputs.c
+srcs-y += fwrite.c
+srcs-y += write.c
+endif
+
 subdirs-y += newlib
 subdirs-$(arch_arm) += arch/$(ARCH)

lib/libutils/isoc/write.c
@@ -1,0 +1,16 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2020, Huawei Technologies Co., Ltd
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+ssize_t write(int fd, const void *buf, size_t count)
+{
+	if (fd != 1 && fd != 2)
+		abort();
+
+	return printf("%*s", (int)count, (char *)buf);
+}

nxp-imx/imx-optee-os

commit 2625df869bfc9fae0a43ab31570f1f71bba399f8

libutils: Fix fputs() when TRACE is disabled
when TRACE is disabled trace_ext_puts() is not available.

Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
Fixes: 405a5072d8bd ("libutils: add simplified fputc(), fputs(), fwrite(), write()")

lib/libutils/isoc/fputs.c
@@ -7,11 +7,12 @@
 #include <stdlib.h>
 #include <trace.h>
 
-int fputs(const char *s, FILE *stream)
+int fputs(const char *s __maybe_unused, FILE *stream)
 {
 	if (stream != stdout && stream != stderr)
 		abort();
-
+#if TRACE_LEVEL > 0
 	trace_ext_puts(s);
+#endif
 	return 0;
 }