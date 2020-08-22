OP-TEE/optee_os

commit 796ea6d867547350b371dfa64a0959f3ee5bc107

drivers: caam: implement NXP CAAM Driver - RSA
Add the NXP CAAM drivers:
- Asymmetric RSA:
- Encrypt/Decrypt
- Sign/Verify
- Mathematical operation XOR (mod N)

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Acked-by: Jens Wiklander <jens.wiklander@linaro.org>

core/arch/arm/plat-imx/crypto_conf.mk
@@ -16,6 +16,7 @@
 # DBG_JR     BIT32(5)  // Job Ring trace
 # DBG_RNG    BIT32(6)  // RNG trace
 # DBG_HASH   BIT32(7)  // Hash trace
+# DBG_RSA    BIT32(8)  // RSA trace
 CFG_DBG_CAAM_TRACE ?= 0x2
 CFG_DBG_CAAM_DESC ?= 0x0
 CFG_DBG_CAAM_BUF ?= 0x0
@@ -55,7 +56,31 @@ $$(call force, CFG_NXP_CAAM_$$(_var)_DRV, y)
 $$(call force, CFG_CRYPTO_DRV_$$(_var), y)
 endef
 
+# Return 'y' if at least one of the variable
+# CFG_CRYPTO_xxx_HW is 'y'
+cryphw-one-enabled = $(call cfg-one-enabled, \
+                        $(foreach v,$(1), CFG_NXP_CAAM_$(v)_DRV))
+
 # Definition of the HW and Cryto Driver Algorithm supported by all i.MX
 $(eval $(call cryphw-enable-drv-hw, HASH))
+
+ifneq ($(filter y, $(CFG_MX6QP) $(CFG_MX6Q) $(CFG_MX6D) $(CFG_MX6DL) \
+	$(CFG_MX6S) $(CFG_MX6SL) $(CFG_MX6SLL) $(CFG_MX6SX)), y)
+$(eval $(call cryphw-enable-drv-hw, RSA))
+
+# Define the RSA Private Key Format used by the CAAM
+#   Format #1: (n, d)
+#   Format #2: (p, q, d)
+#   Format #3: (p, q, dp, dq, qp)
+CFG_NXP_CAAM_RSA_KEY_FORMAT ?= 3
+
+endif
+
+$(call force, CFG_NXP_CAAM_ACIPHER_DRV, $(call cryphw-one-enabled, RSA))
+
+#
+# Enable Cryptographic Driver interface
+#
+CFG_CRYPTO_DRV_ACIPHER ?= $(CFG_NXP_CAAM_ACIPHER_DRV)
 
 endif

core/drivers/crypto/caam/acipher/caam_math.c
@@ -1,0 +1,216 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2018-2020 NXP
+ *
+ * CAAM Mathematical Operation manager.
+ * Implementation of Mathematical operation using CAAM's MATH function
+ */
+#include <caam_acipher.h>
+#include <caam_common.h>
+#include <caam_hal_ctrl.h>
+#include <caam_jr.h>
+#include <caam_utils_mem.h>
+#include <caam_utils_sgt.h>
+#include <caam_utils_status.h>
+#include <drvcrypt.h>
+#include <drvcrypt_math.h>
+#include <mm/core_memprot.h>
+#include <string.h>
+#include <tee/cache.h>
+
+#include "local.h"
+
+/*
+ * MATH operation A xor B modulus n
+ *
+ * @data  [in/out] operation data
+ */
+static TEE_Result do_xor_mod_n(struct drvcrypt_mod_op *data)
+{
+	TEE_Result ret = TEE_ERROR_GENERIC;
+	enum caam_status retstatus = CAAM_FAILURE;
+	struct caam_jobctx jobctx = { };
+	uint32_t *desc = NULL;
+	int realloc = 0;
+	struct caambuf res_align = { };
+	struct caamsgtbuf sgtres = { .sgt_type = false };
+	struct caambuf data_a = { .data = data->a.data,
+				  .length = data->a.length };
+	struct caamsgtbuf sgtdata_a = { .sgt_type = false };
+	struct caambuf data_b = { .data = data->b.data,
+				  .length = data->b.length };
+	struct caamsgtbuf sgtdata_b = { .sgt_type = false };
+
+	RSA_TRACE("(A xor B) mod n");
+
+	data_a.paddr = virt_to_phys(data_a.data);
+	data_b.paddr = virt_to_phys(data_b.data);
+
+	if (!data_a.paddr || !data_b.paddr)
+		return ret;
+
+	if (!caam_mem_is_cached_buf(data_a.data, data_a.length))
+		data_a.nocache = 1;
+
+	retstatus = caam_sgt_build_block_data(&sgtdata_a, NULL, &data_a);
+	if (retstatus != CAAM_NO_ERROR) {
+		ret = TEE_ERROR_GENERIC;
+		goto out;
+	}
+
+	if (!caam_mem_is_cached_buf(data_b.data, data_b.length))
+		data_b.nocache = 1;
+
+	retstatus = caam_sgt_build_block_data(&sgtdata_b, NULL, &data_b);
+	if (retstatus != CAAM_NO_ERROR) {
+		ret = TEE_ERROR_GENERIC;
+		goto out;
+	}
+
+	/*
+	 * ReAllocate the result buffer with a maximum size
+	 * of the Key Modulus's size (N) if not cache aligned
+	 */
+	realloc = caam_set_or_alloc_align_buf(data->result.data, &res_align,
+					      data->result.length);
+	if (realloc == -1) {
+		ret = TEE_ERROR_OUT_OF_MEMORY;
+		goto out;
+	}
+
+	retstatus = caam_sgt_build_block_data(&sgtres, NULL, &res_align);
+	if (retstatus != CAAM_NO_ERROR) {
+		ret = TEE_ERROR_GENERIC;
+		goto out;
+	}
+
+#ifdef CFG_CAAM_64BIT
+#define XOR_OP_DESC_SIZE 14
+#else
+#define XOR_OP_DESC_SIZE 11
+#endif
+	/* Allocate the job descriptor */
+	desc = caam_calloc_desc(XOR_OP_DESC_SIZE);
+	if (!desc) {
+		ret = TEE_ERROR_OUT_OF_MEMORY;
+		goto out;
+	}
+
+	/* Load in N Modulus Size */
+	caam_desc_init(desc);
+	caam_desc_add_word(desc, DESC_HEADER(0));
+	caam_desc_add_word(desc, LD_IMM(CLASS_1, REG_PKHA_N_SIZE, 4));
+	caam_desc_add_word(desc, data->n.length);
+
+	/* Load in A first value */
+	if (sgtdata_a.sgt_type) {
+		caam_desc_add_word(desc, FIFO_LD_SGT(CLASS_1, PKHA_A, NOACTION,
+						     sgtdata_a.length));
+		caam_desc_add_ptr(desc, virt_to_phys(sgtdata_a.sgt));
+
+		caam_sgt_cache_op(TEE_CACHECLEAN, &sgtdata_a);
+	} else {
+		caam_desc_add_word(desc, FIFO_LD(CLASS_1, PKHA_A, NOACTION,
+						 sgtdata_a.length));
+		caam_desc_add_ptr(desc, sgtdata_a.buf->paddr);
+
+		if (!sgtdata_a.buf->nocache)
+			cache_operation(TEE_CACHECLEAN, sgtdata_a.buf->data,
+					sgtdata_a.length);
+	}
+
+	/* Load in B second value */
+	if (sgtdata_b.sgt_type) {
+		caam_desc_add_word(desc, FIFO_LD_SGT(CLASS_1, PKHA_B, NOACTION,
+						     sgtdata_b.length));
+		caam_desc_add_ptr(desc, virt_to_phys(sgtdata_b.sgt));
+
+		caam_sgt_cache_op(TEE_CACHECLEAN, &sgtdata_b);
+	} else {
+		caam_desc_add_word(desc, FIFO_LD(CLASS_1, PKHA_B, NOACTION,
+						 sgtdata_b.length));
+		caam_desc_add_ptr(desc, sgtdata_b.buf->paddr);
+
+		if (!sgtdata_b.buf->nocache)
+			cache_operation(TEE_CACHECLEAN, sgtdata_b.buf->data,
+					sgtdata_b.length);
+	}
+
+	/* Operation B = A xor B mod n */
+	caam_desc_add_word(desc, PKHA_F2M_OP(MOD_ADD_A_B, B));
+
+	/* Store the result */
+	if (sgtres.sgt_type) {
+		caam_desc_add_word(desc, FIFO_ST_SGT(PKHA_B, sgtres.length));
+		caam_desc_add_ptr(desc, virt_to_phys(sgtres.sgt));
+
+		caam_sgt_cache_op(TEE_CACHEFLUSH, &sgtres);
+	} else {
+		caam_desc_add_word(desc, FIFO_ST(PKHA_B, sgtres.length));
+		caam_desc_add_ptr(desc, sgtres.buf->paddr);
+
+		if (!sgtres.buf->nocache)
+			cache_operation(TEE_CACHEFLUSH, sgtres.buf->data,
+					sgtres.length);
+	}
+
+	RSA_DUMPDESC(desc);
+
+	if (!res_align.nocache)
+		cache_operation(TEE_CACHEFLUSH, res_align.data,
+				data->result.length);
+
+	jobctx.desc = desc;
+	retstatus = caam_jr_enqueue(&jobctx, NULL);
+
+	if (retstatus == CAAM_NO_ERROR) {
+		if (!res_align.nocache)
+			cache_operation(TEE_CACHEINVALIDATE, res_align.data,
+					data->result.length);
+
+		if (realloc)
+			memcpy(data->result.data, res_align.data,
+			       data->result.length);
+
+		RSA_DUMPBUF("Output", data->result.data, data->result.length);
+		ret = TEE_SUCCESS;
+	} else {
+		RSA_TRACE("CAAM Status 0x%08" PRIx32, jobctx.status);
+		ret = job_status_to_tee_result(jobctx.status);
+	}
+
+out:
+	caam_free_desc(&desc);
+
+	if (realloc == 1)
+		caam_free_buf(&res_align);
+
+	if (sgtdata_a.sgt_type)
+		caam_sgtbuf_free(&sgtdata_a);
+
+	if (sgtdata_b.sgt_type)
+		caam_sgtbuf_free(&sgtdata_b);
+
+	if (sgtres.sgt_type)
+		caam_sgtbuf_free(&sgtres);
+
+	return ret;
+}
+
+/*
+ * Registration of the MATH Driver
+ */
+static const struct drvcrypt_math driver_math = {
+	.xor_mod_n = &do_xor_mod_n,
+};
+
+enum caam_status caam_math_init(vaddr_t ctrl_addr __unused)
+{
+	enum caam_status retstatus = CAAM_FAILURE;
+
+	if (caam_hal_ctrl_pknum(ctrl_addr))
+		if (!drvcrypt_register_math(&driver_math))
+			retstatus = CAAM_NO_ERROR;
+
+	return retstatus;
+}

core/drivers/crypto/caam/acipher/local.h
@@ -1,0 +1,31 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2018-2020 NXP
+ *
+ * CAAM Cipher Local header.
+ */
+#ifndef __LOCAL_H__
+#define __LOCAL_H__
+
+#include "caam_common.h"
+
+/*
+ * Prime generator structure
+ */
+struct prime_data {
+	uint8_t era;	   /* CAAM Era version */
+	size_t key_size;   /* Key size in bits */
+	struct caambuf *e; /* Key exponent e */
+	struct caambuf *p; /* Prime p */
+	struct caambuf *q; /* Prime q (can be NULL of only p asked) */
+};
+
+/*
+ * Generate a Prime Number
+ * Algorithm based on the Chapter B.3.3 of the FIPS.184-6 specification
+ *
+ * @data  [in/out] Prime generation data
+ */
+enum caam_status caam_prime_gen(struct prime_data *data);
+
+#endif /* __LOCAL_H__ */

core/drivers/crypto/caam/acipher/sub.mk
@@ -1,0 +1,4 @@
+incdirs-y += ../include
+
+srcs-$(CFG_NXP_CAAM_RSA_DRV) += caam_rsa.c
+srcs-y += caam_prime.c caam_math.c

core/drivers/crypto/caam/caam_ctrl.c
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright 2017-2019 NXP
+ * Copyright 2017-2020 NXP
  *
  * Brief   CAAM Global Controller.
  */
+#include <caam_acipher.h>
 #include <caam_common.h>
 #include <caam_hal_cfg.h>
 #include <caam_hal_clk.h>
@@ -52,6 +53,20 @@ static TEE_Result crypto_driver_init(void)
 
 	/* Initialize the Hash Module */
 	retstatus = caam_hash_init(jrcfg.base);
+	if (retstatus != CAAM_NO_ERROR) {
+		retresult = TEE_ERROR_GENERIC;
+		goto exit_init;
+	}
+
+	/* Initialize the MATH Module */
+	retstatus = caam_math_init(jrcfg.base);
+	if (retstatus != CAAM_NO_ERROR) {
+		retresult = TEE_ERROR_GENERIC;
+		goto exit_init;
+	}
+
+	/* Initialize the RSA Module */
+	retstatus = caam_rsa_init(jrcfg.base);
 	if (retstatus != CAAM_NO_ERROR) {
 		retresult = TEE_ERROR_GENERIC;
 		goto exit_init;

core/drivers/crypto/caam/hal/common/hal_ctrl.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BSD-2-Clause
 /*
- * Copyright 2018-2019 NXP
+ * Copyright 2018-2020 NXP
  *
  * Brief   CAAM Controller Hardware Abstration Layer.
  *         Implementation of primitives to access HW.
@@ -37,4 +37,23 @@ uint8_t caam_hal_ctrl_hash_limit(vaddr_t baseaddr)
 	}
 
 	return UINT8_MAX;
+}
+
+uint8_t caam_hal_ctrl_pknum(vaddr_t baseaddr)
+{
+	uint32_t val = 0;
+
+	val = io_caam_read32(baseaddr + CHANUM_LS);
+
+	return GET_CHANUM_LS_PKNUM(val);
+}
+
+uint8_t caam_hal_ctrl_era(vaddr_t baseaddr)
+{
+	uint32_t val = 0;
+
+	/* Read the number of instance */
+	val = io_caam_read32(baseaddr + CCBVID);
+
+	return GET_CCBVID_CAAM_ERA(val);
 }

nxp-imx/imx-optee-os

commit f83847fef95dacc1973f035de2bfb290e5a1daff

core: imx: crypto_conf: i.mx 7ulp does not support CAAM PKHA
There is no support for CAAM PKHA in i.MX 7ULP SOC.

Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>
Fixes: 796ea6d86754 ("drivers: caam: implement NXP CAAM Driver - RSA")

core/arch/arm/plat-imx/crypto_conf.mk
@@ -73,7 +73,7 @@ $(eval $(call cryphw-enable-drv-hw, HMAC))
 $(eval $(call cryphw-enable-drv-hw, CMAC))
 
 ifneq ($(filter y, $(CFG_MX6QP) $(CFG_MX6Q) $(CFG_MX6D) $(CFG_MX6DL) \
-	$(CFG_MX6S) $(CFG_MX6SL) $(CFG_MX6SLL) $(CFG_MX6SX)), y)
+	$(CFG_MX6S) $(CFG_MX6SL) $(CFG_MX6SLL) $(CFG_MX6SX) $(CFG_MX7ULP)), y)
 $(eval $(call cryphw-enable-drv-hw, RSA))
 
 # Define the RSA Private Key Format used by the CAAM