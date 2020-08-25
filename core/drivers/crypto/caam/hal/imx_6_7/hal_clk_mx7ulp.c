c922ceeeb06a2348f24fa354fdd433f84f897d6a
Cedric Neveux <cedric.neveux@nxp.com>
2020-08-25 15:41:17 +0200

    TEE-591 drivers: caam: Fix hal clock registers read/write
    Fix CAAM Clock registers access to use io_read/write function and
    no io_caam_read/write.
    
    Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

--- core/drivers/crypto/caam/hal/imx_6_7/hal_clk_mx7ulp.c
+++ core/drivers/crypto/caam/hal/imx_6_7/hal_clk_mx7ulp.c
@@ -5,7 +5,7 @@
  * Brief   CAAM Clock functions.
  */
 #include <caam_hal_clk.h>
-#include <caam_io.h>
+#include <io.h>
 #include <mm/core_memprot.h>
 #include <platform_config.h>
 
@@ -14,7 +14,7 @@ void caam_hal_clk_enable(bool enable)
 	vaddr_t pcc2_base = (vaddr_t)phys_to_virt(PCC2_BASE, MEM_AREA_IO_SEC);
 
 	if (enable)
-		io_caam_write32(pcc2_base + PCC_CAAM, PCC_ENABLE_CLOCK);
+		io_write32(pcc2_base + PCC_CAAM, PCC_ENABLE_CLOCK);
 	else
-		io_caam_write32(pcc2_base + PCC_CAAM, PCC_DISABLE_CLOCK);
+		io_write32(pcc2_base + PCC_CAAM, PCC_DISABLE_CLOCK);
 }
