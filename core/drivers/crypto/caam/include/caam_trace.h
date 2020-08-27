OP-TEE/optee_os

commit dfb57b8b3bfecfbfdd594364a58792a0bc857593

drivers: caam: implement NXP CAAM driver - Cipher
Add the NXP CAAM drivers:
  - Cipher (AES/DES/DES3)

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Signed-off-by: Clement Faure <clement.faure@nxp.com>
Acked-by: Etienne Carriere <etienne.carriere@linaro.org>

core/drivers/crypto/caam/include/caam_trace.h
@@ -34,6 +34,7 @@
 #define DBG_TRACE_RNG	 BIT32(6)  /* RNG trace */
 #define DBG_TRACE_HASH	 BIT32(7)  /* Hash trace */
 #define DBG_TRACE_RSA	 BIT32(8)  /* RSA trace */
+#define DBG_TRACE_CIPHER BIT32(9)  /* Cipher dump Buffer */
 
 /* HAL */
 #if CAAM_DBG_TRACE(HAL)
@@ -148,6 +149,29 @@
 #define RSA_TRACE(...)
 #define RSA_DUMPDESC(desc)
 #define RSA_DUMPBUF(...)
+#endif
+
+/* Cipher */
+#if CAAM_DBG_TRACE(CIPHER)
+#define CIPHER_TRACE DRV_TRACE
+#if CAAM_DBG_DESC(CIPHER)
+#define CIPHER_DUMPDESC(desc)                                                  \
+	do {                                                                   \
+		CIPHER_TRACE("CIPHER Descriptor");                             \
+		DRV_DUMPDESC(desc);                                            \
+	} while (0)
+#else
+#define CIPHER_DUMPDESC(desc)
+#endif
+#if CAAM_DBG_TRACE(CIPHER)
+#define CIPHER_DUMPBUF DRV_DUMPBUF
+#else
+#define CIPHER_DUMPBUF(...)
+#endif
+#else
+#define CIPHER_TRACE(...)
+#define CIPHER_DUMPDESC(desc)
+#define CIPHER_DUMPBUF(...)
 #endif
 
 #if (TRACE_LEVEL >= TRACE_DEBUG)

core/arch/arm/plat-imx/crypto_conf.mk
@@ -17,6 +17,7 @@
 # DBG_RNG    BIT32(6)  // RNG trace
 # DBG_HASH   BIT32(7)  // Hash trace
 # DBG_RSA    BIT32(8)  // RSA trace
+# DBG_CIPHER BIT32(9)  // Cipher trace
 CFG_DBG_CAAM_TRACE ?= 0x2
 CFG_DBG_CAAM_DESC ?= 0x0
 CFG_DBG_CAAM_BUF ?= 0x0
@@ -63,6 +64,7 @@ cryphw-one-enabled = $(call cfg-one-enabled, \
 
 # Definition of the HW and Cryto Driver Algorithm supported by all i.MX
 $(eval $(call cryphw-enable-drv-hw, HASH))
+$(eval $(call cryphw-enable-drv-hw, CIPHER))
 
 ifneq ($(filter y, $(CFG_MX6QP) $(CFG_MX6Q) $(CFG_MX6D) $(CFG_MX6DL) \
 	$(CFG_MX6S) $(CFG_MX6SL) $(CFG_MX6SLL) $(CFG_MX6SX)), y)

core/drivers/crypto/caam/caam_ctrl.c
@@ -5,6 +5,7 @@
  * Brief   CAAM Global Controller.
  */
 #include <caam_acipher.h>
+#include <caam_cipher.h>
 #include <caam_common.h>
 #include <caam_hal_cfg.h>
 #include <caam_hal_clk.h>
@@ -67,6 +68,13 @@ static TEE_Result crypto_driver_init(void)
 
 	/* Initialize the RSA Module */
 	retstatus = caam_rsa_init(jrcfg.base);
+	if (retstatus != CAAM_NO_ERROR) {
+		retresult = TEE_ERROR_GENERIC;
+		goto exit_init;
+	}
+
+	/* Initialize the Cipher Module */
+	retstatus = caam_cipher_init(jrcfg.base);
 	if (retstatus != CAAM_NO_ERROR) {
 		retresult = TEE_ERROR_GENERIC;
 		goto exit_init;

core/drivers/crypto/caam/cipher/caam_cipher_xts.c
@@ -1,0 +1,255 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2018-2020 NXP
+ *
+ * Implementation of Cipher XTS functions
+ */
+#include <caam_common.h>
+#include <caam_utils_mem.h>
+#include <mm/core_memprot.h>
+#include <string.h>
+
+#include "local.h"
+
+/*
+ * Galois Multiplication
+ *
+ * @buf  [in/out] buffer to multiply
+ */
+static void do_galois_mult(struct caambuf *buf)
+{
+	size_t idx = 0;
+	uint8_t tmp = 0;
+	uint8_t tmptmp = 0;
+
+	for (idx = 0; idx < buf->length; idx++) {
+		tmptmp = buf->data[idx] >> 7;
+		buf->data[idx] = (buf->data[idx] << 1) | tmp;
+		tmp = tmptmp;
+	}
+
+	if (tmptmp)
+		buf->data[0] ^= 0x87;
+}
+
+/*
+ * Tweak a cipher block (XTS mode)
+ *
+ * @ctx        Cipher context
+ * @enc_tweak  [in/out] Encrypted tweak (Galois multiplication)
+ * @srcbuf     Source data to encrypt/decrypt
+ * @dstbuf     [out] Destination data encrypted/decrypted
+ * @tmp        Temporary data buffer
+ */
+static enum caam_status do_tweak_block(struct cipherdata *ctx,
+				       struct caambuf *enc_tweak,
+				       struct caambuf *srcbuf,
+				       struct caambuf *dstbuf,
+				       struct caambuf *tmp)
+{
+	enum caam_status retstatus = CAAM_FAILURE;
+	unsigned int idx = 0;
+
+	/*
+	 * TODO: Optimization by using CAAM to do it with MATH op in the
+	 * operation description
+	 */
+	for (idx = 0; idx < ctx->alg->size_block; idx++)
+		tmp->data[idx] = srcbuf->data[idx] ^ enc_tweak->data[idx];
+
+	retstatus = caam_cipher_block(ctx, false, NEED_KEY1, ctx->encrypt, tmp,
+				      tmp, false);
+
+	if (retstatus != CAAM_NO_ERROR)
+		return retstatus;
+
+	for (idx = 0; idx < ctx->alg->size_block; idx++)
+		dstbuf->data[idx] = tmp->data[idx] ^ enc_tweak->data[idx];
+
+	/* Galois field multiplication of the tweak */
+	do_galois_mult(enc_tweak);
+
+	return retstatus;
+}
+
+TEE_Result caam_cipher_update_xts(struct drvcrypt_cipher_update *dupdate)
+{
+	TEE_Result ret = TEE_ERROR_GENERIC;
+	enum caam_status retstatus = CAAM_FAILURE;
+	struct cipherdata *ctx = dupdate->ctx;
+	struct caambuf enc_tweak = { };
+	struct caambuf tmpsrc = { };
+	struct caambuf tmpdst = { };
+	struct caambuf srcbuf = { };
+	struct caambuf dstbuf = { };
+	size_t idx = 0;
+	size_t fullsize = 0;
+	size_t lastblk = 0;
+	paddr_t psrc = 0;
+	paddr_t pdst = 0;
+
+	CIPHER_TRACE("Algo AES XTS length=%zu - %s", dupdate->src.length,
+		     ctx->encrypt ? "Encrypt" : " Decrypt");
+
+	psrc = virt_to_phys(dupdate->src.data);
+	pdst = virt_to_phys(dupdate->dst.data);
+
+	/* Check the payload/cipher physical addresses */
+	if (!psrc || !pdst) {
+		CIPHER_TRACE("Bad Addr (src %#" PRIxPA ") (dst %#" PRIxPA ")",
+			     psrc, pdst);
+		return TEE_ERROR_GENERIC;
+	}
+
+	/* First operation is to encrypt the tweak with the key #2 */
+	/* Allocate the encrypted tweak buffer */
+	retstatus = caam_alloc_align_buf(&enc_tweak, ctx->tweak.length);
+	if (retstatus != CAAM_NO_ERROR)
+		return TEE_ERROR_OUT_OF_MEMORY;
+
+	/* Allocate a temporary destination buffer to encrypt tweak block */
+	retstatus = caam_alloc_align_buf(&tmpdst, ctx->alg->size_block);
+	if (retstatus != CAAM_NO_ERROR) {
+		ret = TEE_ERROR_OUT_OF_MEMORY;
+		goto out;
+	}
+
+	retstatus = caam_cipher_block(ctx, false, NEED_KEY2, true, &ctx->tweak,
+				      &enc_tweak, false);
+	if (retstatus != CAAM_NO_ERROR) {
+		CIPHER_TRACE("Tweak encryption error");
+		ret = TEE_ERROR_GENERIC;
+		goto out;
+	}
+
+	/*
+	 * Encrypt or Decrypt input data.
+	 * Check if the last block is partial or not
+	 *  - if last block is partial, rebuild a complete
+	 *    block using the penultimate complete block
+	 *    encryption/decryption.
+	 *  - else do all blocks.
+	 */
+
+	/* Calculate the number of complete block */
+	fullsize = dupdate->src.length;
+	lastblk = fullsize % ctx->alg->size_block;
+	fullsize -= lastblk;
+
+	/* One full block is needed */
+	if (!fullsize) {
+		ret = TEE_ERROR_BAD_PARAMETERS;
+		goto out;
+	}
+
+	if (lastblk)
+		fullsize -= ctx->alg->size_block;
+
+	srcbuf.data = dupdate->src.data;
+	srcbuf.length = ctx->alg->size_block;
+	srcbuf.paddr = psrc;
+
+	dstbuf.data = dupdate->dst.data;
+	dstbuf.length = ctx->alg->size_block;
+	dstbuf.paddr = pdst;
+
+	for (; fullsize > 0; fullsize -= ctx->alg->size_block) {
+		CIPHER_TRACE("Tweak block fullsize %zu", fullsize);
+		retstatus = do_tweak_block(ctx, &enc_tweak, &srcbuf, &dstbuf,
+					   &tmpdst);
+
+		CIPHER_TRACE("Tweak block ret 0x%" PRIx32, retstatus);
+		if (retstatus != CAAM_NO_ERROR) {
+			ret = TEE_ERROR_GENERIC;
+			goto out;
+		}
+
+		CIPHER_DUMPBUF("Source", srcbuf.data, srcbuf.length);
+		CIPHER_DUMPBUF("Dest", dstbuf.data, dstbuf.length);
+
+		/* Increment the source and destination block */
+		srcbuf.data += ctx->alg->size_block;
+		srcbuf.paddr += ctx->alg->size_block;
+
+		dstbuf.data += ctx->alg->size_block;
+		dstbuf.paddr += ctx->alg->size_block;
+	}
+
+	if (lastblk) {
+		CIPHER_TRACE("Last block size is %zu", lastblk);
+
+		/*
+		 * Allocate the temporary buffer containing the
+		 * penultimate block computed
+		 */
+		retstatus = caam_alloc_align_buf(&tmpsrc, ctx->alg->size_block);
+		if (retstatus != CAAM_NO_ERROR) {
+			ret = TEE_ERROR_OUT_OF_MEMORY;
+			goto out;
+		}
+
+		if (!ctx->encrypt) {
+			/*
+			 * In case of decryption, need to multiply
+			 * the tweak first
+			 */
+			memcpy(tmpsrc.data, enc_tweak.data, enc_tweak.length);
+			do_galois_mult(&tmpsrc);
+
+			retstatus = do_tweak_block(ctx, &tmpsrc, &srcbuf,
+						   &tmpdst, &tmpdst);
+		} else {
+			retstatus = do_tweak_block(ctx, &enc_tweak, &srcbuf,
+						   &tmpdst, &tmpdst);
+		}
+
+		CIPHER_TRACE("Tweak penultimate block ret 0x%" PRIx32,
+			     retstatus);
+
+		if (retstatus != CAAM_NO_ERROR) {
+			ret = TEE_ERROR_GENERIC;
+			goto out;
+		}
+
+		/* Build the last block and create the last destination block */
+		for (idx = 0; idx < lastblk; idx++) {
+			tmpsrc.data[idx] =
+				srcbuf.data[ctx->alg->size_block + idx];
+			dstbuf.data[ctx->alg->size_block + idx] =
+				tmpdst.data[idx];
+		}
+
+		for (; idx < ctx->alg->size_block; idx++)
+			tmpsrc.data[idx] = tmpdst.data[idx];
+
+		retstatus = do_tweak_block(ctx, &enc_tweak, &tmpsrc, &dstbuf,
+					   &tmpdst);
+
+		CIPHER_DUMPBUF("Source", tmpsrc.data, tmpsrc.length);
+		CIPHER_DUMPBUF("Dest", dstbuf.data, dstbuf.length);
+		CIPHER_TRACE("Tweak last block ret 0x%" PRIx32, retstatus);
+
+		if (retstatus != CAAM_NO_ERROR) {
+			ret = TEE_ERROR_GENERIC;
+			goto out;
+		}
+	}
+
+	/* Finalize by decrypting the tweak back */
+	retstatus = caam_cipher_block(ctx, false, NEED_KEY2, false, &enc_tweak,
+				      &ctx->tweak, false);
+	if (retstatus != CAAM_NO_ERROR) {
+		CIPHER_TRACE("Tweak decryption error");
+		ret = TEE_ERROR_GENERIC;
+		goto out;
+	}
+
+	ret = TEE_SUCCESS;
+
+out:
+	caam_free_buf(&enc_tweak);
+	caam_free_buf(&tmpsrc);
+	caam_free_buf(&tmpdst);
+
+	return ret;
+}

core/drivers/crypto/caam/cipher/local.h
@@ -1,0 +1,73 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2018-2019 NXP
+ *
+ * CAAM Cipher Local header.
+ */
+#ifndef __LOCAL_H__
+#define __LOCAL_H__
+
+#include <drvcrypt.h>
+#include <drvcrypt_cipher.h>
+
+/*
+ * Definition of flags tagging which key(s) is required
+ */
+#define NEED_KEY1  BIT(0)
+#define NEED_KEY2  BIT(1)
+#define NEED_IV    BIT(2)
+#define NEED_TWEAK BIT(3)
+
+/*
+ * Cipher Algorithm definition
+ */
+struct cipheralg {
+	uint32_t type;             /* Algo type for operation */
+	uint8_t size_block;        /* Computing block size */
+	uint8_t size_ctx;          /* CAAM Context Register size */
+	uint8_t ctx_offset;        /* CAAM Context Register offset */
+	uint8_t require_key;       /* Tag defining key(s) required */
+	struct caamdefkey def_key; /* Key size accepted */
+
+	TEE_Result (*update)(struct drvcrypt_cipher_update *dupdate);
+};
+
+/*
+ * Full Cipher data SW context
+ */
+struct cipherdata {
+	uint32_t *descriptor;        /* Job descriptor */
+	bool encrypt;                /* Encrypt direction */
+	struct caambuf key1;         /* First Key */
+	struct caambuf key2;         /* Second Key */
+	struct caambuf tweak;        /* XTS Tweak */
+	struct caambuf ctx;          /* CAAM Context Register */
+	struct caamblock blockbuf;   /* Temporary Block buffer */
+	const struct cipheralg *alg; /* Reference to the algo constants */
+};
+
+/*
+ * Update of the cipher operation of complete block except
+ * if last block. Last block can be partial block.
+ *
+ * @ctx      Cipher context
+ * @savectx  Save or not the context
+ * @keyid    Id of the key to be used during operation
+ * @encrypt  Encrypt or decrypt direction
+ * @src      Source data to encrypt/decrypt
+ * @dst      [out] Destination data encrypted/decrypted
+ * @blockbuf Saved block during previous streaming update
+ */
+enum caam_status caam_cipher_block(struct cipherdata *ctx, bool savectx,
+				   uint8_t keyid, bool encrypt,
+				   struct caambuf *src, struct caambuf *dst,
+				   bool blockbuf);
+
+/*
+ * Update of the cipher operation in xts mode.
+ *
+ * @dupdate  Data update object
+ */
+TEE_Result caam_cipher_update_xts(struct drvcrypt_cipher_update *dupdate);
+
+#endif /* __LOCAL_H__ */

core/drivers/crypto/caam/cipher/sub.mk
@@ -1,0 +1,4 @@
+incdirs-y += ../include
+
+srcs-y += caam_cipher.c
+srcs-y += caam_cipher_xts.c

core/drivers/crypto/caam/include/caam_cipher.h
@@ -1,0 +1,25 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2018-2019 NXP
+ *
+ * Brief   CAAM Cipher manager header.
+ */
+#ifndef __CAAM_CIPHER_H__
+#define __CAAM_CIPHER_H__
+
+#include <caam_common.h>
+
+#ifdef CFG_NXP_CAAM_CIPHER_DRV
+/*
+ * Initialize the Cipher module
+ *
+ * @ctrl_addr   Controller base address
+ */
+enum caam_status caam_cipher_init(vaddr_t ctrl_addr);
+#else
+static inline enum caam_status caam_cipher_init(vaddr_t ctrl_addr __unused)
+{
+	return CAAM_NO_ERROR;
+}
+#endif /* CFG_NXP_CAAM_CIPHER_DRV */
+#endif /* __CAAM_CIPHER_H__ */

nxp-imx/imx-optee-os

commit 4a655e4d1e777002415be8a38e6dd83339570586

TEE-593 drivers: caam: fix Cipher dump buffer trace
To enable the buffer trace in the caam driver the macro to use is
CAAM_DBG_BUF(<driver>).

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>
Fixes: dfb57b8b3bf ("drivers: caam: implement NXP CAAM driver - Cipher")

core/drivers/crypto/caam/include/caam_trace.h
@@ -170,7 +170,7 @@
 #else
 #define CIPHER_DUMPDESC(desc)
 #endif
-#if CAAM_DBG_TRACE(CIPHER)
+#if CAAM_DBG_BUF(CIPHER)
 #define CIPHER_DUMPBUF DRV_DUMPBUF
 #else
 #define CIPHER_DUMPBUF(...)