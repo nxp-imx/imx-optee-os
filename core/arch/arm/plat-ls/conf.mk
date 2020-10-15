00fbffea1dbccbc21e29359ecedf19ae611a8a0b
Franck LENORMAND <franck.lenormand@nxp.com>
2020-10-15 16:32:04 +0200

    LFOPTEE-3 core: ls: Fix CAAM DEBUG configuration option
    When compiling with CFG_NXP_CAAM for LS, an issue happens:
    In file included from core/drivers/crypto/caam/hash/caam_hash.c:13:0:
    core/drivers/crypto/crypto_api/include/drvcrypt.h:22:6:
     |_ warning: "CFG_CRYPTO_DRIVER_DEBUG" is not defined,
        evaluates to 0 [-Wundef]
     #if (CFG_CRYPTO_DRIVER_DEBUG & DRV_DBG_TRACE)
    
    This patch replace the "n" by a "0" like done for IMX.
    
    Signed-off-by: Franck LENORMAND <franck.lenormand@nxp.com>

--- core/arch/arm/plat-ls/conf.mk
+++ core/arch/arm/plat-ls/conf.mk
@@ -165,7 +165,7 @@ ifeq ($(CFG_NXP_CAAM),y)
 # it with generic crypto API can be enabled.
 CFG_CRYPTO_DRIVER ?= y
 CFG_CAAM_64BIT ?= y
-CFG_CRYPTO_DRIVER_DEBUG ?= n
+CFG_CRYPTO_DRIVER_DEBUG ?= 0
 else
 $(call force,CFG_CRYPTO_DRIVER,n)
 $(call force,CFG_WITH_SOFTWARE_PRNG,y)
