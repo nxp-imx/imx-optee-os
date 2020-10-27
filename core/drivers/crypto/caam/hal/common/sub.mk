181711965e61535ba65f855a41e5b9768a6059fd
Franck LENORMAND <franck.lenormand@nxp.com>
2020-10-27 15:47:15 +0100

    TEE-602 drivers: caam:  Fix MP and SM drivers compilation
    Compilation fails when CAAM driver is compiled without CFG_CRYPTO_DRV_MP
    or CFG_CRYPTO_DRV_SM. This is the case when using SoC that does not have those
    IP block like Layerscape devices.
    
    Signed-off-by: Franck LENORMAND <franck.lenormand@nxp.com>

--- core/drivers/crypto/caam/hal/common/sub.mk
+++ core/drivers/crypto/caam/hal/common/sub.mk
@@ -3,9 +3,11 @@ incdirs-y += ../$(CAAM_HAL_DIR)
 incdirs-y += .
 
 srcs-$(CFG_DT) += hal_cfg_dt.c
-srcs-$(CFG_DT) += hal_sm_dt.c
 srcs-y += hal_cfg.c
 srcs-y += hal_rng.c
 srcs-y += hal_jr.c
 srcs-y += hal_ctrl.c
+ifeq ($(CFG_CRYPTO_DRV_SM),y)
 srcs-y += hal_sm.c
+srcs-$(CFG_DT) += hal_sm_dt.c
+endif
