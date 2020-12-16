nxp-imx/imx-optee-os

commit 4e31a46ff797063e15d3b1d9f53787c050710c03

drivers: implement NXP CAAM blob SM
Add NXP CAAM driver:
  - Blob encapsulation from CAAM Secure Memory

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/arch/arm/plat-imx/crypto_conf.mk
@@ -30,7 +30,7 @@ CFG_DBG_CAAM_DESC ?= 0x0
 CFG_DBG_CAAM_BUF ?= 0x0
 
 # Enable the BLOB module used for the hardware unique key
-CFG_NXP_CAAM_BLOB_DRV ?= y
+# CFG_NXP_CAAM_BLOB_DRV ?= y
 
 #
 # CAAM Job Ring configuration
@@ -78,6 +78,7 @@ $(eval $(call cryphw-enable-drv-hw, CIPHER))
 $(eval $(call cryphw-enable-drv-hw, HMAC))
 $(eval $(call cryphw-enable-drv-hw, CMAC))
 $(eval $(call cryphw-enable-drv-hw, SM))
+$(eval $(call cryphw-enable-drv-hw, BLOB))
 
 ifneq ($(filter y, $(CFG_MX6QP) $(CFG_MX6Q) $(CFG_MX6D) $(CFG_MX6DL) \
 	$(CFG_MX6S) $(CFG_MX6SL) $(CFG_MX6SLL) $(CFG_MX6SX) $(CFG_MX7ULP)), y)

core/drivers/crypto/caam/blob/caam_blob_sm.c
@@ -1,0 +1,189 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2019 NXP
+ *
+ * Brief   CAAM Generation of an encapsulated DEK blob
+ *         Use the CAAM Blob encapsulation from CAAM Secure Memory
+ */
+#include <caam_common.h>
+#include <caam_sm.h>
+#include <caam_utils_mem.h>
+#include <caam_utils_sgt.h>
+#include <drivers/caam/crypto_extension.h>
+#include <mm/core_memprot.h>
+#include <string.h>
+#include <tee/cache.h>
+
+#ifdef CFG_PHYS_64BIT
+#define BLOB_OPERATE_DESC_ENTRIES 12
+#else
+#define BLOB_OPERATE_DESC_ENTRIES 10
+#endif
+
+TEE_Result caam_blob_sm_encapsulate(struct crypto_blob *blob,
+				    struct crypto_sm_page *sm_page)
+{
+	TEE_Result ret = TEE_ERROR_GENERIC;
+	enum caam_status retstatus = CAAM_FAILURE;
+	struct sm_page_addr sm_addr = {};
+	bool realloc = false;
+	struct caambuf blob_align = {};
+	struct caamsgtbuf sgtblob = { .sgt_type = false };
+	paddr_t paddr_blob = 0;
+	unsigned int opflags = 0;
+	size_t outsize = 0;
+	struct caam_jobctx jobctx = {};
+	uint32_t *desc = NULL;
+
+	if (!blob->payload.data || !blob->blob.data)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	outsize = blob->payload.length + BLOB_PAD_SIZE;
+
+	switch (blob->type) {
+	case BLOB_BLACK_CCM:
+		opflags = PROT_BLOB_TYPE(BLACK_KEY) | PROT_BLOB_INFO(CCM);
+		outsize = ROUNDUP(BLACK_KEY_CCM_SIZE(outsize), 16);
+		break;
+
+	case BLOB_BLACK_ECB:
+		opflags = PROT_BLOB_TYPE(BLACK_KEY) | PROT_BLOB_INFO(ECB);
+		outsize = ROUNDUP(BLACK_KEY_ECB_SIZE(outsize), 16);
+		break;
+
+	case BLOB_RED:
+		break;
+
+	default:
+		return TEE_ERROR_BAD_PARAMETERS;
+	}
+
+	if (blob->blob.length < outsize) {
+		BLOB_TRACE("Blob buffer too short expected %zu bytes", outsize);
+		blob->blob.length = outsize;
+		return TEE_ERROR_SHORT_BUFFER;
+	}
+
+	/* Re-allocate output buffer if alignment needed */
+	retstatus = caam_set_or_alloc_align_buf(blob->blob.data, &blob_align,
+					      outsize, &realloc);
+	if (retstatus != CAAM_NO_ERROR) {
+		BLOB_TRACE("Blob reallocation error");
+		return TEE_ERROR_OUT_OF_MEMORY;
+	}
+
+	retstatus = caam_sgt_build_block_data(&sgtblob, NULL, &blob_align);
+	if (retstatus != CAAM_NO_ERROR) {
+		ret = TEE_ERROR_GENERIC;
+		goto exit_operate;
+	}
+
+	if (sgtblob.sgt_type) {
+		paddr_blob = virt_to_phys(sgtblob.sgt);
+		caam_sgt_cache_op(TEE_CACHEFLUSH, &sgtblob);
+	} else {
+		paddr_blob = sgtblob.buf->paddr;
+		if (!sgtblob.buf->nocache)
+			cache_operation(TEE_CACHEFLUSH, sgtblob.buf->data,
+					sgtblob.length);
+	}
+
+	/* Allocate page(s) in one Secure Memory partition */
+	ret = caam_sm_alloc(sm_page, &sm_addr);
+	if (ret != CAAM_NO_ERROR) {
+		BLOB_TRACE("Secure memory allocation error 0x%" PRIx32, ret);
+		goto exit_operate;
+	}
+
+	/*
+	 * Set the partition access rights for the group #1 to be
+	 * a blob export/import
+	 */
+	/* Copy input data to encapsulate in Secure Memory allocated */
+	memcpy((void *)sm_addr.vaddr, blob->payload.data, blob->payload.length);
+
+	BLOB_DUMPBUF("Secure Memory", (void *)sm_addr.vaddr,
+		     blob->payload.length);
+
+	caam_sm_set_access_perm(sm_page, SM_GRP_BLOB, 0);
+
+	/* Allocate the descriptor */
+	desc = caam_calloc_desc(BLOB_OPERATE_DESC_ENTRIES);
+	if (!desc) {
+		BLOB_TRACE("CAAM Context Descriptor Allocation error");
+		ret = TEE_ERROR_OUT_OF_MEMORY;
+		goto exit_operate;
+	}
+
+	/*
+	 * Create the Blob encapsulation descriptor
+	 */
+	caam_desc_init(desc);
+	caam_desc_add_word(desc, DESC_HEADER(0));
+
+	/*
+	 * Command Protocol - The Key Modifier used when payload is
+	 * in Secure Memory is only 64 bits and it's loaded in the
+	 * Class 2 Key register at offset 12.
+	 *
+	 * Use the load immediata key value because key is small.
+	 */
+	caam_desc_add_word(desc, LD_IMM_OFF(CLASS_2, REG_KEY, 8, 12));
+	caam_desc_add_word(desc, blob->key[0]);
+	caam_desc_add_word(desc, blob->key[1]);
+
+	/* Define the Input data sequence */
+	caam_desc_add_word(desc, SEQ_IN_PTR(blob->payload.length));
+	caam_desc_add_ptr(desc, sm_addr.paddr);
+
+	/* Define the Output data sequence */
+	if (sgtblob.sgt_type)
+		caam_desc_add_word(desc, SEQ_OUT_PTR(sgtblob.length));
+	else
+		caam_desc_add_word(desc, SEQ_OUT_PTR(sgtblob.length));
+	caam_desc_add_ptr(desc, paddr_blob);
+
+	/* Define the encapsulation operation */
+	caam_desc_add_word(desc, BLOB_ENCAPS | PROT_BLOB_SEC_MEM | opflags);
+
+	BLOB_DUMPDESC(desc);
+
+	cache_operation(TEE_CACHECLEAN, blob->payload.data,
+			blob->payload.length);
+
+	jobctx.desc = desc;
+	retstatus = caam_jr_enqueue(&jobctx, NULL);
+
+	if (retstatus == CAAM_NO_ERROR) {
+		BLOB_TRACE("Done CAAM BLOB from Secure Memory encaps");
+
+		if (!blob_align.nocache)
+			cache_operation(TEE_CACHEINVALIDATE, blob_align.data,
+					blob_align.length);
+
+		BLOB_DUMPBUF("Blob Output", blob_align.data, blob_align.length);
+
+		blob->blob.length = blob_align.length;
+
+		if (realloc)
+			memcpy(blob->blob.data, blob_align.data,
+			       blob->blob.length);
+
+		ret = TEE_SUCCESS;
+	} else {
+		BLOB_TRACE("CAAM Status 0x%08" PRIx32 "", jobctx.status);
+		ret = TEE_ERROR_GENERIC;
+	}
+
+exit_operate:
+	if (realloc)
+		caam_free_buf(&blob_align);
+
+	caam_sm_free_page(sm_page);
+	caam_free_desc(&desc);
+
+	if (sgtblob.sgt_type)
+		caam_sgtbuf_free(&sgtblob);
+
+	return ret;
+}

core/drivers/crypto/caam/blob/sub.mk
@@ -1,3 +1,5 @@
 srcs-y += caam_blob.c
+srcs-$(CFG_NXP_CAAM_SM_DRV) += caam_blob_sm.c
 
 incdirs-y += ../include
+

core/drivers/crypto/caam/include/caam_desc_defines.h
@@ -338,7 +338,8 @@
  */
 #define PROT_BLOB_FMT_MSTR		BIT32(1)
 #define PROT_BLOB_TYPE(type)		SHIFT_U32(1, PROT_BLOB_TYPE_##type)
-#define PROT_BLOB_TYPE_BLACK_KEY	2
+#define PROT_BLOB_TYPE_BLACK_KEY	BIT32(2)
+#define PROT_BLOB_SEC_MEM		BIT32(3)
 #define PROT_BLOB_EKT			8
 #define PROT_BLOB_INFO(aes)		SHIFT_U32(PROT_BLOB_AES_##aes, \
 						PROT_BLOB_EKT)

core/drivers/crypto/caam/include/caam_desc_helper.h
@@ -121,6 +121,14 @@ static inline void dump_desc(uint32_t *desc)
 	(CMD_LOAD_TYPE | CMD_CLASS(cla) | CMD_IMM | LOAD_DST(dst) |            \
 	 LOAD_LENGTH(len))
 
+/**
+ * @brief  Load Immediate value of length \a len to register \a dst of
+ *         class \a cla starting of register offset \a off
+ */
+#define LD_IMM_OFF(cla, dst, len, off)                                         \
+	(CMD_LOAD_TYPE | CMD_CLASS(cla) | CMD_IMM | LOAD_DST(dst) |            \
+	 LOAD_OFFSET(off) | LOAD_LENGTH(len))
+
 /*
  * Load Immediate value of length len to register dst w/o class
  */
@@ -524,7 +532,7 @@ static inline void dump_desc(uint32_t *desc)
 #define BLACK_KEY_ECB_SIZE(size) ROUNDUP(size, 16)
 
 /*
- * Sequence Inout Pointer of length len
+ * Sequence Input Pointer of length len
  */
 #define SEQ_IN_PTR(len) (CMD_SEQ_IN_TYPE | SEQ_LENGTH(len))
 
@@ -532,5 +540,10 @@ static inline void dump_desc(uint32_t *desc)
  * Sequence Output Pointer of length len
  */
 #define SEQ_OUT_PTR(len) (CMD_SEQ_OUT_TYPE | SEQ_LENGTH(len))
+
+/*
+ * Sequence Output SGT of length len
+ */
+#define SEQ_OUT_SGT_PTR(len) (CMD_SEQ_OUT_TYPE | CMD_SGT | SEQ_LENGTH(len))
 
 #endif /* __CAAM_DESC_HELPER_H__ */

core/include/drivers/caam/crypto_extension.h
@@ -68,4 +68,55 @@ struct crypto_sm_page {
 	unsigned int nb_pages;     /* Number of pages used */
 };
 #endif /* CFG_CRYPTO_DRV_SM */
+
+
+#ifdef CFG_CRYPTO_DRV_BLOB
+/*
+ * Blob size padding in bytes
+ */
+#define BLOB_BKEK_SIZE	32
+#define BLOB_MAC_SIZE	16
+#define BLOB_PAD_SIZE	(BLOB_BKEK_SIZE + BLOB_MAC_SIZE)
+
+/*
+ * Blob Key modifier is 128 bits
+ */
+#define BLOB_KEY_MODIFIER_BITS	128
+
+/*
+ * Blob encryption/decryption type
+ */
+enum crypto_blob_type {
+	BLOB_RED = 0,   /* Red Blob mode   - data in plain text */
+	BLOB_BLACK_ECB, /* Black Blob mode - data encrypted in AES ECB */
+	BLOB_BLACK_CCM, /* Black Blod mode - data encrypted in AES CCM */
+};
+
+/*
+ * Blob data structure where
+ * if encapsulation:
+ *       - payload is the input
+ *       - blob is the output
+ * if decapsulation:
+ *       - blob is the input
+ *       - payload is the output
+ */
+struct crypto_blob {
+	enum crypto_blob_type type;                /* Blob Type */
+	uint32_t key[BLOB_KEY_MODIFIER_BITS / 32]; /* Blob Key modifier */
+	struct cryptobuf payload;                  /* Payload */
+	struct cryptobuf blob;                     /* Blob */
+};
+
+#ifdef CFG_CRYPTO_DRV_SM
+/*
+ * Blob encapsulation using CAAM Secure Memory.
+ *
+ * @blob_data  [in/out] Blob data
+ */
+TEE_Result caam_blob_sm_encapsulate(struct crypto_blob *blob,
+				    struct crypto_sm_page *sm_page);
+#endif /* CFG_CRYPTO_DRV_SM */
+#endif /* CFG_CRYPTO_DRV_BLOB */
+
 #endif /* __CRYPTO_EXTENSTION_H__ */

commit a334dc43a3e7a1eff46abb791cfd29e854862a03

TEE-625 conf: remove unused configuration flag
Remove the commented line CFG_NXP_CAAM_BLOB_DRV ?= y that has been
replaced by $(eval $(call cryphw-enable-drv-hw, BLOB)).

Fixes: 4e31a46ff7 (drivers: implement NXP CAAM blob SM)
Signed-off-by: Clement Faure <clement.faure@nxp.com>

core/arch/arm/plat-imx/crypto_conf.mk
@@ -31,9 +31,6 @@ CFG_DBG_CAAM_TRACE ?= 0x2
 CFG_DBG_CAAM_DESC ?= 0x0
 CFG_DBG_CAAM_BUF ?= 0x0
 
-# Enable the BLOB module used for the hardware unique key
-# CFG_NXP_CAAM_BLOB_DRV ?= y
-
 ifeq ($(CFG_MX7ULP), y)
 # i.MX 7ULP is sharing one single line of interrupt for the 2 Job Ring interfaces
 # disable the use of interrupts in OP-TEE.