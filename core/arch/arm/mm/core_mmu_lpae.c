f7870fe9372a3c6d7c3a0273f523f7c5f03bfbf4
Nitin Garg <nitin.garg@nxp.com>
2021-03-26 18:55:52 -0500

    LFOPTEE-37: core_mmu_lpae: clear L2 tables and indexes
    In the context of Cockpit on A53, optee is not reloaded
    when rebooting. So, we need to clear the global variables
    that store MMU tables at init, else the tables will
    cumulate with previous run.
    
    Signed-off-by: Seb Fagard <sebastien.fagard@nxp.com>
    Signed-off-by: Nitin Garg <nitin.garg@nxp.com>
    Signed-off-by: Abel Vesa <abel.vesa@nxp.com>

--- core/arch/arm/mm/core_mmu_lpae.c
+++ core/arch/arm/mm/core_mmu_lpae.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: (BSD-2-Clause AND BSD-3-Clause)
 /*
  * Copyright (c) 2015-2016, 2022 Linaro Limited
+ * Copyright 2021 NXP
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -624,6 +625,14 @@ static void core_init_mmu_prtn_tee(struct mmu_partition *prtn,
 
 	/* Clear table before use */
 	memset(prtn->base_tables, 0, sizeof(base_xlation_table));
+#ifdef CFG_COCKPIT
+	/* Clear L2 table before use */
+	memset(prtn->xlat_tables, 0, XLAT_TABLES_SIZE);
+	/* Clear L2 table for TA before use */
+	memset(prtn->l2_ta_tables, 0, XLAT_TABLES_SIZE);
+	prtn->asid = 0;
+	prtn->xlat_tables_used = 0;
+#endif
 
 	for (n = 0; !core_mmap_is_end_of_table(mm + n); n++)
 		if (!core_mmu_is_dynamic_vaspace(mm + n))
