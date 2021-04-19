675c6110229f7602a3b956453c111482760680b6
Clement Faure <clement.faure@nxp.com>
2021-04-19 15:39:29 +0200

    core: ls: enabled CFG_ENABLE_EMBEDDED_TESTS by default
    Tests done by PTAs are compiled under this flag
    CFG_ENABLE_EMBEDDED_TESTS.
    Enable this flag by default for all LS platforms.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>

--- core/arch/arm/plat-ls/conf.mk
+++ core/arch/arm/plat-ls/conf.mk
@@ -10,6 +10,9 @@ $(call force,CFG_TEE_OS_DRAM0_SIZE,0x4000000)
 
 CFG_NXP_SNVS ?= y
 
+# Enable embedded tests by default
+CFG_ENABLE_EMBEDDED_TESTS ?= y
+
 CFG_CORE_HEAP_SIZE ?= 131072
 
 ifeq ($(PLATFORM_FLAVOR),ls1021atwr)
