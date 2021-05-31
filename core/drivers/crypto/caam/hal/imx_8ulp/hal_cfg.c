d6d55d163cde115c43fb572745e63459862c1283
Clement Faure <clement.faure@nxp.com>
2021-05-31 17:12:05 +0200

    LFOPTEE-48 drivers: caam: add imx8ulp CAAM HAL
    Add imx8ulp CAAM HAL functions.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>

---
+++ core/drivers/crypto/caam/hal/imx_8ulp/hal_cfg.c
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2021 NXP
+ *
+ * Brief   CAAM Configuration.
+ */
+#include <caam_hal_cfg.h>
+
+void caam_hal_cfg_setup_nsjobring(struct caam_jrcfg *jrcfg)
+{
+	caam_hal_cfg_common_setup_nsjobring(jrcfg);
+}
