f2b3ff97cffbe22fe1b79161831bf4b43d2ead6e
Clement Faure <clement.faure@nxp.com>
2021-10-19 12:30:48 +0200

    LFOPTEE-82: drivers: caam: enable the power management callbacks on 8ulp
    Enable the power management callbacks on imx8ulp to allow the CAAM
    driver to re-instantiate the CAAM RNG.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>

--- core/arch/arm/plat-imx/pm/sub.mk
+++ core/arch/arm/plat-imx/pm/sub.mk
@@ -10,6 +10,6 @@ srcs-$(CFG_MX6SX) += psci-cpuidle-imx6sx.S cpuidle-imx6sx.c
 srcs-$(CFG_MX6SL) += psci-cpuidle-imx6sl.S cpuidle-imx6sl.c
 srcs-$(CFG_MX6SLL) += psci-cpuidle-imx6sll.S cpuidle-imx6sll.c
 srcs-$(CFG_MX7ULP) += pm-imx7ulp.c psci-suspend-imx7ulp.S imx7ulp_suspend.c
-srcs-$(CFG_MX8M) += pm-imx8.c
+srcs-$(CFG_MX8M)$(CFG_MX8ULP) += pm-imx8.c
 
 cflags-psci.c-y += -Wno-suggest-attribute=noreturn
