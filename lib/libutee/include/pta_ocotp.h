139fca7d97e8a97406aeca1a2c48eb1bcf8cbea4
Clement Faure <clement.faure@nxp.com>
2021-10-19 17:37:52 +0200

    LFOPTEE-65 pta: imx: add OCOTP PTA
    Add a OCOTP PTA for i.MX platforms. This PTA exposes two commands:
     - the read of OCOTP shadow registers.
     - the read of the platform UID.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>

---
+++ lib/libutee/include/pta_ocotp.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2021 NXP
+ */
+#ifndef __PTA_OCOTP_H__
+#define __PTA_OCOTP_H__
+
+#define PTA_OCOTP_UUID \
+	{ \
+		0x9abdf255, 0xd8fa, 0x40de, \
+		{ \
+			0x8f, 0x60, 0x4d, 0x0b, 0x27, 0x92, 0x7b, 0x7d \
+		} \
+	}
+
+#define PTA_OCOTP_CHIP_UID  1
+#define PTA_OCOTP_READ_FUSE 2
+
+#endif /* __PTA_OCOTP_H__ */
