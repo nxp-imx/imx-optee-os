nxp-imx/imx-optee-os

commit 5a238abd466b5a1652c4daa78544c52871bba30c

LFOPTEE-3 core: Do not modify embedded dtb.
when compile with CFG_EMBED_DT, a dtb is embedded in the
firmware. Do not modify it.

Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>

core/include/kernel/dt.h
@@ -117,8 +117,14 @@ int dt_disable_status(void *fdt, int node);
  *
  * Returns 0 on success or -1 on failure
  */
+#if !defined(CFG_EMBED_DT)
 int dt_enable_secure_status(void *fdt, int node);
-
+#else
+static inline int dt_enable_secure_status(void *fdt __unused, int node __unused)
+{
+	return 0;
+}
+#endif
 /*
  * FDT manipulation functions, not provided by <libfdt.h>
  */

core/kernel/dt.c
@@ -83,6 +83,7 @@ int dt_disable_status(void *fdt, int node)
 	return 0;
 }
 
+#if !defined(CFG_EMBED_DT)
 int dt_enable_secure_status(void *fdt, int node)
 {
 	if (dt_disable_status(fdt, node)) {
@@ -95,6 +96,7 @@ int dt_enable_secure_status(void *fdt, int node)
 
 	return 0;
 }
+#endif
 
 int dt_map_dev(const void *fdt, int offs, vaddr_t *base, size_t *size)
 {

commit a7c3788a4d3f4623312a04c5b9e0d659546909d7

LF-4824 core: update CFG_EMBED_DT flag to CFG_EMBED_DTB flag
update CFG_EMBED_DT flag to CFG_EMBED_DTB flag. The flag CFG_EMBED_DT
does not exist and is not defined.

Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>
Fixes: 5a238ab ("LFOPTEE-3 core: Do not modify embedded dtb.")

core/include/kernel/dt.h
@@ -117,7 +117,7 @@ int dt_disable_status(void *fdt, int node);
  *
  * Returns 0 on success or -1 on failure
  */
-#if !defined(CFG_EMBED_DT)
+#if !defined(CFG_EMBED_DTB)
 int dt_enable_secure_status(void *fdt, int node);
 #else
 static inline int dt_enable_secure_status(void *fdt __unused, int node __unused)

core/kernel/dt.c
@@ -83,7 +83,7 @@ int dt_disable_status(void *fdt, int node)
 	return 0;
 }
 
-#if !defined(CFG_EMBED_DT)
+#if !defined(CFG_EMBED_DTB)
 int dt_enable_secure_status(void *fdt, int node)
 {
 	if (dt_disable_status(fdt, node)) {