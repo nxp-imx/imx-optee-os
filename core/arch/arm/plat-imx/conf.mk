520d38f60538ea244e5a18b4444003fa5a541887
Clement Faure <clement.faure@nxp.com>
2021-11-17 17:28:58 +0100

    LF-4794 imx: imx8ulp: move OPTEE OS start address
    To allow the S400 to reach the OPTEE OS image and authenticate it, move
    the OPTEE OS binary to 0xa6000000.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>

--- core/arch/arm/plat-imx/conf.mk
+++ core/arch/arm/plat-imx/conf.mk
@@ -520,10 +520,11 @@ ifeq ($(CFG_ARM64_core),y)
 CFG_DT ?= y
 CFG_EXTERNAL_DTB_OVERLAY = y
 #few special case to handle
-ifneq (,$(filter y, $(CFG_MX8MN) $(CFG_MX8MP) $(CFG_MX8DX) $(CFG_MX8DXL) \
-		    $(CFG_MX8ULP)))
+ifneq (,$(filter y, $(CFG_MX8MN) $(CFG_MX8MP) $(CFG_MX8DX) $(CFG_MX8DXL)))
 # New device will have base addresses within the first 1GB of DDR
 CFG_TZDRAM_START ?= ($(CFG_DRAM_BASE) + 0x16000000)
+else ifneq (,$(filter y, $(CFG_MX8ULP)))
+CFG_TZDRAM_START ?= ($(CFG_DRAM_BASE) + 0x26000000)
 else
 # for backward compatibility all the other will keep existing location at the end of DDR.
 # put optee end of ddr for AARCH64
