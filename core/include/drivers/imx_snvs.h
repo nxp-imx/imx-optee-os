25384273d14cf282e6147efa98a4d305670cd7ad
Clement Faure <clement.faure@nxp.com>
2022-03-01 11:09:56 +0100

    drivers: imx_snvs: add master key selection
    Select the OTPMK as the SNVS master key when the platforms is in closed
    state.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>

--- core/include/drivers/imx_snvs.h
+++ core/include/drivers/imx_snvs.h
@@ -2,8 +2,21 @@
 /*
  * Copyright (C) 2020 Pengutronix
  * Rouven Czerwinski <entwicklung@pengutronix.de>
+ * Copyright 2022 NXP
  */
 #ifndef __DRIVERS_IMX_SNVS_H
 #define __DRIVERS_IMX_SNVS_H
+
+#include <tee_api_types.h>
+
+/* Set the OTPMK Key as Master key */
+#ifdef CFG_IMX_SNVS
+TEE_Result imx_snvs_set_master_otpmk(void);
+#else
+static inline TEE_Result imx_snvs_set_master_otpmk(void)
+{
+	return TEE_ERROR_NOT_IMPLEMENTED;
+}
+#endif
 
 #endif /* __DRIVERS_IMX_SNVS_H */
