3f2c86ea7eb01f71a069008952766912ebc78755
Clement Faure <clement.faure@nxp.com>
2022-03-21 16:36:45 +0100

    TEE-517 core: imx: Add dynamic shared memory configuration
    Add dynamic share memory configuration. Enable it for i.MX 8mscale
    family except for i.MX 8MP.
    Enable DEK Blob support, and put a warning if dynamic share memory is
    not enabled, as it depends on this feature.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>
    Signed-off-by: Silvano di Ninno <silvano.dininno@nxp.com>

--- core/arch/arm/plat-imx/platform_config.h
+++ core/arch/arm/plat-imx/platform_config.h
@@ -66,4 +66,21 @@
 #include <config/imx6sll.h>
 #endif
 
+/*
+ * Calculate Non Secure memory region, after Secure memory carved out.
+ * Assumption is memory for TEE is 32M
+ * Those defines are used to register dynamic shared memory
+ * Currently enable only mscale family except 8MP.
+ */
+#if defined(CFG_MX8MQ) || defined(CFG_MX8MM) || defined(CFG_MX8MN)
+#define DRAM0_NSEC_BASE CFG_DRAM_BASE
+#define DRAM0_NSEC_SIZE (CFG_TZDRAM_START - CFG_DRAM_BASE)
+#define DRAM1_NSEC_BASE (CFG_TZDRAM_START + 0x2000000)
+#define DRAM1_NSEC_SIZE ((CFG_DRAM_BASE - DRAM1_NSEC_BASE) + CFG_DDR_SIZE)
+#endif
+
+#if defined(CFG_IMX_DEK_HAB) && !defined(DRAM0_NSEC_SIZE)
+#warning HAB DEK Blob is enabled but it requires dynamic shared memory \
+and this feature is not enabled !!
+#endif
 #endif /*PLATFORM_CONFIG_H*/
