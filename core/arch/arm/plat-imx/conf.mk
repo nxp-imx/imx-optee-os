4a56e38d99efbd5f68088ae142aa4e275aa0e8ce
Clement Faure <clement.faure@nxp.com>
2022-03-23 18:41:17 +0100

    core: imx: enable TZASC driver for all i.MX platforms
    Enable TZASC for all i.MX platforms.
    Force the disable of TZASC on mx7ulp, imx8q and imx8d.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>

--- core/arch/arm/plat-imx/conf.mk
+++ core/arch/arm/plat-imx/conf.mk
@@ -200,6 +200,7 @@ CFG_DRAM_BASE ?= 0x80000000
 CFG_TEE_CORE_NB_CORE ?= 6
 $(call force,CFG_IMX_OCOTP,n)
 $(call force,CFG_SC_IPC_BASE,SC_IPC0_BASE)
+$(call force,CFG_TZC380,n)
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx8qx-flavorlist)))
 $(call force,CFG_MX8QX,y)
 $(call force,CFG_ARM64_core,y)
@@ -209,6 +210,7 @@ CFG_DRAM_BASE ?= 0x80000000
 CFG_TEE_CORE_NB_CORE ?= 4
 $(call force,CFG_IMX_OCOTP,n)
 $(call force,CFG_SC_IPC_BASE,SC_IPC0_BASE)
+$(call force,CFG_TZC380,n)
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx8dxl-flavorlist)))
 $(call force,CFG_MX8DXL,y)
 $(call force,CFG_ARM64_core,y)
@@ -219,6 +221,7 @@ $(call force,CFG_TEE_CORE_NB_CORE,2)
 $(call force,CFG_IMX_OCOTP,n)
 $(call force,CFG_NXP_CAAM,n)
 $(call force,CFG_SC_IPC_BASE,SC_IPC0_BASE)
+$(call force,CFG_TZC380,n)
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx8ulp-flavorlist)))
 $(call force,CFG_MX8ULP,y)
 $(call force,CFG_ARM64_core,y)
@@ -228,6 +231,7 @@ CFG_TEE_CORE_NB_CORE ?= 2
 $(call force,CFG_NXP_SNVS,n)
 $(call force,CFG_IMX_OCOTP,n)
 $(call force,CFG_SC_IPC_BASE,SC_IPC0_BASE)
+$(call force,CFG_TZC380,n)
 else
 $(error Unsupported PLATFORM_FLAVOR "$(PLATFORM_FLAVOR)")
 endif
@@ -546,6 +550,7 @@ $(call force,CFG_IMX_OCOTP,n)
 endif
 CFG_IMX_OCOTP ?= y
 CFG_IMX_DIGPROG ?= y
+CFG_TZC380 ?= y
 
 # Almost all platforms include CAAM HW Modules, except the
 # ones forced to be disabled
