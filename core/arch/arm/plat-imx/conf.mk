e09654534f03772ad54519c1dfdef443edb40571
Clement Faure <clement.faure@nxp.com>
2022-03-24 09:55:21 +0100

    core: imx: enable CFG_CORE_HUK_SUBKEY_COMPAT_USE_OTP_DIE_ID by default
    Use the platform tee_otp_get_die_id() implementation to generate the SSK
    key.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>

--- core/arch/arm/plat-imx/conf.mk
+++ core/arch/arm/plat-imx/conf.mk
@@ -551,6 +551,7 @@ endif
 CFG_IMX_OCOTP ?= y
 CFG_IMX_DIGPROG ?= y
 CFG_TZC380 ?= y
+CFG_CORE_HUK_SUBKEY_COMPAT_USE_OTP_DIE_ID ?= y
 
 # Almost all platforms include CAAM HW Modules, except the
 # ones forced to be disabled
