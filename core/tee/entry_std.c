nxp-imx/imx-optee-os

commit 717c024a8d7eab2e214401a70f474fc5d3ed2350

TEE-526 core: arm: retrieve SDP Memory cacheability
Add function tee_entry_is_sdp_cacched to retrieve the SDP Memory
cacheability

Signed-off-by: Cedric Neveux <cedric.neveux@nxp.com>

core/tee/entry_std.c
@@ -587,6 +587,18 @@ err:
 	return res;
 }
 
+bool tee_entry_is_sdp_cached(void)
+{
+#ifdef CFG_SECURE_DATA_PATH
+	uint32_t cattr;
+
+	if (!mobj_get_cattr(*sdp_mem_mobjs, &cattr))
+		return cattr & TEE_MATTR_CACHE_CACHED;
+#endif
+
+	return false;
+}
+
 static TEE_Result default_mobj_init(void)
 {
 #ifdef CFG_CORE_RESERVED_SHM

core/include/tee/entry_std.h
@@ -23,4 +23,7 @@ TEE_Result __tee_entry_std(struct optee_msg_arg *arg, uint32_t num_params);
 /* Get list head for sessions opened from non-secure */
 void nsec_sessions_list_head(struct tee_ta_session_head **open_sessions);
 
+/* Retrieve SDP mem cacheability */
+bool tee_entry_is_sdp_cached(void);
+
 #endif /* TEE_ENTRY_STD_H */

commit a0359fd6df523e64cc1e4d2fc84f3d15d7f2362e

LFOPTEE-211 core: update tee_entry_is_sdp_cached
Take in consideration change introduce by
commit 8afe7a7c5220
("core: rename mobj_get_cattr() to mobj_get_mem_type()")
and
commit 8b427282e747
("core: change TEE_MATTR_CACHE_ to TEE_MATTR_MEM_TYPE_")

to the fix following build errors
core/tee/entry_std.c: In function 'tee_entry_is_sdp_cached':
core/tee/entry_std.c:595:14: error: implicit declaration of function
'mobj_get_cattr'; did you mean 'mobj_get_pa'?
[-Werror=implicit-function-declaration]
  595 |         if (!mobj_get_cattr(*sdp_mem_mobjs, &cattr))
      |              ^~~~~~~~~~~~~~
      |              mobj_get_pa
core/tee/entry_std.c:596:32: error: 'TEE_MATTR_CACHE_CACHED' undeclared
(first use in this function); did you mean 'TEE_MATTR_MEM_TYPE_CACHED'?
  596 |                 return cattr & TEE_MATTR_CACHE_CACHED;
      |                                ^~~~~~~~~~~~~~~~~~~~~~
      |                                TEE_MATTR_MEM_TYPE_CACHED

Fixes: 717c024a8d7e ("TEE-526 core: arm: retrieve SDP Memory cacheability")
Signed-off-by: Olivier Masse <olivier.masse@nxp.com>

core/tee/entry_std.c
@@ -590,10 +590,10 @@ err:
 bool tee_entry_is_sdp_cached(void)
 {
 #ifdef CFG_SECURE_DATA_PATH
-	uint32_t cattr;
+	uint32_t mem_type = 0;
 
-	if (!mobj_get_cattr(*sdp_mem_mobjs, &cattr))
-		return cattr & TEE_MATTR_CACHE_CACHED;
+	if (!mobj_get_mem_type(*sdp_mem_mobjs, &mem_type))
+		return mem_type & TEE_MATTR_MEM_TYPE_CACHED;
 #endif
 
 	return false;