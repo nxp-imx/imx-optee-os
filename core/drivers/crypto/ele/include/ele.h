42ff1e33d3660f35954fc8877814a159547d466c
Sahil Malhotra <sahil.malhotra@nxp.com>
2023-01-25 17:06:24 +0530

    LFOPTEE-178 drivers: ele: Add Generate/Delete Key APIs
    Add Generate/Delete Key APIs
    
    Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>

--- core/drivers/crypto/ele/include/ele.h
+++ core/drivers/crypto/ele/include/ele.h
@@ -7,6 +7,7 @@
 #define __ELE_H_
 
 #include <drivers/imx_mu.h>
+#include <utils_mem.h>
 #include <tee_api_types.h>
 #include <trace.h>
 
@@ -57,5 +58,41 @@ TEE_Result imx_ele_key_mgmt_open(uint32_t key_store_handle,
  * @key_mgmt_handle: EdgeLock Enclave key management handle
  */
 TEE_Result imx_ele_key_mgmt_close(uint32_t key_mgmt_handle);
+
+/*
+ * Generate a Key be it Asymmetric or Symmetric
+ *
+ * @key_mgmt_handle: EdgeLock Enclave key management handle
+ * @public_key_size: Size in bytes of the output where to store the generated
+ *		     Key. It must be 0 if a symmetric key is generated.
+ *		     If the size is different than 0, EdgeLock Enclave will
+ *		     attempt to copy the public key for asymmetric algorithm.
+ * @key_group: Indicates the generated key group.
+ * @flags: Flags for operation on key.(Strict operation)
+ * @key_lifetime: Lifetime of the key (Volatile or Persistent)
+ * @key_usage: Defines cryptographic operations that key can execute.
+ * @key_type: Defines Key type
+ * @key_size: Key Size in bits
+ * @permitted_algo: Defines algorithms in which key can be used.
+ * @public_key_addr: In case of Asymmetric Key, address to where Edgelock
+ *		Enclave will copy the Public Key.
+ * @key_identifier: Identifier of the generated key
+ */
+TEE_Result imx_ele_generate_key(uint32_t key_mgmt_handle,
+				size_t public_key_size, uint16_t key_group,
+				uint8_t flags, uint32_t key_lifetime,
+				uint32_t key_usage, uint16_t key_type,
+				size_t key_size, uint32_t permitted_algo,
+				uint8_t *public_key_addr,
+				uint32_t *key_identifier);
+
+/*
+ * Delete a Key
+ *
+ * @key_mgmt_handle: EdgeLock Enclave key management handle
+ * @key_identifier: Identifier of key to be deleted
+ */
+TEE_Result imx_ele_delete_key(uint32_t key_mgmt_handle,
+			      uint32_t key_identifier);
 
 #endif /* __ELE_H_ */
--- core/drivers/crypto/ele/key_mgmt.c
+++ core/drivers/crypto/ele/key_mgmt.c
@@ -5,9 +5,12 @@
 #include <ele.h>
 #include <mm/core_memprot.h>
 #include <string.h>
+#include <utils_mem.h>
 
 #define ELE_CMD_KEY_MGMT_OPEN  0x40
 #define ELE_CMD_KEY_MGMT_CLOSE 0x41
+#define ELE_CMD_GENERATE_KEY   0x42
+#define ELE_CMD_DELETE_KEY     0x4E
 
 /*
  * Open a key management session with EdgeLock Enclave.
@@ -84,4 +87,129 @@ TEE_Result imx_ele_key_mgmt_close(uint32_t key_mgmt_handle)
 	memcpy(msg.data.u8, &cmd, sizeof(cmd));
 
 	return imx_ele_call(&msg);
+}
+
+TEE_Result imx_ele_generate_key(uint32_t key_mgmt_handle,
+				size_t public_key_size, uint16_t key_group,
+				uint8_t flags, uint32_t key_lifetime,
+				uint32_t key_usage, uint16_t key_type,
+				size_t key_size, uint32_t permitted_algo,
+				uint8_t *public_key_addr,
+				uint32_t *key_identifier)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+
+	struct imx_mu_msg msg = {};
+	struct imx_ele_buf public_key = {};
+	struct gen_key_msg_cmd {
+		uint32_t key_mgmt_handle;
+		uint32_t key_id;
+		uint16_t public_key_size;
+		uint16_t key_group;
+		uint16_t key_type;
+		uint16_t key_size;
+		uint32_t key_lifetime;
+		uint32_t key_usage;
+		uint32_t permitted_algo;
+		uint8_t flags;
+		uint8_t reserved[3];
+		uint32_t public_key_addr;
+		uint32_t crc;
+	} __packed cmd = {};
+
+	struct gen_key_msg_rsp {
+		uint32_t rsp_code;
+		uint32_t key_identifier;
+		uint16_t pub_key_size;
+		uint16_t reserved;
+	} rsp = {};
+
+	if (!key_identifier || !public_key_addr)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	res = imx_ele_buf_alloc(&public_key, NULL, public_key_size);
+	if (res != TEE_SUCCESS) {
+		EMSG("Public key memory allocation failed");
+		return res;
+	}
+
+	cmd.key_mgmt_handle = key_mgmt_handle;
+	cmd.key_id = 0;
+	cmd.public_key_size = (uint16_t)public_key.size;
+	cmd.key_group = key_group;
+	cmd.key_type = key_type;
+	cmd.key_size = (uint16_t)key_size;
+	cmd.key_lifetime = key_lifetime;
+	cmd.key_usage = key_usage;
+	cmd.permitted_algo = permitted_algo;
+	cmd.flags = flags;
+	cmd.public_key_addr = public_key.paddr;
+	cmd.crc = 0;
+
+	msg.header.version = ELE_VERSION_HSM;
+	msg.header.size = SIZE_MSG_32(cmd);
+	msg.header.tag = ELE_REQUEST_TAG;
+	msg.header.command = ELE_CMD_GENERATE_KEY;
+
+	memcpy(msg.data.u8, &cmd, sizeof(cmd));
+	update_crc(&msg);
+
+	res = imx_ele_call(&msg);
+	if (res != TEE_SUCCESS) {
+		EMSG("Failed to generate key res = %" PRIx32, res);
+		goto out;
+	}
+
+	imx_ele_buf_cache_op(TEE_CACHEINVALIDATE, &public_key);
+
+	res = imx_ele_buf_copy(&public_key, public_key_addr, public_key_size);
+	if (res != TEE_SUCCESS) {
+		EMSG("Public key copy failed");
+		goto out;
+	}
+
+	memcpy(&rsp, msg.data.u8, sizeof(rsp));
+
+	*key_identifier = rsp.key_identifier;
+
+out:
+	imx_ele_buf_free(&public_key);
+	return res;
+}
+
+TEE_Result imx_ele_delete_key(uint32_t key_mgmt_handle, uint32_t key_identifier)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+
+	struct delete_key_msg_cmd {
+		uint32_t key_mgmt_handle;
+		uint32_t key_identifier;
+		uint16_t rsvd1;
+		uint8_t flags;
+		uint8_t rsvd2;
+	} __packed cmd = {
+		.key_mgmt_handle = key_mgmt_handle,
+		.key_identifier = key_identifier,
+		.rsvd1 = 0,
+		.flags = 0,
+		.rsvd2 = 0,
+	};
+
+	struct imx_mu_msg msg = {
+		.header.version = ELE_VERSION_HSM,
+		.header.size = SIZE_MSG_32(cmd),
+		.header.tag = ELE_REQUEST_TAG,
+		.header.command = ELE_CMD_DELETE_KEY,
+	};
+
+	memcpy(msg.data.u8, &cmd, sizeof(cmd));
+	update_crc(&msg);
+
+	res = imx_ele_call(&msg);
+	if (res != TEE_SUCCESS) {
+		EMSG("Failed to delete key res = %" PRIx32, res);
+		return res;
+	}
+
+	return TEE_SUCCESS;
 }
