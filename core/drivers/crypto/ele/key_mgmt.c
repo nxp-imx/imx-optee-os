abfdb0fe4526b834953bb80ecbd0e56b35f5124b
Sahil Malhotra <sahil.malhotra@nxp.com>
2023-01-29 11:21:11 +0530

    LFOPTEE-178 drivers: ele: Add Key Management APIs
    Add Key Management Open/Close APIs
    
    Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>

---
+++ core/drivers/crypto/ele/key_mgmt.c
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright 2023 NXP
+ */
+#include <ele.h>
+#include <mm/core_memprot.h>
+#include <string.h>
+
+#define ELE_CMD_KEY_MGMT_OPEN  0x40
+#define ELE_CMD_KEY_MGMT_CLOSE 0x41
+
+/*
+ * Open a key management session with EdgeLock Enclave.
+ *
+ * @key_store_handle: EdgeLock Enclave key store handle
+ * @key_mgmt_handle: EdgeLock Enclave Key management handle
+ */
+TEE_Result imx_ele_key_mgmt_open(uint32_t key_store_handle,
+				 uint32_t *key_mgmt_handle)
+{
+	TEE_Result res = TEE_ERROR_GENERIC;
+	struct key_mgmt_open_cmd {
+		uint32_t key_store_handle;
+		uint32_t msbi;
+		uint32_t msbo;
+		uint8_t flags;
+		uint8_t reserved[3];
+		uint32_t crc;
+	} __packed cmd = {
+		.key_store_handle = key_store_handle,
+		.msbi = 0,
+		.msbo = 0,
+		.flags = 0,
+		.reserved = {},
+		.crc = 0,
+	};
+
+	struct key_mgmt_open_msg_rsp {
+		uint32_t rsp_code;
+		uint32_t key_mgmt_handle;
+	} rsp = {};
+
+	struct imx_mu_msg msg = {
+		.header.version = ELE_VERSION_HSM,
+		.header.size = SIZE_MSG_32(cmd),
+		.header.tag = ELE_REQUEST_TAG,
+		.header.command = ELE_CMD_KEY_MGMT_OPEN,
+	};
+
+	if (!key_mgmt_handle)
+		return TEE_ERROR_BAD_PARAMETERS;
+
+	memcpy(msg.data.u8, &cmd, sizeof(cmd));
+	update_crc(&msg);
+
+	res = imx_ele_call(&msg);
+	if (res != TEE_SUCCESS) {
+		EMSG("Failed to open key management service flow");
+		return res;
+	}
+
+	memcpy(&rsp, msg.data.u8, sizeof(rsp));
+
+	*key_mgmt_handle = rsp.key_mgmt_handle;
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result imx_ele_key_mgmt_close(uint32_t key_mgmt_handle)
+{
+	struct key_mgmt_close_cmd {
+		uint32_t key_mgmt_handle;
+	} cmd = {
+		.key_mgmt_handle = key_mgmt_handle,
+	};
+
+	struct imx_mu_msg msg = {
+		.header.version = ELE_VERSION_HSM,
+		.header.size = SIZE_MSG_32(cmd),
+		.header.tag = ELE_REQUEST_TAG,
+		.header.command = ELE_CMD_KEY_MGMT_CLOSE,
+	};
+
+	memcpy(msg.data.u8, &cmd, sizeof(cmd));
+
+	return imx_ele_call(&msg);
+}
--- core/drivers/crypto/ele/sub.mk
+++ core/drivers/crypto/ele/sub.mk
@@ -3,3 +3,4 @@ incdirs-y += include
 srcs-y += ele.c
 srcs-y += memutils.c
 srcs-y += key_store.c
+srcs-y += key_mgmt.c
