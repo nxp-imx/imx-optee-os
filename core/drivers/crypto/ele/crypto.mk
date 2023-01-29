c03258dcd6be3189e99c7fd5c625b4adf54fcb76
Sahil Malhotra <sahil.malhotra@nxp.com>
2023-01-29 11:21:41 +0530

    LFOPTEE-178 drivers: ele: Add support for ECC operations
    This patch add support for:
    1. ECC Key Generation
    2. ECC Signing/Verification operation.
    
    Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>

--- core/drivers/crypto/ele/crypto.mk
+++ core/drivers/crypto/ele/crypto.mk
@@ -1,4 +1,16 @@
 ifeq ($(CFG_IMX_ELE),y)
+CFG_IMX_ELE_ECC_DRV ?= n
+CFG_IMX_ELE_ACIPHER_DRV ?= $(CFG_IMX_ELE_ECC_DRV)
+
+# If IMX ELE Driver is supported, the Crypto Driver interfacing
+# it with generic crypto API can be enabled.
+CFG_CRYPTO_DRIVER ?= $(CFG_IMX_ELE_ACIPHER_DRV)
+
+ifeq ($(CFG_CRYPTO_DRIVER),y)
+CFG_CRYPTO_DRIVER_DEBUG ?= 0
+CFG_CRYPTO_DRV_ECC ?= $(CFG_IMX_ELE_ECC_DRV)
+CFG_CRYPTO_DRV_ACIPHER ?= $(CFG_IMX_ELE_ACIPHER_DRV)
+endif # CFG_CRYPTO_DRIVER
 
 # Issues in the ELE FW prevent OPTEE and Kernel from using
 # the RNG concurrently at runtime. To prevent any issue,
