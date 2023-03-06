c7ebfce4c15f9bc55c33a86c25f0c9abdebcd874
Sahil Malhotra <sahil.malhotra@nxp.com>
2023-03-06 17:51:04 +0530

    LFOPTEE-178 drivers: ele: Create a global key store handle for all subsequent calls
    Creating a global session handle for use in subsequent calls.
    
    Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>

--- core/drivers/crypto/ele/ele.c
+++ core/drivers/crypto/ele/ele.c
@@ -9,6 +9,7 @@
 #include <kernel/delay.h>
 #include <kernel/panic.h>
 #include <kernel/tee_common_otp.h>
+#include <key_store.h>
 #include <memutils.h>
 #include <mm/core_memprot.h>
 #include <mm/core_mmu.h>
@@ -417,11 +418,19 @@ static TEE_Result imx_ele_global_data_init(void)
 {
 	TEE_Result res = TEE_ERROR_GENERIC;
 	uint32_t session_handle = 0;
+	uint32_t key_store_handle = 0;
 
 	res = imx_ele_get_global_session_handle(&session_handle);
-	if (res)
+	if (res) {
 		EMSG("Failed to open global session");
+		goto err;
+	}
 
+	res = imx_ele_get_global_key_store_handle(&key_store_handle);
+	if (res)
+		EMSG("Failed to open global key store");
+
+err:
 	return res;
 }
 
