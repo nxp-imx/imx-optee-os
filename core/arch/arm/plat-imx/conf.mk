5eebee8114d953c83e91d10d2457f86a1ec24dbf
Clement Faure <clement.faure@nxp.com>
2023-04-17 16:26:08 +0200

    LFOPTEE-37: plat-imx: add platforms mx8qm mek cockpit a53 and a72
    based on arch imx8qm, those platforms flavors differences are:
    - core numbers:
      A53 cluster is 1st-4th core, A72 cluster is 5th-6th core.
    - RAM max size is divided by 2.
    the RAM starts at: 0x8000000 for A53, 0xC000000 for A72
    - debug uart0 for A53, uart2 for A72.
    - Disable NXP CAAM for both cockpit domains
    
    Signed-off-by: Seb Fagard <sebastien.fagard@nxp.com>
    Signed-off-by: Nitin Garg <nitin.garg@nxp.com>
    Signed-off-by: Abel Vesa <abel.vesa@nxp.com>

--- core/arch/arm/plat-imx/conf.mk
+++ core/arch/arm/plat-imx/conf.mk
@@ -75,6 +75,8 @@ mx8mp-flavorlist = \
 
 mx8qm-flavorlist = \
 	mx8qmmek \
+	mx8qmmekcockpita53 \
+	mx8qmmekcockpita72 \
 
 mx8qx-flavorlist = \
 	mx8qxpmek \
@@ -198,6 +200,7 @@ CFG_IMX_LPUART ?= y
 CFG_DRAM_BASE ?= 0x80000000
 CFG_TEE_CORE_NB_CORE ?= 6
 $(call force,CFG_IMX_OCOTP,n)
+$(call force,CFG_SC_IPC_BASE,SC_IPC0_BASE)
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx8qx-flavorlist)))
 $(call force,CFG_MX8QX,y)
 $(call force,CFG_ARM64_core,y)
@@ -206,6 +209,7 @@ CFG_IMX_LPUART ?= y
 CFG_DRAM_BASE ?= 0x80000000
 CFG_TEE_CORE_NB_CORE ?= 4
 $(call force,CFG_IMX_OCOTP,n)
+$(call force,CFG_SC_IPC_BASE,SC_IPC0_BASE)
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx8dxl-flavorlist)))
 $(call force,CFG_MX8DXL,y)
 $(call force,CFG_ARM64_core,y)
@@ -215,6 +219,7 @@ CFG_DRAM_BASE ?= 0x80000000
 $(call force,CFG_TEE_CORE_NB_CORE,2)
 $(call force,CFG_IMX_OCOTP,n)
 $(call force,CFG_NXP_CAAM,n)
+$(call force,CFG_SC_IPC_BASE,SC_IPC0_BASE)
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx8ulp-flavorlist)))
 $(call force,CFG_MX8ULP,y)
 $(call force,CFG_ARM64_core,y)
@@ -223,6 +228,7 @@ CFG_DRAM_BASE ?= 0x80000000
 CFG_TEE_CORE_NB_CORE ?= 2
 $(call force,CFG_NXP_SNVS,n)
 $(call force,CFG_IMX_OCOTP,n)
+$(call force,CFG_SC_IPC_BASE,SC_IPC0_BASE)
 CFG_IMX_MU ?= y
 CFG_IMX_ELE ?= y
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx93-flavorlist)))
@@ -404,6 +410,24 @@ CFG_UART_BASE ?= UART0_BASE
 CFG_NSEC_DDR_1_BASE ?= 0x880000000UL
 CFG_NSEC_DDR_1_SIZE  ?= 0x380000000UL
 CFG_CORE_ARM64_PA_BITS ?= 40
+endif
+
+ifneq (,$(filter $(PLATFORM_FLAVOR),mx8qmmekcockpita53))
+CFG_DRAM_BASE := 0x80000000
+CFG_DDR_SIZE ?= 0x40000000
+CFG_UART_BASE ?= UART0_BASE
+CFG_TEE_CORE_NB_CORE = 4
+$(call force,CFG_NXP_CAAM,n)
+CFG_COCKPIT ?= y
+endif
+
+ifneq (,$(filter $(PLATFORM_FLAVOR),mx8qmmekcockpita72))
+CFG_DRAM_BASE := 0xC0000000
+CFG_DDR_SIZE ?= 0x40000000
+CFG_UART_BASE ?= UART2_BASE
+CFG_SC_IPC_BASE = SC_IPC3_BASE
+$(call force,CFG_NXP_CAAM,n)
+CFG_COCKPIT ?= y
 endif
 
 ifneq (,$(filter $(PLATFORM_FLAVOR),mx8dxlevk))
