9df96433804cc6579ad9bff96935fa4b1f9524d9
Clement Faure <clement.faure@nxp.com>
2023-04-17 14:58:02 +0200

    pm: imx: add power management drivers
    Add power management drivers for imx6, imx7 and imx8 platforms.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>

---
+++ core/drivers/pm/imx/gpcv2.c
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (C) 2017 NXP
+ *
+ * Peng Fan <peng.fan@nxp.com>
+ */
+
+#include <imx.h>
+#include <imx_pm.h>
+#include <io.h>
+#include <mm/core_memprot.h>
+#include <platform_config.h>
+#include <stdint.h>
+
+#define GPC_PGC_C1			0x840
+#define GPC_PGC_C1_PUPSCR		0x844
+
+#define GPC_PGC_PCG_MASK		BIT(0)
+
+#define GPC_CPU_PGC_SW_PUP_REQ		0xf0
+#define GPC_PU_PGC_SW_PUP_REQ		0xf8
+#define GPC_CPU_PGC_SW_PDN_REQ		0xfc
+#define GPC_PU_PGC_SW_PDN_REQ		0x104
+#define GPC_PGC_SW_PDN_PUP_REQ_CORE1_MASK BIT(1)
+
+static vaddr_t gpc_base(void)
+{
+	return core_mmu_get_va(GPC_BASE, MEM_AREA_IO_SEC, 1);
+}
+
+void imx_gpcv2_set_core_pgc(bool enable, uint32_t offset)
+{
+	uint32_t val = io_read32(gpc_base() + offset) & (~GPC_PGC_PCG_MASK);
+
+	if (enable)
+		val |= GPC_PGC_PCG_MASK;
+
+	io_write32(gpc_base() + offset, val);
+}
+
+void imx_gpcv2_set_core1_pdn_by_software(void)
+{
+	uint32_t val = io_read32(gpc_base() + GPC_CPU_PGC_SW_PDN_REQ);
+
+	imx_gpcv2_set_core_pgc(true, GPC_PGC_C1);
+
+	val |= GPC_PGC_SW_PDN_PUP_REQ_CORE1_MASK;
+
+	io_write32(gpc_base() + GPC_CPU_PGC_SW_PDN_REQ, val);
+
+	while ((io_read32(gpc_base() + GPC_CPU_PGC_SW_PDN_REQ) &
+	       GPC_PGC_SW_PDN_PUP_REQ_CORE1_MASK) != 0)
+		;
+
+	imx_gpcv2_set_core_pgc(false, GPC_PGC_C1);
+}
+
+void imx_gpcv2_set_core1_pup_by_software(void)
+{
+	uint32_t val = io_read32(gpc_base() + GPC_CPU_PGC_SW_PUP_REQ);
+
+	imx_gpcv2_set_core_pgc(true, GPC_PGC_C1);
+
+	val |= GPC_PGC_SW_PDN_PUP_REQ_CORE1_MASK;
+
+	io_write32(gpc_base() + GPC_CPU_PGC_SW_PUP_REQ, val);
+
+	while ((io_read32(gpc_base() + GPC_CPU_PGC_SW_PUP_REQ) &
+	       GPC_PGC_SW_PDN_PUP_REQ_CORE1_MASK) != 0)
+		;
+
+	imx_gpcv2_set_core_pgc(false, GPC_PGC_C1);
+}
---
+++ core/drivers/pm/imx/sub.mk
@@ -0,0 +1,14 @@
+global-incdirs-y += .
+
+subdirs-$(CFG_MX6)$(CFG_MX7) += busfreq
+subdirs-y += cpuidle
+subdirs-y += suspend
+
+srcs-$(CFG_PSCI_ARM32) += psci.c imx_ocram.c
+srcs-$(CFG_MX7) += pm-imx7.c gpcv2.c
+srcs-$(CFG_MX6) += pm-imx6.c
+srcs-$(CFG_MX6)$(CFG_MX7) +=  imx_src.c mmdc.c
+srcs-$(CFG_MX7ULP) += pm-imx7ulp.c
+srcs-$(CFG_MX8M)$(CFG_MX8ULP) += pm-imx8.c
+
+asm-defines-y += imx_pm_asm_defines.c
