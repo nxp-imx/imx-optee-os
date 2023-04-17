1e79f969e1af3abb6d87ce2aafeed51536b18bb9
Clement Faure <clement.faure@nxp.com>
2023-04-17 11:35:21 +0200

    core: imx: remove power management code for imx7 platforms
    Remove all power management related code of imx7 platforms. This code is
    outdated and no longer operational on recent releases.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>

--- core/arch/arm/plat-imx/imx_sip.h
+++ core/arch/arm/plat-imx/imx_sip.h
@@ -6,10 +6,6 @@
 #ifndef __IMX_SIP_H__
 #define __IMX_SIP_H__
 
-#define IMX_SIP_PL310_ENABLE			1
-#define IMX_SIP_PL310_DISABLE			2
-#define IMX_SIP_PL310_ENABLE_WRITEBACK		3
-#define IMX_SIP_PL310_DISABLE_WRITEBACK		4
-#define IMX_SIP_PL310_ENABLE_WFLZ		5
+
 
 #endif /* __IMX_SIP_H__ */
--- core/arch/arm/plat-imx/sub.mk
+++ core/arch/arm/plat-imx/sub.mk
@@ -1,14 +1,7 @@
 global-incdirs-y += .
 srcs-y += main.c imx-common.c
 
-srcs-$(CFG_MX6)$(CFG_MX7) += imx_src.c mmdc.c
-
 srcs-$(CFG_PL310) += imx_pl310.c
-ifeq ($(CFG_PSCI_ARM32),y)
-$(call force,CFG_PM_ARM32,y)
-asm-defines-y += imx_pm_asm_defines.c
-endif
-
 ifneq (,$(filter y, $(CFG_MX6Q) $(CFG_MX6QP) $(CFG_MX6D) $(CFG_MX6DL) \
 	$(CFG_MX6S) $(CFG_MX6SL) $(CFG_MX6SLL) $(CFG_MX6SX)))
 srcs-y += a9_plat_init.S
@@ -18,7 +11,5 @@ endif
 ifneq (,$(filter y, $(CFG_MX7) $(CFG_MX7ULP) $(CFG_MX6UL) $(CFG_MX6ULL)))
 srcs-y += a7_plat_init.S
 endif
-
-subdirs-$(CFG_PSCI_ARM32) += pm
 
 subdirs-y += drivers
