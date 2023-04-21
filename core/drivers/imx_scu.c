7cbbc766a08cf4cd3b20460c5ecd7a37bfedcce1
Clement Faure <clement.faure@nxp.com>
2023-04-21 20:11:03 +0200

    drivers: imx_scu: add resume capabilities
    Add suspend and resume callbacks for the driver.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>

--- core/drivers/imx_scu.c
+++ core/drivers/imx_scu.c
@@ -8,7 +8,9 @@
 #include <imx.h>
 #include <initcall.h>
 #include <io.h>
+#include <kernel/cache_helpers.h>
 #include <kernel/tz_ssvce_def.h>
+#include <kernel/pm.h>
 #include <mm/core_memprot.h>
 
 /* Invalidate all registers */
@@ -36,4 +38,24 @@ TEE_Result scu_init(void)
 
 	return TEE_SUCCESS;
 }
-driver_init(scu_init);
+
+static TEE_Result pm_enter_resume(enum pm_op op, uint32_t pm_hint __unused,
+		const struct pm_callback_handle *pm_handle __unused)
+{
+	if (op == PM_OP_RESUME) {
+		scu_init();
+		dcache_op_all(DCACHE_OP_CLEAN_INV);
+	}
+	return TEE_SUCCESS;
+}
+
+static TEE_Result scu_configure(void)
+{
+	scu_init();
+#ifdef CFG_PSCI_ARM32
+	register_pm_driver_cb(pm_enter_resume, NULL, "imx-scu");
+#endif
+	return TEE_SUCCESS;
+}
+
+driver_init(scu_configure);
