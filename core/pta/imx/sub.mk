aeedd9b4f47dc8a24ade6303a621a9b0ed0c4469
Olivier Masse <olivier.masse@nxp.com>
2023-06-27 15:34:03 +0200

    LFOPTEE-261 core: pta: imx ocotp ELE support added
    Introduce ELE read fuse function to support
    PTA_OCOTP_CMD_READ_FUSE pta command on imx93.
    
    Signed-off-by: Olivier Masse <olivier.masse@nxp.com>

--- core/pta/imx/sub.mk
+++ core/pta/imx/sub.mk
@@ -1,5 +1,5 @@
 srcs-$(CFG_IMX_DIGPROG) += digprog.c
-srcs-$(CFG_IMX_OCOTP) += ocotp.c
+srcs-$(call cfg-one-enabled,CFG_IMX_OCOTP CFG_IMX_ELE) += ocotp.c
 srcs-$(CFG_NXP_CAAM_MP_DRV) += manufacturing_protection.c
 srcs-$(CFG_NXP_CAAM_DEK_DRV) += dek_blob.c
 srcs-$(CFG_IMX_TRUSTED_ARM_CE) += trusted_arm_ce.c
