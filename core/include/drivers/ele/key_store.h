6b268bff6631c6ecbb89f93054b6eb331a0fa12a
Sahil Malhotra <sahil.malhotra@nxp.com>
2023-09-19 12:14:08 +0200

    LFOPTEE-277 drivers: ele: make imx_ele_key_store_open()/close() public
    These functions can be used from PTA, so making them public.
    
    Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>

--- core/include/drivers/ele/key_store.h
+++ core/include/drivers/ele/key_store.h
@@ -8,6 +8,33 @@
 #include <tee_api_types.h>
 
 /*
+ * Open a Keystore session with EdgeLock Enclave.
+ *
+ * @session_handle: EdgeLock Enclave session handle
+ * @key_store_id: User defined word identifying the key store
+ * @auth_nonce: Nonce used as authentication proof for accessing
+ *		the key store.
+ * @create: Whether to create the key store or load it.
+ * @mon_inc: Whether to increment the monotonic counter or not.
+ * @sync: Whether to push persistent keys in the NVM(Non Volatile Memory).
+ *        Without it, even if the key attribute is set as persistent
+ *        at the key creation (generation, importation), the key will
+ *        not be stored in the NVM.
+ * @key_store_handle: EdgeLock Enclave Key store handle.
+ */
+TEE_Result imx_ele_key_store_open(uint32_t session_handle,
+				  uint32_t key_store_id, uint32_t auth_nonce,
+				  bool create, bool mon_inc, bool sync,
+				  uint32_t *key_store_handle);
+
+/*
+ * Close Key store with EdgeLock Enclave.
+ *
+ * @key_store_handle: EdgeLock Enclave key store handle
+ * @strict: Whether to push persistent keys in the NVM.
+ */
+TEE_Result imx_ele_key_store_close(uint32_t key_store_handle);
+/*
  * Get global Key store handle.
  *
  * @key_store_handle: EdgeLock Enclave key store handle
