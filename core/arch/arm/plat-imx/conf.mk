2bf41cb3ba2edcfbf9e93234d31893be0da7b1d0
Sahil Malhotra <sahil.malhotra@nxp.com>
2023-09-19 12:15:53 +0200

    LFOPTEE-277 pta: ele_test: add ELE Test PTA
    This PTA will be used for testing ELE features.
    Generate key/delete key feature is added in this.
    
    Set CFG_IMX_ELE_TEST_PTA=y to compile this PTA
    This PTA has been tested only o i.MX93 A1 SoCs.
    
    Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>

--- core/arch/arm/plat-imx/conf.mk
+++ core/arch/arm/plat-imx/conf.mk
@@ -262,6 +262,7 @@ $(call force,CFG_NXP_CAAM,n)
 CFG_IMX_MU ?= y
 CFG_IMX_ELE ?= y
 CFG_IN_TREE_EARLY_TAS += trusted_keys/f04a0fe7-1f5d-4b9b-abf7-619b85b4ce8c
+CFG_IMX_ELE_TEST_PTA ?= n
 else ifneq (,$(filter $(PLATFORM_FLAVOR),$(mx95-flavorlist)))
 $(call force,CFG_MX95,y)
 $(call force,CFG_ARM64_core,y)
