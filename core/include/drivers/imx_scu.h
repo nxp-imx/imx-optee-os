caa9eeb8136d36c33eca51db181fdb619f38c47d
Clement Faure <clement.faure@nxp.com>
2023-09-28 17:11:08 +0200

    core: imx: export scu_init function
    Export scu_init() function for cpu idle.
    
    Signed-off-by: Clement Faure <clement.faure@nxp.com>

---
+++ core/include/drivers/imx_scu.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright 2023 NXP
+ */
+#ifndef __DRIVERS_IMX_SCU_H
+#define __DRIVERS_IMX_SCU_H
+
+#include <tee_api_types.h>
+
+TEE_Result scu_init(void);
+
+#endif /* __DRIVERS_IMX_SCU_H */
