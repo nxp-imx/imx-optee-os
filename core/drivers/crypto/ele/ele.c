03957d9b04b98743d2f6a1f4ec33efd7aa4c3953
Sahil Malhotra <sahil.malhotra@nxp.com>
2025-04-01 20:51:51 +0200

    LFOPTEE-419 drivers: ele: enable getting HUK/RNG from ELE on i.MX943
    Enable support for getting HUK/RNG from ELE on i.MX943
    
    Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>

--- core/drivers/crypto/ele/ele.c
+++ core/drivers/crypto/ele/ele.c
@@ -395,7 +395,8 @@ static TEE_Result imx_ele_global_init(void)
 
 driver_init(imx_ele_global_init);
 
-#if defined(CFG_MX93) || defined(CFG_MX91) || defined(CFG_MX95)
+#if defined(CFG_MX93) || defined(CFG_MX91) || defined(CFG_MX95) || \
+	defined(CFG_MX943)
 /*
  * Key buffer pointer must be align on a cache line
  * as cache invalidate is done after key derivation.
