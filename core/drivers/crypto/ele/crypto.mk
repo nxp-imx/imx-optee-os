3ebf49d0d72a7bc435d0f207d6d19f3ce21b87ac
Sahil Malhotra <sahil.malhotra@nxp.com>
2025-07-29 11:12:43 +0200

    LFOPTEE-455 drivers: imx: ele: disable Software RNG
    For enabling DM-crypt by default, need to enable getting RNG
    from ELE.
    
    Signed-off-by: Sahil Malhotra <sahil.malhotra@nxp.com>

--- core/drivers/crypto/ele/crypto.mk
+++ core/drivers/crypto/ele/crypto.mk
@@ -17,5 +17,5 @@ endif # CFG_CRYPTO_DRIVER
 # use the software RNG instead in OPTEE.
 # But with Kernel ELE driver disabled, Runtime ELE RNG
 # generation can be done.
-CFG_WITH_SOFTWARE_PRNG ?= y
+CFG_WITH_SOFTWARE_PRNG ?= n
 endif # CFG_IMX_ELE
